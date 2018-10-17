77dc19034d9e410ea20751f4e6c37f96e3f84423
Majk Shkurti <majkshkurti94@gmail.com>
2018-10-17 18:45:46 +0200

    Working on ways layer interaction

--- app/front_end/js/interaction.js
+++ app/front_end/js/interaction.js
@@ -1,10 +1,12 @@
 import {GeoJSON,WFS,GML} from 'ol/format';
+import {intersects as intersectsFilter} from 'ol/format/filter';
+import {fromCircle} from 'ol/geom/Polygon';
 import {Vector as VectorLayer} from 'ol/layer';
 import {Vector as VectorSource} from 'ol/source'; 
 import ApiConstants from './secrets';	
 import {bbox as bboxStrategy} from 'ol/loadingstrategy';
 import {map} from './map';
-import {iconStyle,drawing_style} from './style';
+import {iconStyle,drawing_style,boundaryStyle} from './style';
 import Select from 'ol/interaction/Select';
 import {Draw, Modify, Snap} from 'ol/interaction';
 import { pointerMove} from 'ol/events/condition';
@@ -12,7 +14,8 @@ import {Stroke, Style} from 'ol/style';
 import Feature from 'ol/Feature';
 import {tool_tip} from './tooltip';
 import {draw_isochrone} from './isochrones';
- 	
+
+
 const userid = Math.floor(Math.random() * 10000000);
 
 var objectid; 	
@@ -278,10 +281,180 @@ $('button').click(function () {
             break;
     }
 });
+
+
+
+
+//--- WAYS LAYER USER INTERACTION ---//
+
+function WfsRequestFunction (srsName,namespace,workspace,layerName,filter){
+    console.log(srsName,namespace,workspace,layerName,filter);
+    var wfs = new WFS().writeGetFeature({
+        srsName: srsName,
+        featureNS: namespace,
+        featurePrefix: workspace,
+        featureTypes: [layerName],
+        outputFormat: 'application/json',
+        filter: filter
+    });
+    var xmlparser = xs.serializeToString(wfs);
+    return xmlparser;
+}
+
+
+    //Add the query layer to the map
+var QueryLayerSouce = new VectorSource({wrapX: false});
+var QueryLayer = new VectorLayer({
+        source: QueryLayerSouce,
+        style: drawing_style
+    });
+    QueryLayer.setMap(map);
+
+    //Ways Layer
+var ExtractStreetsSource = new VectorSource({wrapX: false});
+var ExtractStreetsLayer = new VectorLayer({
+    source: ExtractStreetsSource,
+    style: boundaryStyle
+});
+ExtractStreetsLayer.setMap(map);
+
+var searchInteraction = {
+    interaction: null,
+    drawStartEvent: function (evt){
+       
+    },
+    drawStopEvent: function (evt){
+     
+        $('#mySpinner').addClass('spinner');
+        var circle = evt.feature.getGeometry();  
+        console.log(circle);  
+       var geom = fromCircle(circle);
+       var filter = intersectsFilter('geom', geom, 'EPSG:3857');
+        //Get the feature that intersect with geometry
+       var wfsRequestXmlString = WfsRequestFunction('EPSG:3857',ApiConstants.geoserver_namespaceURI, ApiConstants.geoserver_workspace,'ways',filter);
+        fetch(ApiConstants.wfs_url,{
+            method: 'POST',
+            body: wfsRequestXmlString,
+            headers: {
+                'Content-Type': 'application/xml',
+                'Accept': 'application/xml'
+            }
+        }).then(function(response){
+            return response.json();
+        }).then(function (json) {
+            $('#mySpinner').removeClass('spinner');	
+            var features = new GeoJSON().readFeatures(json);
+            if (features.length == 0) {
+                return;
+            }
+            ExtractStreetsSource.clear();
+            ExtractStreetsSource.addFeatures(features);
+            searchInteraction.stop();
+          
+        });
+
+    },
+    init: function(){
+        this.stop();
+        QueryLayer.getSource().clear();
+        ExtractStreetsSource.clear();
+        //Add circle interaction to search for the geojson features on the map
+        var circleDraw = new Draw({
+            source: QueryLayer.getSource(),
+            type: 'Circle'
+        });
+        this.interaction = circleDraw;
+        map.addInteraction(circleDraw);
+        circleDraw.on('drawend',this.drawStopEvent);
+        circleDraw.on('drawstart',this.drawStartEvent);
+    },
+    stop: function (){
+        if (this.interaction != null){
+            map.removeInteraction(this.interaction);
+        }
+        this.interaction = null;
+    }
+}
+
+//BUTTON EVENT HANDLERS
+
+var waysInteraction = {
+    featuresToCommit: [],
+    currentInteraction: null,
+    interactionType: null,
+    snapInteraction: null,
+    drawStart: function (evt) {
+        waysInteraction.featuresToCommit = [];
+       
+        
+    },
+    drawEnd: function (evt){
+     var geom = evt.getGeometry();
+
+    },
+    remove: function(){
+     if (this.snapInteraction != null){
+         map.removeInteraction(this.snapInteraction);
+     }
+     if (this.currentInteraction != null){
+         map.removeInteraction(this.currentInteraction);
+     }
+    },
+    add: function (interaction,type){
+        this.remove();
+        map.addInteraction(interaction);
+        var snap = new Snap({source: ExtractStreetsSource});
+        map.addInteraction(snap);
+        this.currentInteraction = interaction;
+        this.interactionType = type;
+        interaction.on('drawstart',this.drawStart);
+        interaction.on('drawend',this.drawEnd);
+        this.snapInteraction = snap
+    }
+}
+
+$('.expert_draw').click(function () {
+    let buttonID = this.id;
+    if (buttonID == 'btnQuery'){
+        searchInteraction.init();
+        return;
+    }
+
+    switch(buttonID){
+        case 'btnDraw':
+       var drawInteraction = new Draw({
+                source: ExtractStreetsSource,
+                type: 'LineString'
+                });
+        waysInteraction.add(drawInteraction,'draw');
+        tool_tip(drawInteraction,'line');
+        break;
+        case 'btnModify':
+        var modifyInteraction = new Modify({
+                source: ExtractStreetsSource,
+                });
+                console.log('modify executed');
+        waysInteraction.add(modifyInteraction,'edit');
+        break
+        case 'btnDelete':
+        break;
+        //click event
+       
+    }
+
+ 
+
+
+})
+
+//Interaction Functions
+
+
+
+
+
+
 export {userid,number_calculations,layerWFS_point,drawnLine};
-
-
-
 
 
 //http://212.83.58.36:8080/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&viewparams=gid:xxx;&typeNames=cite:pois
