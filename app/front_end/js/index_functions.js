25e5ae9b1e41f3727ae2668601fdc124bbb08bd3
Majk Shkurti <majkshkurti94@gmail.com>
2018-12-02 13:46:40 +0100

    Changed Curson Pointer on Thematic Data Pois Label

--- app/front_end/js/index_functions.js
+++ app/front_end/js/index_functions.js
@@ -40,7 +40,7 @@ let index_function = function () {
 				  let cell = `<input type="checkbox" class ="filled-in thematic_item_check thematic_item_checkShared" id="check_${array[key_1]}" unchecked></input>`
 								  +`<label for="check_${array[key_1]}" style="padding-left:25px;height:16px;"></label>`
 				  				  +`<img style="padding-right:5px;" for="check_${array[key_1]}" src="../pois/${pois[array[key_1]][0]}.png">`
-			     				  +`<label for="check_${array[key_1]}">${pois[array[key_1]][1]}</label>`	
+			     				  +`<label style="cursor:pointer;" for="check_${array[key_1]}">${pois[array[key_1]][1]}</label>`	
 								  + `<input name="n" class="thematic_data_weight"type="number" min="1" max="5" step="1" value="1"/>`
 				console.log(pois[array[key_1]][0]);
 				  
