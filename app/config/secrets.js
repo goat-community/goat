b824ac0c3e02934894f7769e5f6541851643daac
Elias Pajares <elias.pajares@tum.de>
2018-12-13 18:49:22 +0100

    work on network modification

--- app/config/secrets.js
+++
@@ -1,7 +0,0 @@
-HOST="localhost"
-PORT=5432
-DATABASE="goat"
-USER="goat"
-PASSWORD="earlmanigault"
-DOWNLOAD_LINK="https://download.geofabrik.de/europe/germany/bayern/oberbayern-latest.osm.pbf"
-BOUNDING_BOX="--bounding-box top=48.456141 left=11.619284 bottom=48.324715 right=11.796303"
