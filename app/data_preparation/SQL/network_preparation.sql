afa609e0ba6ecb1fd7f568a387749582a8ecc66a
Elias Pajares <elias.pajares@tum.de>
2019-02-25 14:16:43 +0100

    improved network preparation

--- app/data_preparation/SQL/network_preparation.sql
+++ app/data_preparation/SQL/network_preparation.sql
@@ -17,6 +17,47 @@ UPDATE ways_vertices_pgr SET class_ids = c.class_ids
 FROM class_ids c
 WHERE ways_vertices_pgr.id = c.id;
 
+--Mark vertices that are on network islands
+WITH count_ids AS (
+	SELECT count(*), source AS id 
+	FROM ways
+	GROUP by source
+	UNION ALL
+	SELECT count(*), target AS id
+	FROM ways
+	GROUP by target
+),
+only_once AS (
+	SELECT c.id,sum(c.count), v.geom 
+	FROM count_ids c, ways_vertices_pgr v
+	WHERE c.id = v.id
+	GROUP by c.id, v.geom
+	having sum(count) < 2
+),
+vertices_islands AS (
+	SELECT w.source, w.target
+	FROM only_once o, only_once o1, ways w
+	WHERE w.source = o.id 
+	and w.target = o1.id
+),
+vertices_to_update AS (
+	SELECT x.id
+	FROM (
+		SELECT source AS id 
+		FROM vertices_islands 
+		UNION ALL
+		SELECT target AS id 
+		FROM vertices_islands 
+	) x
+	, ways_vertices_pgr v
+	WHERE v.id = x.id
+)
+UPDATE ways_vertices_pgr 
+SET class_ids = array[0]
+FROM vertices_to_update v
+WHERE ways_vertices_pgr.id = v.id;
+
+
 CREATE TABLE ways_userinput as
 SELECT * FROM ways;
 
@@ -61,62 +102,62 @@ insert INTO osm_way_classes(class_id,name) values(503,'tertiary_use_sidepath');
 insert INTO osm_way_classes(class_id,name) values(504,'tertiary_link_use_sidepath');
 insert INTO osm_way_classes(class_id,name) values(601,'foot_yes');
 
-with links_to_UPDATE as (
+WITH links_to_UPDATE as (
 SELECT osm_id, o.class_id 
 FROM planet_osm_line p, osm_way_classes o
 WHERE highway in('secondary','secondary_link','tertiary','tertiary_link')
 AND foot = 'use_sidepath'
 AND concat(highway,'_use_sidepath') = o.name
 )
-UPDATE ways set class_id = l.class_id
+UPDATE ways SET class_id = l.class_id
 FROM links_to_UPDATE l
 WHERE ways.osm_id = l.osm_id;
-UPDATE ways_userinput set class_id = w.class_id
+UPDATE ways_userinput SET class_id = w.class_id
 FROM ways w 
 WHERE ways_userinput.osm_id = w.osm_id;
 
 
-with links_to_UPDATE as (
+WITH links_to_UPDATE as (
 SELECT osm_id, o.class_id 
 FROM planet_osm_line p, osm_way_classes o
 WHERE highway in('secondary','secondary_link','tertiary','tertiary_link')
 AND foot = 'use_sidepath'
 AND concat(highway,'_use_sidepath') = o.name
 )
-UPDATE ways set class_id = l.class_id
+UPDATE ways SET class_id = l.class_id
 FROM links_to_UPDATE l
 WHERE ways.osm_id = l.osm_id;
-UPDATE ways_userinput set class_id = w.class_id
+UPDATE ways_userinput SET class_id = w.class_id
 FROM ways w 
 WHERE ways_userinput.osm_id = w.osm_id;
 
 
-with links_to_UPDATE as (
+WITH links_to_UPDATE as (
 SELECT osm_id
 FROM planet_osm_line p
 WHERE foot = 'yes'
 )
-UPDATE ways set class_id = 601
+UPDATE ways SET class_id = 601
 FROM links_to_UPDATE l
 WHERE ways.osm_id = l.osm_id;
-UPDATE ways_userinput set class_id = w.class_id
+UPDATE ways_userinput SET class_id = w.class_id
 FROM ways w 
 WHERE ways_userinput.osm_id = w.osm_id;
 
 
-with links_to_UPDATE as (
+WITH links_to_UPDATE as (
 SELECT osm_id, o.class_id 
 FROM planet_osm_line p, osm_way_classes o
 WHERE highway in('secondary','secondary_link','tertiary','tertiary_link')
 AND foot = 'use_sidepath'
 AND concat(highway,'_use_sidepath') = o.name
 )
-UPDATE ways set class_id = l.class_id
+UPDATE ways SET class_id = l.class_id
 FROM links_to_UPDATE l
 WHERE ways.osm_id = l.osm_id;
 
 
-with class_id as (
+WITH class_id as (
 SELECT unnest(variable_array) class_id
 FROM variable_container 
 WHERE identifier = 'excluded_class_id_walking'
@@ -139,10 +180,10 @@ WHERE (tags -> 'sidewalk') IS NOT NULL
 AND (tags -> 'sidewalk') <> 'no'
 AND (tags -> 'sidewalk') <> 'none'
 )
-UPDATE ways set class_id = 601
+UPDATE ways SET class_id = 601
 FROM links_to_UPDATE l
 WHERE ways.osm_id = l.osm_id;
 
-UPDATE ways_userinput set class_id = w.class_id
+UPDATE ways_userinput SET class_id = w.class_id
 FROM ways w 
 WHERE ways_userinput.osm_id = w.osm_id;
