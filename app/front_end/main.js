0cb1ec8e8ce0e78fa4899776281cefe241d95fb2
Majk Shkurti <majkshkurti@outlook.com>
2019-04-08 20:35:52 +0200

    Removed frontend.

--- app/front_end/main.js
+++
@@ -1,8 +0,0 @@
-
-import './js/events';
-import './js/tooltip';
-import './js/index_functions';
-import './js/map';
-import './js/data_preparation';
-import './js/isochrones';
-import './js/interaction';
