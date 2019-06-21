e6fab8bceeae55aeb65574e47dd520099ad7a3ce
Elias Pajares <elias.pajares@tum.de>
2019-06-21 21:51:53 +0200

    work on performance pre_calculate_grid

--- app/database_functions/pgrouting_edges_multi.sql
+++ app/database_functions/pgrouting_edges_multi.sql
@@ -1,5 +1,6 @@
-CREATE OR REPLACE FUNCTION public.pgrouting_edges_multi(minutes integer,array_starting_points NUMERIC[][],speed numeric)
- RETURNS TABLE (start_vertex bigint, geom geometry, cost numeric) 
+
+CREATE OR REPLACE FUNCTION public.pgrouting_edges_multi(minutes integer,array_starting_points NUMERIC[][],speed NUMERIC, objectids int[])
+ RETURNS SETOF type_catchment_edges 
  LANGUAGE plpgsql
 AS $function$
 DECLARE
@@ -9,7 +10,7 @@ excluded_class_id text;
 categories_no_foot text;
 buffer geometry;
 begin
-  DROP TABLE closest_vertices;
+  DROP TABLE IF EXISTS closest_vertices;
   distance = minutes*speed;
  
   SELECT variable_array::text
@@ -23,13 +24,14 @@ begin
   WHERE identifier = 'categories_no_foot';
  
   CREATE temp TABLE closest_vertices AS  
-  SELECT closest_vertex[1]::bigint closest_vertices, closest_vertex[2]::geometry AS geom FROM (
-	  SELECT closest_vertex(lat_lon_array[1],lat_lon_array[2],0.0018,'excluded_class_id_walking')
-	  --INTO array_starting_vertices
+  SELECT closest_vertex[1]::bigint closest_vertices, closest_vertex[2]::geometry AS geom, objectid 
+  FROM (
+	  SELECT closest_vertex(lat_lon_array[1],lat_lon_array[2],0.0018,'excluded_class_id_walking'), objectid
 	  FROM (
-	  	SELECT UNNEST_2d_1d(array_starting_points) AS lat_lon_array
+	  	SELECT UNNEST_2d_1d(array_starting_points) AS lat_lon_array, UNNEST(objectids) AS objectid
 	  )x
   ) y;
+
   
  SELECT array_agg(closest_vertices)
  INTO array_starting_vertices
@@ -40,18 +42,16 @@ begin
  FROM closest_vertices c;
   
  RETURN query 
-  SELECT from_v start_vertex, v.geom, x.agg_cost::numeric AS cost
-  FROM ways_vertices_pgr v, 
-  (	SELECT seq, from_v, node, edge, agg_cost FROM pgr_drivingDistance(
+  SELECT x.from_v::int start_vertex, x.node::int, x.edge::int, x.agg_cost::numeric AS cost, w.geom, c.objectid, w.class_id
+  FROM ways w, 
+  (	SELECT from_v, node, edge, agg_cost FROM pgr_drivingDistance(
 	'SELECT id::int4, source, target, length_m as cost 
 	FROM ways
 	WHERE NOT class_id = any(''' || excluded_class_id || ''')
     AND (NOT foot = any('''||categories_no_foot||''') OR foot IS NULL)
 	AND geom && (SELECT ST_Buffer(ST_Union(c.geom)::geography,'||distance||')::geometry FROM closest_vertices c)'
 	,array_starting_vertices, distance,FALSE,FALSE)
-  )x
-  WHERE v.id = x.node;
+  )x, closest_vertices c
+  WHERE w.id = x.edge AND c.closest_vertices = from_v;
 END ;
 $function$;
-
---SELECT * FROM pgrouting_edges_multi(10,array[[11.50972,48.18523],[11.51356,48.18517],[11.51160,48.18301]],83.33)
