d3633cc419a68a099f6dba6c4c581144efcc5cf2
Majk Shkurti <majkshkurti@outlook.com>
2019-08-13 10:47:35 +0200

    Added Dynamic Heatmap graph

---
+++ app/client/src/components/layers/filter/DynamicHeatmap.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card class="">
+      <v-app-bar color="green" dark>
+        <v-app-bar-nav-icon
+          ><v-icon>fas fa-chart-line</v-icon></v-app-bar-nav-icon
+        >
+        <v-toolbar-title
+          >Dynamic Heatmap - ({{ selectedAmenity.name }})</v-toolbar-title
+        >
+
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon @click.stop="show = false"
+          ><v-icon>close</v-icon></v-app-bar-nav-icon
+        >
+      </v-app-bar>
+      <v-card-text>
+        <!-- <v-select class="mx-2" label="Select sensitivity" outlined></v-select> -->
+        <v-text-field
+          v-model="selectedAmenity.sensitivity"
+          class="mx-2"
+          label="Accessibility index"
+          outlined
+        ></v-text-field>
+        <line-chart :amenity="selectedAmenity" />
+      </v-card-text>
+      <v-card-actions> </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import LineChart from "../../other/LineChart";
+
+export default {
+  components: {
+    LineChart
+  },
+  props: {
+    visible: { type: Boolean, required: false, default: false },
+    selectedAmenity: { type: Object }
+  },
+  computed: {
+    show: {
+      get() {
+        return this.visible;
+      },
+      set(value) {
+        if (!value) {
+          this.$emit("close");
+        }
+      }
+    }
+  }
+};
+</script>
---
+++ app/client/src/components/other/LineChart.vue
@@ -0,0 +1,84 @@
+<script>
+import { Line } from "vue-chartjs";
+import { mapGetters } from "vuex";
+
+export default {
+  extends: Line,
+  props: {
+    chartdata: { type: Object },
+    options: { type: Object },
+    amenity: { type: Object }
+  },
+  mounted() {
+    this.renderLineChart();
+  },
+  methods: {
+    renderLineChart: function() {
+      this.renderChart(
+        {
+          labels: this.dynamicHeatmapTravelTimes,
+          datasets: [
+            {
+              label: "Accessibility",
+              fill: false,
+              borderColor: "rgb(54, 162, 235)",
+              data: this.accessibilityGravityData,
+              pointRadius: 2
+            }
+          ]
+        },
+        {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  callback: function(value) {
+                    return value + " %";
+                  }
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: "Accessibility Gravity (%)"
+                }
+              }
+            ],
+            xAxes: [
+              {
+                scaleLabel: {
+                  display: true,
+                  labelString: "Traveltime (seconds)"
+                }
+              }
+            ]
+          }
+        }
+      );
+    }
+  },
+  watch: {
+    amenity: {
+      handler: function() {
+        this.renderLineChart();
+      },
+      deep: true
+    }
+  },
+  computed: {
+    ...mapGetters("pois", {
+      dynamicHeatmapTravelTimes: "dynamicHeatmapTravelTimes"
+    }),
+    accessibilityGravityData() {
+      console.log("passed");
+      console.log(this.amenity);
+
+      const accessibilityGravity = this.dynamicHeatmapTravelTimes.map(
+        x => Math.exp(x * this.amenity.sensitivity) * 100
+      );
+      return accessibilityGravity;
+    }
+  }
+};
+</script>
