f00019c128603ca3d6b6caf52da0d4d277eeda68
Majk Shkurti <majkshkurti@outlook.com>
2019-08-18 17:10:33 +0200

    Fixed api isochrone count.

--- app/client/src/components/layers/filter/DynamicHeatmap.vue
+++ app/client/src/components/layers/filter/DynamicHeatmap.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="show" max-width="500px">
+  <v-dialog v-model="show" scrollable max-width="500px">
     <v-card class="">
       <v-app-bar color="green" dark>
         <v-app-bar-nav-icon
@@ -14,28 +14,29 @@
           ><v-icon>close</v-icon></v-app-bar-nav-icon
         >
       </v-app-bar>
-      <v-card-text>
-        <v-select
-          v-model="selectedAmenity.weight"
-          :items="weightListValues"
-          @change="updateHeatmap"
-          class="mx-2 mt-4"
-          label="Select Weight"
-          outlined
-        ></v-select>
-        <template>
-          <v-combobox
-            v-model="selectedAmenity.sensitivity"
-            :items="sensitivityListValues"
+      <vue-scroll>
+        <v-card-text>
+          <v-select
+            v-model="selectedAmenity.weight"
+            :items="weightListValues"
             @change="updateHeatmap"
-            class="mx-2"
-            label="Sensitivity index"
+            class="mx-2 mt-4"
+            label="Select Weight"
             outlined
-          ></v-combobox>
-          <sensitivity-chart :amenity="selectedAmenity" />
-        </template>
-      </v-card-text>
-      <v-card-actions> </v-card-actions>
+          ></v-select>
+          <template>
+            <v-combobox
+              v-model="selectedAmenity.sensitivity"
+              :items="sensitivityListValues"
+              @change="updateHeatmap"
+              class="mx-2"
+              label="Sensitivity index"
+              outlined
+            ></v-combobox>
+            <sensitivity-chart :amenity="selectedAmenity" />
+          </template>
+        </v-card-text>
+      </vue-scroll>
     </v-card>
   </v-dialog>
 </template>
--- app/client/src/controllers/OlIsochroneController.js
+++ app/client/src/controllers/OlIsochroneController.js
@@ -2,9 +2,12 @@ import OlBaseController from "./OlBaseController";
 import OlStyleDefs from "../style/OlStyleDefs";
 import VectorSource from "ol/source/Vector";
 import VectorLayer from "ol/layer/Vector";
+import DrawInteraction, { createBox } from "ol/interaction/Draw";
 import { unByKey } from "ol/Observable";
-import store from "../store/modules/isochrones";
+import isochroneStore from "../store/modules/isochrones";
+import poisStore from "../store/modules/pois";
 import LayerUtils from "../utils/Layer";
+import { transform } from "ol/proj.js";
 
 export default class OlIsochroneController extends OlBaseController {
   constructor(map) {
@@ -32,24 +35,38 @@ export default class OlIsochroneController extends OlBaseController {
    */
   addInteraction(calculationType) {
     const me = this;
-    me.removeInteraction();
+    me.clear();
     //Add Interaction for single|multiple calculation type...
     if (calculationType === "single") {
       console.log("single...");
     } else {
+      console.log(isochroneStore.state.multiIsochroneCalculationMethods);
       if (
-        store.state.multiIsochroneCalculationMethods.active === "study_area"
+        isochroneStore.state.multiIsochroneCalculationMethods.active ===
+        "study_area"
       ) {
+        //Study are method
         if (!me.studyAreaLayer) {
           me.studyAreaLayer = LayerUtils.getAllChildLayers(me.map).filter(
             layer => layer.get("name") === "study_area_administration"
           );
         }
         if (me.studyAreaLayer.length > 0) {
-          //Make study area layer visible if is not.
           me.studyAreaLayer[0].setVisible(true);
         }
         me.setupMapClick();
+      } else {
+        //Draw Boundary box method
+        const drawBoundary = new DrawInteraction({
+          source: me.selectionSource,
+          type: "Circle",
+          geometryFunction: createBox()
+        });
+
+        me.map.addInteraction(drawBoundary);
+
+        // make select interaction available as member
+        me.drawBoundary = drawBoundary;
       }
     }
   }
@@ -62,12 +79,27 @@ export default class OlIsochroneController extends OlBaseController {
     const me = this;
     const map = me.map;
     me.mapClickListenerKey = map.on("click", evt => {
-      console.log(evt);
+      const coordinate = transform(evt.coordinate, "EPSG:3857", "EPSG:4326");
+      const speed = isochroneStore.state.options.speed;
+      const minutes = isochroneStore.state.options.minutes;
+      const regionType = "study_area";
+
+      const amenities = poisStore.state.selectedPois
+        .map(item => {
+          return "'" + item.value + "'";
+        })
+        .toString();
+
+      console.log(coordinate, speed, minutes, regionType, amenities);
     });
   }
 
   removeInteraction() {
     const me = this;
+    // cleanup possible old select interaction
+    if (me.drawBoundary) {
+      me.map.removeInteraction(me.drawBoundary);
+    }
     if (me.mapClickListenerKey) {
       unByKey(me.mapClickListenerKey);
     }
@@ -77,7 +109,7 @@ export default class OlIsochroneController extends OlBaseController {
     super.clear();
     const me = this;
     if (me.selectionSource) {
-      me.selectionSource().clear();
+      me.selectionSource.clear();
     }
   }
 }
