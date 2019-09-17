7bda6c7b08d121ea6f9c63de5715de4998d6f7f2
Majk Shkurti <majkshkurti@outlook.com>
2019-09-17 19:44:41 +0200

    Test travis-ci => docker hub

---
+++ scripts/docker-public-travis.sh
@@ -0,0 +1,25 @@
+DOCKER_ENV=''
+DOCKER_TAG=''
+
+case "$TRAVIS_BRANCH" in
+  "master")
+    DOCKER_ENV=production
+    DOCKER_TAG=latest
+    ;;
+  "development")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;    
+esac
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+docker build -f ./app/client/Dockerfile -t goat-client:$DOCKER_TAG ./app/client --no-cache
+
+docker tag goat-client:$DOCKER_TAG $DOCKER_USERNAME/goat-client:$DOCKER_TAG
+
+docker push $DOCKER_USERNAME/goat-client:$DOCKER_TAG
+
+
+
+
