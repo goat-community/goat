f6eca5cfdbaade78a3dfe781e890dafdf7e40f99
Majk Shkurti <majkshkurti@outlook.com>
2019-09-24 22:06:04 +0200

    Work on Docker

--- scripts/docker-public-travis.sh
+++ scripts/docker-public-travis.sh
@@ -21,12 +21,11 @@ esac
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 docker build -f app/client/Dockerfile -t goat-client:$DOCKER_TAG app/client --no-cache
+docker build -f app/print/Dockerfile -t goat-print:$DOCKER_TAG app/print --no-cache
+docker build -f app/api/Dockerfile -t goat-print:$DOCKER_TAG app/api --no-cache
 
 docker tag goat-client:$DOCKER_TAG $DOCKER_USERNAME/goat-client:$DOCKER_TAG
+docker tag goat-print:$DOCKER_TAG $DOCKER_USERNAME/goat-print:$DOCKER_TAG
+docker tag goat-api:$DOCKER_TAG $DOCKER_USERNAME/goat-api:$DOCKER_TAG
 
 docker push $DOCKER_USERNAME/goat-client:$DOCKER_TAG
-
-
-
-
-
