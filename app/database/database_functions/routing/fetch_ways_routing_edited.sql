7bf3a405c86fc57b8e19f3f5fd2bcd45c1e40a40
Elias Pajares <elias.pajares@tum.de>
2019-11-16 19:32:21 +0100

    worked on new routing types

--- app/database/database_functions/routing/fetch_ways_routing_edited.sql
+++ app/database/database_functions/routing/fetch_ways_routing_edited.sql
@@ -1,5 +1,5 @@
 DROP FUNCTION IF EXISTS fetch_ways_routing_edited;
-CREATE OR REPLACE FUNCTION public.fetch_ways_routing_edited(buffer_geom text, speed_input numeric, excluded_class_id text[], categories_no_foot text[], modus_input integer, userid_input integer, routing_profile text)
+CREATE OR REPLACE FUNCTION public.fetch_ways_routing_edited(buffer_geom text, speed_input numeric, modus_input integer, userid_input integer, routing_profile text)
  RETURNS SETOF type_fetch_ways_routing
  LANGUAGE plpgsql
 AS $function$
@@ -9,6 +9,8 @@ DECLARE
 	sql_userid text := '';
 	sql_routing_profile text := '';
 	table_name text := 'ways';
+	excluded_class_id text;
+	categories_no_foot text;
 BEGIN 
 	IF modus_input <> 1 THEN 
 		table_name = 'ways_userinput';
@@ -27,19 +29,22 @@ BEGIN
 		sql_ways_ids = ' AND NOT id::int4 = any('''|| excluded_ways_id ||''') ';
 	END IF;
 
+	SELECT select_from_variable_container('excluded_class_id_walking'),
+  	select_from_variable_container('categories_no_foot')
+  	INTO excluded_class_id, categories_no_foot;
+
 	IF  routing_profile = 'safe_night' THEN
 		sql_routing_profile = 'AND (lit_classified = ''yes'' OR lit_classified = ''unclassified'')';
-	END IF;
-	IF  routing_profile = 'wheelchair' THEN
+	ELSEIF routing_profile = 'wheelchair' THEN
 		sql_routing_profile = 'AND ((wheelchair_classified = ''yes'') OR wheelchair_classified = ''limited''
 		OR wheelchair_classified = ''unclassified'')';
 	END IF; 
 
 	RETURN query EXECUTE format(
-		'SELECT  id::integer, source, target, length_m as cost 
+		'SELECT  id::integer, source, target, length_m as cost, geom 
 		FROM '||quote_ident(table_name)||
-		' WHERE NOT class_id = any(''' || excluded_class_id::text || ''')
-    	AND (NOT foot = any('''||categories_no_foot::text||''') OR foot IS NULL)
+		' WHERE NOT class_id = any(''' || excluded_class_id || ''')
+    	AND (NOT foot = any('''||categories_no_foot||''') OR foot IS NULL)
 		AND geom && ST_GeomFromText('''||buffer_geom||''')'||sql_userid||sql_ways_ids||sql_routing_profile
 	);
 END;
