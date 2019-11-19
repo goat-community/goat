83c979900b101f87f268e64279408d9a7700046b
UJehle <u.jehle@tum.de>
2019-11-19 15:12:46 +0100

    added layer "parking"

--- app/database/data_preparation/SQL/network_preparation.sql
+++ app/database/data_preparation/SQL/network_preparation.sql
@@ -251,7 +251,7 @@ CREATE INDEX ON ways USING btree(foot);
 CREATE INDEX ON ways USING btree(id);
 CREATE INDEX ON ways_vertices_pgr USING btree(cnt);
 
---precalculation of visualized features for wheelchair
+--Precalculation of visualized features for wheelchair
 WITH variables AS 
 (
     SELECT variable_object AS wheelchair ,
@@ -313,7 +313,7 @@ FROM
 WHERE w.id = x.id
 ;
 
---precalculation of visualized features for lit
+--Precalculation of visualized features for lit
 WITH variables AS 
 (
     SELECT variable_object AS lit ,
@@ -356,7 +356,7 @@ ALTER TABLE ways_userinput_vertices_pgr add column userid int4;
 CREATE INDEX ON ways_userinput USING btree (userid);
 CREATE INDEX ON ways_userinput_vertices_pgr USING btree (userid);
 
---creation of a table that stores all street crossings
+--Creation of a table that stores all street crossings
 DROP TABLE IF EXISTS crossings;
 CREATE TABLE crossings AS
 (SELECT osm_id, highway, way FROM planet_osm_point WHERE highway = 'crossing');
@@ -377,19 +377,19 @@ FROM (select osm_id, (tags -> 'crossing') AS crossing,
 	FROM planet_osm_point p) l
 WHERE crossings.osm_id = l.osm_id;
 
---creation of a table that stores all sidewalk geometries
-DROP TABLE IF EXISTS ways_offset;
-CREATE TABLE ways_offset AS
+--Creation of a table that stores all sidewalk geometries
+DROP TABLE IF EXISTS ways_offset_sidewalk;
+CREATE TABLE ways_offset_sidewalk AS
 SELECT w.id, w.sidewalk,(ST_OffsetCurve(w.geom,  0.00005, 'join=round mitre_limit=2.0')) AS geom_left, 
 	(ST_OffsetCurve(w.geom,  -0.00005, 'join=round mitre_limit=2.0')) AS geom_right
 FROM ways w
 WHERE w.sidewalk = 'both' OR w.sidewalk = 'left' OR w.sidewalk = 'right';
 
-CREATE INDEX ON ways_offset USING btree(id);
-CREATE INDEX ON ways_offset USING gist(geom_left);
-CREATE INDEX ON ways_offset USING gist(geom_right);
+CREATE INDEX ON ways_offset_sidewalk USING btree(id);
+CREATE INDEX ON ways_offset_sidewalk USING gist(geom_left);
+CREATE INDEX ON ways_offset_sidewalk USING gist(geom_right);
 
---table for visualization of the footpath width
+--Table for visualization of the footpath width
 DROP TABLE IF EXISTS footpaths_union_temp;
 CREATE TABLE footpaths_union_temp AS
 	SELECT o.geom_left AS geom, o.sidewalk,
@@ -399,7 +399,7 @@ CREATE TABLE footpaths_union_temp AS
 		THEN w.sidewalk_both_width
 	ELSE NULL
 	END AS width, highway
-	FROM ways w, ways_offset o
+	FROM ways w, ways_offset_sidewalk o
 	WHERE w.id=o.id AND (o.sidewalk = 'both' OR o.sidewalk = 'left' OR o.sidewalk IS NULL)
 		AND w.class_id::text NOT IN (SELECT unnest(variable_array) from variable_container WHERE identifier = 'excluded_class_id_walking')
 		AND (w.foot NOT IN (SELECT UNNEST(variable_array) FROM variable_container WHERE identifier = 'categories_no_foot') 
@@ -412,7 +412,7 @@ UNION
 		THEN w.sidewalk_both_width
 	ELSE NULL
 	END AS width, highway
-	FROM ways w, ways_offset o
+	FROM ways w, ways_offset_sidewalk o
 	WHERE w.id=o.id AND (o.sidewalk = 'both' OR o.sidewalk = 'right' OR o.sidewalk IS NULL)
 		AND w.class_id::text NOT IN (SELECT unnest(variable_array) from variable_container WHERE identifier = 'excluded_class_id_walking')
 		AND (w.foot NOT IN (SELECT UNNEST(variable_array) FROM variable_container WHERE identifier = 'categories_no_foot') 
@@ -439,4 +439,52 @@ CREATE INDEX ON footpaths_union_temp USING gist(geom);
 ALTER TABLE footpaths_union_temp ADD COLUMN id serial;
 ALTER TABLE footpaths_union_temp ADD PRIMARY KEY(id);
 
+--Creation of a table that stores all parking geometries
+DROP TABLE IF EXISTS ways_offset_parking;
+CREATE TABLE ways_offset_parking AS
+SELECT w.id, w.parking,(ST_OffsetCurve(w.geom,  0.00005, 'join=round mitre_limit=2.0')) AS geom_left, 
+	(ST_OffsetCurve(w.geom,  -0.00005, 'join=round mitre_limit=2.0')) AS geom_right
+FROM ways w
+WHERE w.parking IS NOT NULL OR w.parking_lane_both IS NOT NULL OR w.parking_lane_left IS NOT NULL OR w.parking_lane_right IS NOT NULL;
+
+CREATE INDEX ON ways_offset_parking USING btree(id);
+CREATE INDEX ON ways_offset_parking USING gist(geom_left);
+CREATE INDEX ON ways_offset_parking USING gist(geom_right);
+
+--Table for visualization of parking
+DROP TABLE IF EXISTS parking;
+CREATE TABLE parking AS
+	SELECT o.geom_left AS geom, 
+		w.parking,
+			CASE WHEN w.parking_lane_left IS NOT NULL 
+				THEN w.parking_lane_left
+			WHEN w.parking_lane_both IS NOT NULL 
+				THEN w.parking_lane_both
+			ELSE NULL
+			END AS parking_lane, 
+		highway
+	FROM ways w, ways_offset_parking o
+	WHERE w.id=o.id AND (w.parking_lane_left IS NOT NULL OR w.parking_lane_both IS NOT NULL)
+UNION
+	SELECT o.geom_right AS geom, 
+		w.parking,
+			CASE WHEN w.parking_lane_right IS NOT NULL 
+				THEN w.parking_lane_right
+			WHEN w.parking_lane_both IS NOT NULL 
+				THEN w.parking_lane_both
+			ELSE NULL
+			END AS parking_lane, 
+		highway
+	FROM ways w, ways_offset_parking o
+	WHERE w.id=o.id AND (w.parking_lane_right IS NOT NULL OR w.parking_lane_both IS NOT NULL)
+UNION
+	SELECT o.geom_left, w.parking, 'no' AS parking_lane, w.highway FROM ways w, ways_offset_parking o
+	WHERE w.id=o.id AND w.parking = 'no'
+UNION
+	SELECT o.geom_right, w.parking, 'no' AS parking_lane, w.highway FROM ways w, ways_offset_parking o
+	WHERE w.id=o.id AND w.parking = 'no'
+UNION
+	SELECT geom, parking, NULL AS parking_lane, highway FROM ways
+	WHERE parking IS NULL AND parking_lane_right IS NULL AND parking_lane_left IS NULL AND parking_lane_both IS NULL
+	AND highway IN ('secondary','tertiary','residential','living_street','service');
 
