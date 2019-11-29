5d8df3dd33680a41185b6ae837831f19d834c5d2
UJehle <u.jehle@tum.de>
2019-11-29 17:20:25 +0100

    adjustment of "pois_visualization.sql" to include new condition for wheelchair

--- app/database/database_functions/other/pois_visualization.sql
+++ app/database/database_functions/other/pois_visualization.sql
@@ -1,29 +1,52 @@
---THIS FUNCTION CHECKS IF THE USER INSERTED OPENING HOURS OR NOT AND EXECUTS THE CORRESPONDING FUNCTION TO CREATE THE GEOSERVER VIEW
+--THIS FUNCTION CHECKS THE SELECTED ROUTING PROFILE AND IF THE USER INSERTED OPENING HOURS AND EXECUTS THE CORRESPONDING FUNCTION TO CREATE THE GEOSERVER VIEW
 DROP FUNCTION IF EXISTS pois_visualization;
-CREATE OR REPLACE FUNCTION public.pois_visualization(amenities_input text, d integer, h integer, m integer)
+CREATE OR REPLACE FUNCTION public.pois_visualization(amenities_input text, routing_profile_input text, d integer, h integer, m integer)
  RETURNS SETOF pois_visualization
  LANGUAGE plpgsql
 AS $function$
+DECLARE 	
+	wheelchair_condition text[];
 begin
 
 DROP TABLE IF EXISTS visualization_pois;
 
---if no opening hours are provided by the user
-    IF d = 9999 OR h = 9999 OR m = 9999 THEN 
+	-- Exclude POIs that are not accessible by wheelchair if routing_profile_input = wheelchair
+	IF routing_profile_input = 'wheelchair' THEN 
+		wheelchair_condition = ARRAY['no','No'];
+	ELSE 
+		wheelchair_condition = NULL;
+	END IF;
+
+    --if no opening hours are provided by the user and routing profile is -not- wheelchair
+    IF (d = 9999 OR h = 9999 OR m = 9999) AND routing_profile_input <> 'wheelchair' THEN 
         RETURN query
-        SELECT p.name,p.osm_id,p.opening_hours,p.orgin_geometry,p.geom, NULL AS status
+        SELECT p.name,p.osm_id,p.opening_hours,p.orgin_geometry,p.geom, NULL AS status,p.wheelchair
         FROM pois p,variable_container v 
         WHERE p.amenity IN(amenities_input)
         AND v.identifier = 'poi_categories'
         UNION ALL 
-        SELECT pt.name,NULL AS osm_id,NULL AS orgin_geometry,NULL AS opening_hours,pt.geom , NULL AS status
-        FROM public_transport_stops pt WHERE public_transport_stop IN(amenities_input);
-    --if opening hours are provided by the user
-    ELSE
+        SELECT pt.name,NULL AS osm_id,NULL AS orgin_geometry,NULL AS opening_hours,pt.geom , NULL AS status,pt.wheelchair
+        FROM public_transport_stops pt 
+        WHERE public_transport_stop IN(amenities_input);
+    --if no opening hours are provided by the user and routing profile is wheelchair
+    ELSEIF (d = 9999 OR h = 9999 OR m = 9999) AND routing_profile_input = 'wheelchair' THEN 
+        RETURN query
+        SELECT p.name,p.osm_id,p.opening_hours,p.orgin_geometry,p.geom, NULL AS status,p.wheelchair
+        FROM pois p,variable_container v 
+        WHERE p.amenity IN(amenities_input)
+        AND (p.wheelchair NOT IN (SELECT UNNEST(wheelchair_condition)) OR p.wheelchair IS NULL)
+        AND v.identifier = 'poi_categories'
+        UNION ALL 
+        SELECT pt.name,NULL AS osm_id,NULL AS orgin_geometry,NULL AS opening_hours,pt.geom , NULL AS status,pt.wheelchair
+        FROM public_transport_stops pt 
+        WHERE public_transport_stop IN(amenities_input)
+        AND (wheelchair NOT IN (SELECT UNNEST(wheelchair_condition)) OR wheelchair IS NULL);
+    --if opening hours are provided by the user and routing profile is -not- wheelchair
+    ELSEIF d <> 9999 AND h <> 9999 AND m <> 9999 AND routing_profile_input <> 'wheelchair' THEN 
         RETURN query
         WITH pois_status AS 
         (
-            SELECT p.name,p.osm_id,p.opening_hours,p.orgin_geometry,p.geom,check_open(p.opening_hours,array[d,h,m]) AS status
+            SELECT p.name,p.osm_id,p.opening_hours,p.orgin_geometry,p.geom,check_open(p.opening_hours,array[d,h,m]) AS status,p.wheelchair
             FROM pois p
             WHERE p.amenity IN(amenities_input)
             AND opening_hours IS NOT NULL
@@ -31,10 +54,33 @@ DROP TABLE IF EXISTS visualization_pois;
         SELECT * FROM pois_status
         WHERE status = 'True'
         UNION ALL
-        SELECT pt.name,NULL AS osm_id, NULL AS opening_hours, NULL AS orgin_geometry,pt.geom, NULL AS status
+        SELECT pt.name,NULL AS osm_id, NULL AS opening_hours, NULL AS orgin_geometry,pt.geom, NULL AS status, wheelchair 
         FROM public_transport_stops pt 
         WHERE public_transport_stop IN(amenities_input);
+    --if opening hours are provided by the user and routing profile is wheelchair
+    ELSE 
+        RETURN query
+        WITH pois_status AS 
+        (
+            SELECT p.name,p.osm_id,p.opening_hours,p.orgin_geometry,p.geom,check_open(p.opening_hours,array[d,h,m]) AS status, p.wheelchair 
+            FROM pois p
+            WHERE p.amenity IN(amenities_input)
+            AND opening_hours IS NOT NULL
+        )
+        SELECT * FROM pois_status
+        WHERE status = 'True'
+        AND (wheelchair NOT IN (SELECT UNNEST(wheelchair_condition)) OR wheelchair IS NULL)
+        UNION ALL
+        SELECT pt.name,NULL AS osm_id, NULL AS opening_hours, NULL AS orgin_geometry,pt.geom, NULL AS status, wheelchair 
+        FROM public_transport_stops pt 
+        WHERE public_transport_stop IN(amenities_input)
+        AND (wheelchair NOT IN (SELECT UNNEST(wheelchair_condition)) OR wheelchair IS NULL);
     END IF;
 
 END ;
 $function$
+
+/* SELECT * FROM 
+	(SELECT * FROM regexp_split_to_table(convert_from(decode('cmVzdGF1cmFudCxzdXBlcm1hcmtldA==','base64'),'UTF-8'), ',') AS amenity) x,
+	pois_visualization(x.amenity,'wheelchair', 20, 15, 0);
+*/
