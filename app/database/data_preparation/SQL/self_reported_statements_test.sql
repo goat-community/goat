412c36a29de0fc67dfcc4e62c1c8494c9c79ebb3
UJehle <u.jehle@tum.de>
2020-01-10 10:13:04 +0100

    adjusted the coordinate sytem for the user statements

--- app/database/data_preparation/SQL/self_reported_statements_test.sql
+++ app/database/data_preparation/SQL/self_reported_statements_test.sql
@@ -8,17 +8,17 @@ INSERT INTO user_statements (characteristic, statement, geom)
 VALUES
    ('w40', 'Badly visible curve. Delivery trucks enter the living street way too fast. 
 	Dangerous for people walking on the street.', 
-	ST_SetSRID( ST_Point(1282488.472,6139806.974), 3857)),
+	ST_SetSRID( ST_Point(11.5207902999,48.1976916310), 4326)),
 	('m70', 'Dark underpass, uncomfortable to walk through.', 
-	ST_SetSRID( ST_Point(1283554.049,6140308.233), 3857)),
+	ST_SetSRID( ST_Point(11.5303652325,48.2006969161), 4326)),
 	('m40', 'Social hotspot. This area should be avoided at night.', 
-	ST_SetSRID( ST_Point(1286508.91,6142432.41), 3857)),
+	ST_SetSRID( ST_Point(11.556906316,48.213408834), 4326)),
 	('w25', 'This path is very scary at night.', 
-	ST_SetSRID( ST_Point(1284177.601,6139813.123), 3857)),
+	ST_SetSRID( ST_Point(11.535963676,48.197728236), 4326)),
 	('m40', 'Very confusing road crossing. Too many road users intersect in a too narrow area.', 
-	ST_SetSRID( ST_Point(1282926.295,6139655.084), 3857)),
+	ST_SetSRID( ST_Point(11.52471042,48.19677199), 4326)),
 	('w70', 'There are too few road crossing possibilities at this street.', 
-	ST_SetSRID( ST_Point(1282888.826,6140130.212), 3857)),
+	ST_SetSRID( ST_Point(11.524383135,48.199623895), 4326)),
 	('w25', 'Very uncomfortable area for pedestrians.', 
-	ST_SetSRID( ST_Point(1285150.849,6140721.441), 3857))
+	ST_SetSRID( ST_Point(11.544706173,48.203166156), 4326))
 	;
