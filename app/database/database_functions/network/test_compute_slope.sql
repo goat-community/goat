7b44af5999ad03d096598b1be4e434dcd8880124
Elias Pajares <elias.pajares@tum.de>
2020-01-14 15:14:32 +0100

    worked performance boost slope computation

--- app/database/database_functions/network/test_compute_slope.sql
+++ app/database/database_functions/network/test_compute_slope.sql
@@ -1,21 +1,5 @@
-EXPLAIN ANALYZE
 
-WITH x as (
-	SELECT length_m::numeric, split_long_way(geom,length_m::numeric,30) AS geom 	
-	FROM ways
-	WHERE id = 37562
-)--,
---ele AS (
-	SELECT length_m, geom, get_elevation(ST_STARTPOINT(geom),0.0003) ele, get_elevation(ST_ENDPOINT(geom),0.0003) ele1
-	FROM x
-)
-SELECT length_m, geom, ele, lag(ele,1) OVER (
-	PARTITION BY length_m
-	ORDER BY geom 
-) 
-FROM ele
-
-
+--Convert raster to vector
 CREATE TABLE elevation AS 
 SELECT geom,val 
 FROM dem, LATERAL ST_PixelAsCentroids(rast, 1)
@@ -24,7 +8,7 @@ ALTER TABLE elevation ADD COLUMN id serial;
 ALTER TABLE elevation ADD PRIMARY KEY(id);
 CREATE INDEX ON elevation USING gist(geom);
 
-
+--Function to compute elevation for certain point
 CREATE OR REPLACE FUNCTION public.get_elevation(point geometry, snap_distance NUMERIC)
  RETURNS SETOF NUMERIC 
  LANGUAGE plpgsql
@@ -45,7 +29,23 @@ BEGIN
 END ;
 $function$;
 
-0.0009 -->> 100m
+--Distance in degrees to meters: 0.0009 -->> 100m
 
 EXPLAIN ANALYZE
 SELECT get_elevation(ST_SETSRID(ST_POINT(11.5669,48.1546),4326),0.0003)
+
+--Just some code for testing 
+WITH x as (
+	SELECT length_m::numeric, split_long_way(geom,length_m::numeric,30) AS geom 	
+	FROM ways
+	WHERE id = 37562
+)--,
+--ele AS (
+	SELECT length_m, geom, get_elevation(ST_STARTPOINT(geom),0.0003) ele, get_elevation(ST_ENDPOINT(geom),0.0003) ele1
+	FROM x
+)
+SELECT length_m, geom, ele, lag(ele,1) OVER (
+	PARTITION BY length_m
+	ORDER BY geom 
+) 
+FROM ele
