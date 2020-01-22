845efd482e16ca5b5d2f37acb5dfb373757da9ac
Alexey Grachev <alxgrh@yandex.ru>
2020-01-22 18:05:45 +0300

    add compute_ways_slope_profile4()

--- app/database/database_functions/network/test_compute_slope2.sql
+++ app/database/database_functions/network/test_compute_slope2.sql
@@ -349,4 +349,101 @@ SELECT compute_ways_slope_profile3(TRUE) AS slope_json
 SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (slope_json[1] ->> 'rs_imp') AS rs_imp, slope_json[2:] AS slope_profile
 FROM x;
 
+
+-- batch version with loop over 1000(default) ways per iteration
+
+drop function compute_ways_slope_profile4;
+CREATE OR REPLACE FUNCTION public.compute_ways_slope_profile4(subsample_size int default 1000, table_update boolean default false)
+ RETURNS SETOF jsonb[]
+ LANGUAGE plpgsql
+AS $function$
+declare 
+i integer = 0;
+BEGIN	
 	
+	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='w_split_multi4') THEN
+		CREATE TEMP TABLE IF NOT EXISTS w_split_multi4(id integer primary key, wid integer, length_m numeric, geom geometry[]); 
+		CREATE INDEX IF NOT EXISTS idx_w_split_multi4_wid ON w_split_multi4 USING btree(wid);
+	else 
+		TRUNCATE w_split_multi4;
+	end if;
+
+	-- table for result set
+	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='ways_slope_profiles') THEN
+		CREATE TEMP TABLE IF NOT EXISTS ways_slope_profiles(sp jsonb[]); 
+	else 
+		TRUNCATE ways_slope_profiles;
+	end if; 
+	
+
+	INSERT INTO w_split_multi4
+		WITH x as (
+			SELECT id as wid, length_m::numeric, split_long_way2(geom,length_m::numeric,30) AS geom 	
+			FROM ways 
+			WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container('excluded_class_id_cycling')))
+			AND length_m >= (SELECT select_from_variable_container_s('resolution_dem')::integer)
+		)
+		SELECT ROW_NUMBER() OVER() AS id, wid, length_m, geom
+		FROM x	; 	
+	
+	loop 		
+		insert into ways_slope_profiles
+		with w_split as (
+			select wid, length_m, geom 
+			FROM w_split_multi4 w
+			order by wid
+			limit subsample_size offset subsample_size*i
+		),
+		w_unnested as (
+			select row_number() over (order by w.wid) as rnum, 
+			w.wid, length_m AS original_length, 
+			point as start,lead(point) over (partition by wid) as end
+			FROM w_split w, unnest(w.geom) point
+		),
+		es AS (
+			SELECT w.rnum, w.wid, w.start, d.val e_start, st_distance(w.start::geography,w.end::geography ) as length_m
+			FROM w_unnested w, dem_vec d 
+			WHERE ST_Contains(d.geom, w.start) and w.end is not null
+		),
+		ee AS (
+			SELECT w.rnum, w.wid, w.original_length, w.end, d.val e_end
+			FROM w_unnested w, dem_vec d 
+			WHERE ST_Contains(d.geom, w.end) and w.end is not null
+		),
+		split_slope AS (
+			SELECT es.wid, es.start, ee.end, ee.original_length,es.length_m, 
+			(es.e_start-ee.e_end)/es.length_m AS slope,
+			slope_impedance((100*(es.e_start-ee.e_end)/es.length_m)::NUMERIC) AS s_imp
+			FROM es,ee
+			WHERE es.rnum = ee.rnum
+		),
+		x AS (
+			SELECT array_agg(jsonb_build_object('start',s.start,'end',s.end,'length',length_m,'slope',slope,'imp',s_imp)) slope_profile,
+			SUM((length_m/original_length)*s_imp[1])::NUMERIC s_imp,
+			SUM((length_m/original_length)*s_imp[2])::NUMERIC rs_imp,
+			wid
+			FROM split_slope s
+			group by wid
+		)
+		SELECT jsonb_build_object('s_imp',s_imp,'rs_imp',rs_imp,'id',x.wid) || slope_profile 
+		FROM x;
+		
+		EXIT WHEN NOT FOUND;
+		i = i+1;
+	end loop;
+	return query
+	select sp from ways_slope_profiles;
+	
+END;
+$function$
+;
+
+drop table temp_slopes;
+CREATE TEMP TABLE temp_slopes AS
+WITH x AS (
+SELECT compute_ways_slope_profile4(10000) AS slope_json
+)
+SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (slope_json[1] ->> 'rs_imp') AS rs_imp, slope_json[2:] AS slope_profile
+FROM x;
+
+
