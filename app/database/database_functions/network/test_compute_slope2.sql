3d23c86295c0093fe2d7ad3b5c0132e97f4a4b20
Alexey Grachev <alxgrh@yandex.ru>
2020-01-24 00:16:05 +0300

    tidy up test_compute_slope2.sql

--- app/database/database_functions/network/test_compute_slope2.sql
+++ app/database/database_functions/network/test_compute_slope2.sql
@@ -7,7 +7,7 @@ create index on dem_vec using gist(geom);
 ALTER TABLE dem_vec ADD COLUMN id serial;
 ALTER TABLE dem_vec ADD PRIMARY KEY(id);
 
-CREATE OR REPLACE FUNCTION public.compute_slope_profile2(input_id bigint, table_update boolean, table_name text)
+CREATE OR REPLACE FUNCTION public.compute_ways_slope(input_id bigint, table_update boolean, table_name text)
  RETURNS SETOF jsonb[]
  LANGUAGE plpgsql
 AS $function$
@@ -66,12 +66,12 @@ $function$
 
 
 -- Test usage
-SELECT compute_slope_profile2(100,TRUE,'ways');
+SELECT compute_ways_slope(100,TRUE,'ways');
 
 
 CREATE TEMP TABLE temp_slopes AS
 WITH x AS (
-SELECT compute_slope_profile2(id,TRUE,'ways') AS slope_json
+SELECT compute_ways_slope(id,TRUE,'ways') AS slope_json
 FROM ways 
 WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container('excluded_class_id_cycling')))
 AND length_m >= (SELECT select_from_variable_container_s('resolution_dem')::integer)
@@ -80,83 +80,9 @@ SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (sl
 FROM x;
 
 
---
--- Batch processing of the entire 'ways' table
---
-
-drop function compute_ways_slope_profile;
-CREATE OR REPLACE FUNCTION public.compute_ways_slope_profile(table_update boolean)
- RETURNS SETOF jsonb[]
- LANGUAGE plpgsql
-AS $function$
-BEGIN	
-	
-	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='w_split_multi') THEN
-		CREATE TEMP TABLE IF NOT EXISTS w_split_multi(id integer primary key, wid integer, length_m numeric, geom geometry, e_start geometry, e_end geometry); 
-		CREATE INDEX IF NOT EXISTS idx_w_split_multi_e_start ON w_split_multi USING gist(e_start);
-		CREATE INDEX IF NOT EXISTS idx_w_split_multi_e_end ON w_split_multi USING gist(e_end);
-		CREATE INDEX IF NOT EXISTS idx_w_split_multi_wid ON w_split_multi USING btree(wid);
-	else 
-		TRUNCATE w_split_multi;
-	end if;
-
-	EXECUTE format(
-		'INSERT INTO w_split_multi
-		WITH x as (
-			SELECT id as wid, length_m::numeric, split_long_way(geom,length_m::numeric,30) AS geom 	
-			FROM ways 
-			WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container(''excluded_class_id_cycling'')))
-			AND length_m >= (SELECT select_from_variable_container_s(''resolution_dem'')::integer)
-		)
-		SELECT ROW_NUMBER() OVER() AS id, wid, length_m, geom, ST_STARTPOINT(geom) e_start, ST_ENDPOINT(geom) e_end
-		FROM x'	
-	);	
-	
-	RETURN query
-	WITH es AS (
-		SELECT w.wid, w.geom, d.val e_start, ST_Length(w.geom::geography) AS length_m
-		FROM w_split_multi w, dem_vec d 
-		WHERE ST_Contains(d.geom, w.e_start)
-	),
-	ee AS (
-		SELECT w.wid, length_m AS original_length, w.geom, d.val e_end
-		FROM w_split_multi w, dem_vec d 
-		WHERE ST_Contains(d.geom, w.e_end)
-	),
-	split_slope AS (
-		SELECT es.wid, es.geom, ee.original_length,es.length_m, 
-		(es.e_start-ee.e_end)/es.length_m AS slope,
-		slope_impedance((100*(es.e_start-ee.e_end)/es.length_m)::NUMERIC) AS s_imp
-		FROM es,ee
-		WHERE es.wid = ee.wid and es.geom = ee.geom
-	),
-	x AS (
-		SELECT array_agg(jsonb_build_object('geom',geom,'length',length_m,'slope',slope,'imp',s_imp)) slope_profile,
-		SUM((length_m/original_length)*s_imp[1])::NUMERIC s_imp,
-		SUM((length_m/original_length)*s_imp[2])::NUMERIC rs_imp,
-		wid
-		FROM split_slope
-		group by wid
-	)
-	SELECT jsonb_build_object('s_imp',s_imp,'rs_imp',rs_imp,'id',x.wid) || slope_profile 
-	FROM x;
-END;
-$function$
-;
-
--- Test usage
-CREATE TEMP TABLE temp_slopes AS
-WITH x AS (
-SELECT compute_ways_slope_profile(TRUE) AS slope_json
-)
-SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (slope_json[1] ->> 'rs_imp') AS rs_imp, slope_json[2:] AS slope_profile
-FROM x;
-
-
-
 --- Use alternative split_long_way() approach
 
-CREATE OR REPLACE FUNCTION public.split_long_way2(geom geometry, length_m numeric, max_length integer)
+CREATE OR REPLACE FUNCTION public.split_long_way_to_array(geom geometry, length_m numeric, max_length integer)
  RETURNS geometry[]
  LANGUAGE plpgsql
  IMMUTABLE
@@ -184,176 +110,11 @@ BEGIN
 END;
 $function$
 
--- GROUP BY version 
-CREATE OR REPLACE FUNCTION public.compute_ways_slope_profile2(table_update boolean)
- RETURNS SETOF jsonb[]
- LANGUAGE plpgsql
-AS $function$
-BEGIN	
-	
-	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='w_split_multi2') THEN
-		CREATE TEMP TABLE IF NOT EXISTS w_split_multi2(id integer primary key, wid integer, length_m numeric, geom geometry[]); 
-		CREATE INDEX IF NOT EXISTS idx_w_split_multi2_wid ON w_split_multi2 USING btree(wid);
-	else 
-		TRUNCATE w_split_multi2;
-	end if;
-
-	EXECUTE format(
-		'INSERT INTO w_split_multi2
-		WITH x as (
-			SELECT id as wid, length_m::numeric, split_long_way2(geom,length_m::numeric,30) AS geom 	
-			FROM ways 
-			WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container(''excluded_class_id_cycling'')))
-			AND length_m >= (SELECT select_from_variable_container_s(''resolution_dem'')::integer)
-		)
-		SELECT ROW_NUMBER() OVER() AS id, wid, length_m, geom
-		FROM x	'	
-	);	
-	
-	RETURN query
-	WITH 
-	w_unnested as (
-		select row_number() over (order by w.wid) as rnum, 
-		w.wid, length_m AS original_length, 
-		point as start,lead(point) over (partition by wid) as end
-		FROM w_split_multi2 w, unnest(w.geom) point
-	),
-	es AS (
-		SELECT w.rnum, w.wid, w.start, d.val e_start, st_distance(w.start::geography,w.end::geography ) as length_m
-		FROM w_unnested w, dem_vec d 
-		WHERE ST_Contains(d.geom, w.start) and w.end is not null
-	),
-	ee AS (
-		SELECT w.rnum, w.wid, w.original_length, w.end, d.val e_end
-		FROM w_unnested w, dem_vec d 
-		WHERE ST_Contains(d.geom, w.end) and w.end is not null
-	),
-	split_slope AS (
-		SELECT es.wid, es.start, ee.end, ee.original_length,es.length_m, 
-		(es.e_start-ee.e_end)/es.length_m AS slope,
-		slope_impedance((100*(es.e_start-ee.e_end)/es.length_m)::NUMERIC) AS s_imp
-		FROM es,ee
-		WHERE es.rnum = ee.rnum
-	),
-	x AS (
-		SELECT array_agg(jsonb_build_object('start',s.start,'end',s.end,'length',length_m,'slope',slope,'imp',s_imp)) slope_profile,
-		SUM((length_m/original_length)*s_imp[1])::NUMERIC s_imp,
-		SUM((length_m/original_length)*s_imp[2])::NUMERIC rs_imp,
-		wid
-		FROM split_slope s
-		group by wid
-	)
-	SELECT jsonb_build_object('s_imp',s_imp,'rs_imp',rs_imp,'id',x.wid) || slope_profile 
-	FROM x;
-END;
-$function$
-;
-drop table temp_slopes;
-CREATE TEMP TABLE temp_slopes AS
-WITH x AS (
-SELECT compute_ways_slope_profile2(TRUE) AS slope_json
-)
-SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (slope_json[1] ->> 'rs_imp') AS rs_imp, slope_json[2:] AS slope_profile
-FROM x;
-
-	
--- cursor version
-CREATE OR REPLACE FUNCTION public.compute_ways_slope_profile3(table_update boolean)
- RETURNS SETOF jsonb[]
- LANGUAGE plpgsql
-AS $function$
-declare 
-cur refcursor;
-rec record;
-way_id integer;
-retval jsonb[];
-BEGIN	
-	
-	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='w_split_multi3') THEN
-		CREATE TEMP TABLE IF NOT EXISTS w_split_multi3(id integer primary key, wid integer, length_m numeric, geom geometry[]); 
-		CREATE INDEX IF NOT EXISTS idx_w_split_multi3_wid ON w_split_multi3 USING btree(wid);
-	else 
-		TRUNCATE w_split_multi3;
-	end if;
-
-	EXECUTE format(
-		'INSERT INTO w_split_multi3
-		WITH x as (
-			SELECT id as wid, length_m::numeric, split_long_way2(geom,length_m::numeric,30) AS geom 	
-			FROM ways 
-			WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container(''excluded_class_id_cycling'')))
-			AND length_m >= (SELECT select_from_variable_container_s(''resolution_dem'')::integer)
-		)
-		SELECT ROW_NUMBER() OVER() AS id, wid, length_m, geom
-		FROM x	'	
-	);	
-	
-	open cur for
-	select wid from w_split_multi3;
-
-	
-	loop 
-		fetch cur into way_id;
-		EXIT WHEN NOT FOUND;
-		
-	
-		WITH 
-		w_unnested as (
-			select row_number() over () as rnum, 
-			w.wid, length_m AS original_length, 
-			point as start,lead(point) over () as end
-			FROM w_split_multi3 w, unnest(w.geom) point
-			where w.wid = way_id
-		),
-		es AS (
-			SELECT w.rnum, w.wid, w.start, d.val e_start, st_distance(w.start::geography,w.end::geography ) as length_m
-			FROM w_unnested w, dem_vec d 
-			WHERE ST_Contains(d.geom, w.start) and w.end is not null
-		),
-		ee AS (
-			SELECT w.rnum, w.wid, w.original_length, w.end, d.val e_end
-			FROM w_unnested w, dem_vec d 
-			WHERE ST_Contains(d.geom, w.end) and w.end is not null
-		),
-		split_slope AS (
-			SELECT es.wid, es.start, ee.end, ee.original_length,es.length_m, 
-			(es.e_start-ee.e_end)/es.length_m AS slope,
-			slope_impedance((100*(es.e_start-ee.e_end)/es.length_m)::NUMERIC) AS s_imp
-			FROM es,ee
-			WHERE es.rnum = ee.rnum
-		),
-		x AS (
-			SELECT array_agg(jsonb_build_object('start',s.start,'end',s.end,'length',length_m,'slope',slope,'imp',s_imp)) slope_profile,
-			SUM((length_m/original_length)*s_imp[1])::NUMERIC s_imp,
-			SUM((length_m/original_length)*s_imp[2])::NUMERIC rs_imp,
-			wid
-			FROM split_slope s
-			group by wid
-		)
-		SELECT jsonb_build_object('s_imp',s_imp,'rs_imp',rs_imp,'id',x.wid) || slope_profile 
-		into retval
-		FROM x;
-		
-		return next retval;
-
-	end loop;
-END;
-$function$
-;
-
-drop table temp_slopes;
-CREATE TEMP TABLE temp_slopes AS
-WITH x AS (
-SELECT compute_ways_slope_profile3(TRUE) AS slope_json
-)
-SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (slope_json[1] ->> 'rs_imp') AS rs_imp, slope_json[2:] AS slope_profile
-FROM x;
-
 
 -- batch version with loop over 1000(default) ways per iteration
 
-drop function compute_ways_slope_profile4;
-CREATE OR REPLACE FUNCTION public.compute_ways_slope_profile4(subsample_size int default 1000, table_update boolean default false)
+drop function compute_ways_slope_bulk;
+CREATE OR REPLACE FUNCTION public.compute_ways_slope_bulk(subsample_size int default 1000, table_update boolean default false)
  RETURNS SETOF jsonb[]
  LANGUAGE plpgsql
 AS $function$
@@ -361,11 +122,11 @@ declare
 i integer = 0;
 BEGIN	
 	
-	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='w_split_multi4') THEN
-		CREATE TEMP TABLE IF NOT EXISTS w_split_multi4(id integer primary key, wid integer, length_m numeric, geom geometry[]); 
-		CREATE INDEX IF NOT EXISTS idx_w_split_multi4_wid ON w_split_multi4 USING btree(wid);
+	IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname='w_split_multi') THEN
+		CREATE TEMP TABLE IF NOT EXISTS w_split_multi(id integer primary key, wid integer, length_m numeric, geom geometry[]); 
+		CREATE INDEX IF NOT EXISTS idx_w_split_multi_wid ON w_split_multi USING btree(wid);
 	else 
-		TRUNCATE w_split_multi4;
+		TRUNCATE w_split_multi;
 	end if;
 
 	-- table for result set
@@ -376,9 +137,9 @@ BEGIN
 	end if; 
 	
 
-	INSERT INTO w_split_multi4
+	INSERT INTO w_split_multi
 		WITH x as (
-			SELECT id as wid, length_m::numeric, split_long_way2(geom,length_m::numeric,30) AS geom 	
+			SELECT id as wid, length_m::numeric, split_long_way_to_array(geom,length_m::numeric,30) AS geom 	
 			FROM ways 
 			WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container('excluded_class_id_cycling')))
 			AND length_m >= (SELECT select_from_variable_container_s('resolution_dem')::integer)
@@ -390,7 +151,7 @@ BEGIN
 		insert into ways_slope_profiles
 		with w_split as (
 			select wid, length_m, geom 
-			FROM w_split_multi4 w
+			FROM w_split_multi w
 			order by wid
 			limit subsample_size offset subsample_size*i
 		),
@@ -441,7 +202,7 @@ $function$
 drop table temp_slopes;
 CREATE TEMP TABLE temp_slopes AS
 WITH x AS (
-SELECT compute_ways_slope_profile4(10000) AS slope_json
+SELECT compute_ways_slope_bulk(10000) AS slope_json
 )
 SELECT (slope_json[1] ->> 'id') AS id, (slope_json[1] ->> 's_imp') AS s_imp, (slope_json[1] ->> 'rs_imp') AS rs_imp, slope_json[2:] AS slope_profile
 FROM x;
