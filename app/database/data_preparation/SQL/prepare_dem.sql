ec8377ec1da7cf9ba6f881bfe3348db9cb3ca8ca
Elias Pajares <elias.pajares@tum.de>
2020-02-08 16:37:59 +0100

    added slope calculation to setup

---
+++ app/database/data_preparation/SQL/prepare_dem.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS dem_vec;
+CREATE TABLE dem_vec as 
+SELECT rid, dp.geom , dp.val FROM dem, ST_DumpAsPolygons(rast) dp;
+
+CREATE index ON dem_vec USING gist(geom);
+ALTER TABLE dem_vec ADD COLUMN id serial;
+ALTER TABLE dem_vec ADD PRIMARY KEY(id);
