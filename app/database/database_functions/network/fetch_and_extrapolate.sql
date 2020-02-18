55c87582a20d4cc788c40835c285f99dbed6e399
Elias Pajares <elias.pajares@tum.de>
2020-02-18 17:37:51 +0100

    adjusted to meet new requirements

--- app/database/database_functions/network/fetch_and_extrapolate.sql
+++ app/database/database_functions/network/fetch_and_extrapolate.sql
@@ -1,5 +1,5 @@
 DROP FUNCTION IF EXISTS fetch_ways_routing;
-CREATE OR REPLACE FUNCTION public.fetch_ways_routing(buffer_geom text, modus_input integer, userid_input integer, routing_profile text,fetch_type text default 'multiple', vid integer default 1)
+CREATE OR REPLACE FUNCTION public.fetch_ways_routing(buffer_geom text, modus_input integer, userid_input integer, routing_profile text)
  RETURNS SETOF type_fetch_ways_routing
  LANGUAGE plpgsql
 AS $function$
@@ -8,56 +8,52 @@ DECLARE
 	sql_ways_ids text := '';
 	sql_userid text := '';
 	sql_routing_profile text := '';
-	sql_geom text;
-	sql_id text :='';
+	sql_geom text := format(' AND geom && ST_GeomFromText(''%s'')',buffer_geom);
 	table_name text := 'ways';
-	excluded_class_id integer[];
-	filter_categories text[];
+	excluded_class_id text;
+	filter_categories text;
 	category text := jsonb_build_object('walking','foot','cycling','bicycle') ->>  split_part(routing_profile,'_',1);
 	sql_cost text := jsonb_build_object(
 	'cycling','length_m*(1+COALESCE(s_imp,0)+COALESCE(impedance_surface,0))::float as cost,length_m*(1+COALESCE(rs_imp,0)+COALESCE(impedance_surface,0))::float as reverse_cost',
 	'walking', 'length_m as cost, length_m as reverse_cost'
 	) ->> split_part(routing_profile,'_',1);
 	userid_vertex integer;
+	sql_select_ways text;
 	
 BEGIN 
 
-	excluded_class_id = select_from_variable_container('excluded_class_id_' || split_part(routing_profile,'_',1));
-  	filter_categories = select_from_variable_container('categories_no_' || category);
+	excluded_class_id = (select_from_variable_container('excluded_class_id_' || split_part(routing_profile,'_',1)))::text;
+  	filter_categories = (select_from_variable_container('categories_no_' || category))::text;
 
 	IF modus_input <> 1 THEN 
 		table_name = 'ways_userinput';
 		excluded_ways_id = ids_modified_features(userid_input,'ways');
 
-		sql_userid = ' AND userid IS NULL OR userid='||userid_input;
-		sql_ways_ids = ' AND NOT id::int4 = any('''|| excluded_ways_id ||''') ';
+		sql_userid = ' AND( userid IS NULL OR userid='||userid_input||')';
+		sql_ways_ids = ' AND NOT id::int4 = any('''|| ids_modified_features(userid_input,'ways')::text ||''') ';
 	END IF;
 	
+	raise notice '%',sql_ways_ids;
+
 	IF  routing_profile = 'walking_safe_night' THEN
 		sql_routing_profile = 'AND (lit_classified = ''yes'' OR lit_classified = ''unclassified'')';
 	ELSEIF routing_profile = 'walking_wheelchair' THEN
 		sql_routing_profile = 'AND ((wheelchair_classified = ''yes'') OR wheelchair_classified = ''limited''
 		OR wheelchair_classified = ''unclassified'')';
 	END IF;
-	
-	IF fetch_type = 'single' THEN
-		sql_geom = '';
-		sql_id = format('AND (source = %s OR target = %s)',vid,vid);
-	ELSE 
-		sql_geom = format(' AND geom && ST_GeomFromText(''%s'')',buffer_geom);
-	END IF;
+	RAISE NOTICE '%',category;
 
-
-	RETURN query EXECUTE '
-		SELECT  id::integer, source, target,'||sql_cost||',slope_profile,death_end,geom 
+	sql_select_ways = 
+		'SELECT id::integer, source, target,'||sql_cost||',slope_profile,death_end,geom 
 		FROM '||quote_ident(table_name)||
-		' WHERE class_id NOT IN(SELECT UNNEST($1))
-    	AND (foot NOT IN(SELECT UNNEST($2)) OR foot IS NULL)
-		'||sql_geom||sql_userid||sql_ways_ids||sql_routing_profile||sql_id
-	USING excluded_class_id, filter_categories;
+		' WHERE NOT class_id = ANY('''||excluded_class_id||''')
+    	AND (NOT '||quote_ident(category)||' = ANY('''||filter_categories||''') 
+		OR '||quote_ident(category)||' IS NULL)
+		'||sql_geom||sql_userid||sql_ways_ids||sql_routing_profile;
+
+	return query execute sql_select_ways;
 END;
 $function$;
-
 
 /*select fetch_ways_routing(ST_ASTEXT(ST_BUFFER(ST_POINT(11.543274,48.195524),0.001)),1.33,1,1,'walking_standard');
 */
@@ -109,7 +105,7 @@ BEGIN
 		WHERE e.edge IS NULL;
 	
 	END LOOP;
-		
+	
 	INSERT INTO edges	
 	SELECT w.id AS edge,v.node,CASE WHEN w.SOURCE = v.node THEN v.cost + w.cost ELSE v.cost + w.reverse_cost END AS cost, 
 	w.geom,CASE WHEN w.source = v.node THEN ST_ENDPOINT(w.geom) ELSE ST_STARTPOINT(w.geom) END AS v_geom,objectid_input
