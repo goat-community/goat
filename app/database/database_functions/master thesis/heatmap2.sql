eadaea61651acc7ffdeb8c3414f41df4018927db
Moritz Schott <moritz-schott@t-online.de>
2020-03-08 19:57:13 +0100

    Heatmap2 should consider deleted original pois and modified original pois.

--- app/database/database_functions/master thesis/heatmap2.sql
+++ app/database/database_functions/master thesis/heatmap2.sql
@@ -18,16 +18,21 @@ WITH amenity_sensitivity_weight AS
 (
 	SELECT jsonb_object_keys(x.jsons::jsonb) AS amenity, ((x.jsons->>jsonb_object_keys(x.jsons::jsonb)::TEXT)::jsonb->>'sensitivity')::NUMERIC AS sensitivity, ((x.jsons->>jsonb_object_keys(x.jsons::jsonb)::TEXT)::jsonb->>'weight')::NUMERIC AS weight
 	FROM (
-		SELECT value AS jsons FROM jsonb_array_elements(_dict::jsonB)
+		SELECT value AS jsons FROM jsonb_array_elements(_dict::jsonb)
 	)
 	AS x
 ),
 
 user_pois AS
 (
-	SELECT cell_id, object_id, cost
-	FROM reached_points
-	WHERE user_id = 1 OR user_id = userid
+	SELECT * FROM
+	(
+		SELECT cell_id, object_id, cost
+		FROM reached_points LEFT JOIN (SELECT UNNEST(deleted_feature_ids) AS deleted_feature_ids FROM user_data WHERE user_data.userid = heatmap2.userid AND layer_name = 'pois') AS hidden_features ON hidden_features.deleted_feature_ids = reached_points.object_id
+		WHERE (user_id IS NULL  AND hidden_features.deleted_feature_ids IS NULL) OR user_id = userid
+	) AS user_pois_deleted LEFT JOIN pois_modified ON object_id = original_id
+	WHERE original_id IS NULL
+	-- last join could be avoided, if modified original_pois were marked as 'deleted' in the user_data table.
 ),
 
 amenity_ids AS
@@ -67,7 +72,7 @@ $function$
 
 
 
-SELECT heatmap2(343454, '[{"primary_school":{"sensitivity":-0.004,"weight":1}},{"kindergarten":{"sensitivity":-0.003,"weight":1}},{"secondary_school":{"sensitivity":-0.003,"weight":1}},{"library":{"sensitivity":-0.003,"weight":1}}]')
+--SELECT heatmap2(343454, '[{"kindergarten":{"sensitivity":-0.003,"weight":1}},{"primary_school":{"sensitivity":-0.003,"weight":1}},{"secondary_school":{"sensitivity":-0.003,"weight":1}},{"library":{"sensitivity":-0.003,"weight":1}},{"bar":{"sensitivity":-0.003,"weight":1}},{"biergarten":{"sensitivity":-0.003,"weight":1}},{"cafe":{"sensitivity":-0.003,"weight":1}},{"pub":{"sensitivity":-0.003,"weight":1}},{"fast_food":{"sensitivity":-0.003,"weight":1}},{"ice_cream":{"sensitivity":-0.003,"weight":1}},{"restaurant":{"sensitivity":-0.003,"weight":1}},{"nightclub":{"sensitivity":-0.003,"weight":1}},{"bicycle_rental":{"sensitivity":-0.003,"weight":1}},{"car_sharing":{"sensitivity":-0.003,"weight":1}},{"charging_station":{"sensitivity":-0.003,"weight":1}},{"bus_stop":{"sensitivity":-0.003,"weight":1}},{"tram_stop":{"sensitivity":-0.003,"weight":1}},{"subway_entrance":{"sensitivity":-0.003,"weight":1}},{"rail_station":{"sensitivity":-0.003,"weight":1}},{"taxi":{"sensitivity":-0.003,"weight":1}},{"hairdresser":{"sensitivity":-0.003,"weight":1}},{"atm":{"sensitivity":-0.003,"weight":1}},{"bank":{"sensitivity":-0.003,"weight":1}},{"dentist":{"sensitivity":-0.003,"weight":1}},{"doctors":{"sensitivity":-0.003,"weight":1}},{"pharmacy":{"sensitivity":-0.003,"weight":1}},{"post_box":{"sensitivity":-0.003,"weight":1}},{"fuel":{"sensitivity":-0.003,"weight":1}},{"recycling":{"sensitivity":-0.003,"weight":1}},{"bakery":{"sensitivity":-0.003,"weight":1}},{"butcher":{"sensitivity":-0.003,"weight":1}},{"clothes":{"sensitivity":-0.003,"weight":1}},{"convenience":{"sensitivity":-0.003,"weight":1}},{"greengrocer":{"sensitivity":-0.003,"weight":1}},{"kiosk":{"sensitivity":-0.003,"weight":1}},{"mall":{"sensitivity":-0.003,"weight":1}},{"shoes":{"sensitivity":-0.003,"weight":1}},{"supermarket":{"sensitivity":-0.003,"weight":1}},{"discount_supermarket":{"sensitivity":-0.003,"weight":1}},{"international_supermarket":{"sensitivity":-0.003,"weight":1}},{"hypermarket":{"sensitivity":-0.003,"weight":1}},{"chemist":{"sensitivity":-0.003,"weight":1}},{"organic":{"sensitivity":-0.003,"weight":1}},{"marketplace":{"sensitivity":-0.003,"weight":1}},{"cinema":{"sensitivity":-0.003,"weight":1}},{"theatre":{"sensitivity":-0.003,"weight":1}},{"museum":{"sensitivity":-0.003,"weight":1}},{"hotel":{"sensitivity":-0.003,"weight":1}},{"hostel":{"sensitivity":-0.003,"weight":1}},{"guest_house":{"sensitivity":-0.003,"weight":1}},{"gallery":{"sensitivity":-0.003,"weight":1}}]')
 
 
 
