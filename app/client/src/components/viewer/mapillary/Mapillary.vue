aea6ba17058c2f3e840703aad19725feed8c3e24
Majk Shkurti <majkshkurti94@gmail.com>
2020-03-10 15:04:01 +0100

    [WIP]: Mapillary implementation

--- app/client/src/components/viewer/mapillary/Mapillary.vue
+++ app/client/src/components/viewer/mapillary/Mapillary.vue
@@ -14,15 +14,17 @@ export default {
     };
   },
   mounted() {
-    const clientId = "QjI1NnU0aG5FZFZISE56U3R5aWN4Zzo3NTM1MjI5MmRjODZlMzc0";
-    const key = "w3p5M6s6J3HoX1ZREqXZIw";
+    const clientId = "MkJKbDA0bnZuZlcxeTJHTmFqN3g1dzo1YTM0NjRkM2EyZGU5MzBh";
+    const key = "rrKZdmgdvup_KYJKTESq0Q";
 
     // var urlStartImage = `https://a.mapillary.com/v3/images/${key}?client_id=${clientId}`;
     // var urlSequence = `https://a.mapillary.com/v3/sequences/JMOBN38RSF2h6dpuuiBz6w?client_id=${clientId}`;
 
     // var imagesUrl = `https://a.mapillary.com/v3/images?client_id=${clientId}&sequence_keys=JMOBN38RSF2h6dpuuiBz6w`;
 
-    this.mapillary = new Viewer("mapillary-container", clientId, key, null);
+    this.mapillary = new Viewer("mapillary-container", clientId, key, {
+      component: { cover: false }
+    });
 
     window.addEventListener("resize", () => {
       this.mapillary.resize();
