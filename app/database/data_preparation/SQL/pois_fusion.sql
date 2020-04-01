fe8e02c46fa6f9558a02874a628d03520e23b280
Elias Pajares <elias.pajares@tum.de>
2020-04-01 12:14:30 +0200

    improved clean duplicated pois

--- app/database/data_preparation/SQL/pois_fusion.sql
+++ app/database/data_preparation/SQL/pois_fusion.sql
@@ -6,23 +6,25 @@ DECLARE
 	radius text := 'duplicated_lookup_radius';
 BEGIN
 	DROP TABLE IF EXISTS pois_duplicated;
-	EXECUTE 'CREATE TEMP TABLE pois_duplicated (LIKE '|| quote_ident(table_name) ||' INCLUDING ALL)';
-	ALTER TABLE pois_duplicated ADD distance real;
+	
+	CREATE TEMP TABLE pois_duplicated (id serial, gid integer, distance NUMERIC);
+	ALTER TABLE pois_duplicated ADD PRIMARY KEY(id);
+	
 	EXECUTE 'INSERT INTO pois_duplicated 
-			SELECT o.*, ST_Distance(o.geom,p.geom) AS distance
-			FROM '|| quote_ident(table_name) ||' o
-			JOIN '|| quote_ident(table_name) ||' p
-			ON ST_DWithin( o.geom::geography, p.geom::geography, select_from_variable_container_s('|| quote_literal('duplicated_lookup_radius') ||')::float)
-			AND NOT ST_DWithin(o.geom, p.geom, 0)';
-	DELETE FROM
-			pois_duplicated a
-			USING pois_duplicated b
-		WHERE
-			a.gid > b.gid
-			AND a.distance = b.distance;
+			SELECT min(gid), distance 
+			FROM (
+				SELECT o.gid, ST_Distance(o.geom,p.geom) AS distance
+				FROM '|| quote_ident(table_name) ||' o
+				JOIN '|| quote_ident(table_name) ||' p
+				ON ST_DWithin( o.geom::geography, p.geom::geography, select_from_variable_container_s('|| quote_literal('duplicated_lookup_radius') ||')::float)
+				AND NOT ST_DWithin(o.geom, p.geom, 0)
+			) x
+			GROUP BY distance';
+
 	EXECUTE 'DELETE FROM '||quote_ident(table_name)||' WHERE gid = ANY (SELECT gid FROM pois_duplicated)';
 END;
 $function$;
+
 
 CREATE OR REPLACE FUNCTION pois_fusion()
 	RETURNS void 
