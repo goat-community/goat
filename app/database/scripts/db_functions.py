1e28e90b184fb5b88bc33105d6670542754bf5bc
Elias Pajares <elias.pajares@tum.de>
2020-06-02 18:48:29 +0200

    adjusted to new class

--- app/database/scripts/db_functions.py
+++ app/database/scripts/db_functions.py
@@ -84,8 +84,8 @@ def create_variable_container():
 def update_functions():
     from pathlib import Path
     import glob
-    db_name,user,host = ReadYAML().db_credentials()[:3]
-    db = DB_connection(db_name,user,host)
+    db_name,user,host,port,password = ReadYAML().db_credentials()
+    db = DB_connection(db_name,user,host,port,password)
     db.execute_script_psql('/opt/data_preparation/SQL/types.sql')
     for p in ['/opt/database_functions/other','/opt/database_functions/network','/opt/database_functions/routing','/opt/database_functions/heatmap']:
         for file in Path(p).glob('*.sql'):
