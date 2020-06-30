0456c24d8bb34603e99d45714d2905169735c071
Rafael <rafael.nieto@tum.de>
2020-06-30 14:31:10 +0200

    Function to desintegrate polygons into pois

--- app/database/database_functions/data_preparation/pois_dissagregate_polygons.sql
+++ app/database/database_functions/data_preparation/pois_dissagregate_polygons.sql
@@ -1,134 +1,91 @@
-CREATE OR REPLACE FUNCTION pois_dissagregate_polygons(subset text)
+------------------------------------------------
+----- Pois dissagregate polygons function ------
+-- Calls a subset from planet_osm_polygons and look for doors or intersections with pedestrian or service roads, in case of no gates or intersections, It will return the centroid of the polygon.
+-- This new points will be assigned to a specific amenity (input) and injected into pois database.
+-- Input values
+-- 	subset	: Name of the subset to be used
+--  myamenity: category of the amenity to be assigned to these new points
+
+CREATE OR REPLACE FUNCTION pois_dissagregate_polygons(subset TEXT, myamenity TEXT)
 	RETURNS void
 	LANGUAGE plpgsql
 AS $function$
-DECLARE
-	gate varchar :='gate';
-	NO varchar :='no';
-	private varchar := 'private';
-	entrance varchar := 'entrance';
-	main varchar := 'main';
-	yes varchar := 'yes';
-	entrance_array varchar[] := ARRAY['main','yes'];
-	
 BEGIN 
-	-- 01. Get doors and entrance gates
+	-- Create dummy subset to avoid dynamic sql 
+	DROP TABLE IF EXISTS polygon_subset;
+	CREATE TEMP TABLE polygon_subset (LIKE planet_osm_polygon INCLUDING all);
+	EXECUTE 'INSERT INTO polygon_subset
+	SELECT * FROM '||quote_ident(subset);
+	
+	EXECUTE 'UPDATE polygon_subset
+	SET amenity = '||quote_literal(myamenity); 
+	-- Get doors and entrance gates
+		
 	DROP TABLE IF EXISTS access_points;
-	EXECUTE 'CREATE TABLE access_points (LIKE '||quote_ident(subset)||' INCLUDING ALL)';
+	CREATE TABLE access_points (LIKE planet_osm_polygon INCLUDING all);	
+	ALTER TABLE access_points ADD COLUMN gates geometry;
 	
-	ALTER TABLE access_points
-	ADD COLUMN gates geometry;
-	
-	EXECUTE ' INSERT INTO access_points
-	SELECT pol.*, poi.way as point FROM 
-	(SELECT * FROM planet_osm_point WHERE (barrier = '||quote_literal(gate)||' AND (NOT ("access" = '||quote_literal(no)||' OR "access" ='||quote_literal(private)||' ) OR "access" IS NULL))
-	OR (tags->'||quote_literal(entrance)||' = ANY(ARRAY['||quote_literal(main)||','||quote_literal(yes)||'])) ) poi
-	JOIN '|| quote_ident(subset) ||' AS pol
-	ON ST_intersects(poi.way, pol.way)';
-	ALTER TABLE access_points 
-	DROP COLUMN way;
-	
-	-- 02. Identify polygons without previous matches
+	INSERT INTO access_points
+	SELECT pol.*, poi.way as point FROM
+	(SELECT * FROM planet_osm_point WHERE (barrier = 'gate' AND (NOT ("access" = 'no' OR "access" ='private' ) OR "access" IS NULL))
+	OR (tags->'entrance' = ANY(ARRAY['main','yes']))) poi
+	JOIN polygon_subset AS pol
+	ON ST_intersects(poi.way, pol.way);
+
+	-- Count the no. of doors and gates per polygon 
 	
 	DROP TABLE IF EXISTS no_of_doors;
 	CREATE TABLE no_of_doors(osm_id int, way geometry, no_doors int);
 	
 	INSERT INTO no_of_doors
-	SELECT test.osm_id, test.way, count(access_points.osm_id) AS no_doors FROM 
-	test
-	LEFT JOIN access_points
-	ON test.osm_id = access_points.osm_id GROUP BY test.osm_id, test.way;
-
-	-- 03. Calculate intersection points between polygons without access and road network (footway, path, service)
+	SELECT t.osm_id, t.way, count(ap.osm_id) AS no_doors FROM polygon_subset t
+	LEFT JOIN access_points ap
+	ON t.osm_id = ap.osm_id GROUP BY t.osm_id, t.way;
+		
+	-- Calculate intersection points between polygons with access_points = 0 and road network (footway, path, service)
 	
-	DROP TABLE IF EXISTS access_intersections;
-	CREATE TABLE access_intersections (LIKE planet_osm_polygon INCLUDING ALL);
-	
-	ALTER TABLE access_intersections
-	ADD COLUMN access_point geometry;
-	
-	INSERT INTO access_intersections
+	INSERT INTO access_points
 	SELECT pol.*, (ST_Dump(ST_Intersection(l.way, ST_boundary(pol.way)))).geom AS geom 
 	FROM 
 	(SELECT * FROM planet_osm_line WHERE highway = ANY (ARRAY ['footway','path','service','services'])) AS l, 
-	(SELECT t.* FROM test t
+	(SELECT t.* FROM polygon_subset t
 		LEFT JOIN no_of_doors n
 		ON t.osm_id = n.osm_id
 		WHERE no_doors = 0) AS pol
 	WHERE
 	st_intersects(l.way, pol.way) ;
 
-	
+	-- Select polygons without gates and intersections
 
+	DROP TABLE IF EXISTS no_of_intersections;
+	CREATE TABLE no_of_intersections(LIKE no_of_doors INCLUDING all);
+	ALTER TABLE no_of_intersections ADD COLUMN no_of_intersections int;
+
+	INSERT INTO no_of_intersections		
+	SELECT d.*, count(ai.access_point) FROM no_of_doors d
+	LEFT JOIN access_intersections ai 
+	ON d.osm_id = ai.osm_id
+	GROUP BY d.osm_id, d.way, d.no_doors;
+
+	-- Injection into pois of access_points
+	INSERT INTO pois
+	SELECT osm_id, 'polygon dissagregated' AS origin_geometry, ACCESS, "addr:housenumber" AS housenumber, amenity AS amenity,
+	tags -> 'origin' AS origin, tags -> 'organic' AS organic, denomination,brand,name,
+	operator,public_transport,railway,religion,tags -> 'opening_hours' as opening_hours, ref, tags||hstore('sport', sport)||hstore('leisure', leisure)  AS tags, ST_centroid(way) as geom,
+	tags -> 'wheelchair' as wheelchair
+	FROM access_points;	
+
+	-- Injection into pois of centroids
+	
+	INSERT INTO pois
+	SELECT t.osm_id, 'polygon' AS origin_geometry, ACCESS, "addr:housenumber" AS housenumber, amenity AS amenity,
+	tags -> 'origin' AS origin, tags -> 'organic' AS organic, denomination,brand,name,
+	operator,public_transport,railway,religion,tags -> 'opening_hours' as opening_hours, ref, tags||hstore('sport', sport)||hstore('leisure', leisure)  AS tags, ST_centroid(t.way) as geom,
+	tags -> 'wheelchair' as wheelchair
+	FROM polygon_subset t, no_of_intersections ni WHERE (ni.no_doors + ni.no_of_intersections ) = 0 AND t.osm_id = ni.osm_id;
 
 END;
 $function$
 
-SELECT pois_dissagregate_polygons('test')
-SELECT * FROM access_intersections WHERE name IS null;
-
-DROP TABLE IF EXISTS access_intersections;
-
-SELECT pol.*, (ST_Dump(ST_Intersection(l.way, ST_boundary(pol.way)))).geom AS geom 
-FROM 
-(SELECT * FROM planet_osm_line WHERE highway = ANY (ARRAY ['footway','path','service','services'])) AS l, 
-(	SELECT t.* FROM test t
-	LEFT JOIN no_of_doors n
-	ON t.osm_id = n.osm_id
-	WHERE no_doors = 0) AS pol
-WHERE
-st_intersects(l.way, pol.way) ;
-
-SELECT * FROM access_points;
-
-
-
-
-
-
-
-
-
-
-	SELECT t.* FROM test t
-	LEFT JOIN no_of_doors n
-	ON t.osm_id = n.osm_id
-	WHERE no_doors = 0;
-
-
-
-
-SELECT osm_id, count(osm_id) FROM access_points GROUP BY test.osm_id, test.way;
-SELECT pois_dissagregate_polygons('test')
-SELECT * FROM access_points;
-
-SELECT * FROM test;
-SELECT * FROM no_of_doors;
-
-DROP TABLE IF EXISTS test;
-CREATE TABLE test (LIKE planet_osm_polygon INCLUDING all);
-INSERT INTO test
-SELECT * FROM planet_osm_polygon WHERE (leisure = ANY(ARRAY['sports_centre','water_park']) OR name LIKE '%Bezirkssportanlage%') AND building IS NULL;
-
-
-
--- Select doors and entrances
-SELECT pop.* FROM 
-
-SELECT * FROM 
-(SELECT * FROM planet_osm_point WHERE (barrier = 'gate' AND (NOT ("access" = 'no' OR "access" ='private' ) OR "access" IS NULL)) OR (tags->'entrance' = ANY (ARRAY['main','yes']))) poi
-JOIN test AS pol 
-ON st_intersects(poi.way,st_boundary(pol.way))
-
-SELECT ST_Buffer(way::geography, 1) INTO geotest FROM planet_osm_point WHERE tags->'entrance' = 'main'
-DROP TABLE geotest;
-
-SELECT * FROM test;
-SELECT * FROM planet_osm_point WHERE tags->'entrance' ='main'
-
-DROP TABLE IF EXISTS doors_field;
-SELECT pol.osm_id, count(poi.osm_id), 'Sport complex with gates' AS "case" INTO doors_field
-FROM (SELECT * FROM planet_osm_point pop WHERE barrier = 'gate' AND NOT ("access" = 'no' OR "access" ='private' ) OR "access" IS null) poi
-JOIN sport_centers AS pol 
-ON st_intersects (poi.way, ST_Boundary(pol.way))
-GROUP BY pol.osm_id, pol.way;
+-- Example, test is the subset that has the same structure than planet_osm_polygon
+--SELECT pois_dissagregate_polygons('test','mynewamenity')
