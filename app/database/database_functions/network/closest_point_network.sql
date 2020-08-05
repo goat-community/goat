b0e2a0d30f464b7ff818e2c0a9fb49181ce10acd
Elias Pajares <elias.pajares@tum.de>
2020-08-05 14:00:05 +0200

    added new closest point function

--- app/database/database_functions/network/closest_point_network.sql
+++ app/database/database_functions/network/closest_point_network.sql
@@ -21,3 +21,22 @@ $function$;
 
 --SELECT * FROM closest_point_network(11.239410000,48.158710000)
 
+
+DROP FUNCTION IF EXISTS closest_point_network_geom;
+CREATE OR REPLACE FUNCTION closest_point_network_geom(point geometry)
+RETURNS TABLE(closest_point geometry, fraction float, vid integer, wid integer)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN 
+	RETURN query
+	SELECT ST_CLOSESTPOINT(geom,point) AS closest_point,ST_LineLocatePoint(geom,point) AS fraction, 
+	999999999,w.id AS wid
+	FROM temp_fetched_ways w
+	WHERE St_buffer(point,0.0018) && geom
+	ORDER BY ST_CLOSESTPOINT(geom,point) <-> point
+  	LIMIT 1;
+
+END 
+$function$;
+
+
