ceab6e637292097353700a1c4cb644420c3b4aa5
Rafael <rafael.nieto@tum.de>
2020-08-14 17:15:22 +0200

    Adjustment network depuration for Bogotá

--- app/database/data_preparation/SQL/network_preparation.sql
+++ app/database/data_preparation/SQL/network_preparation.sql
@@ -391,6 +391,9 @@ WHERE v.id = w.id;
 
 UPDATE ways w
 SET foot = 'no'  FROM ( SELECT osm_id FROM planet_osm_line WHERE highway = 'service' AND (tags->'psv' IS NOT NULL OR tags->'bus' = 'yes') ) x WHERE w.osm_id = x.osm_id;
+
+-- Mark underground cycle lanes as foot = 'no' this is for the specific case of BOG
+
 --
 
 
