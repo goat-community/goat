1e08f5fad58fc66b357dd63ff46fb6114166eb0d
Elias Pajares <elias.pajares@tum.de>
2020-08-24 07:23:27 +0200

    bug fix

--- app/database/database_functions/heatmap/reached_pois_derive_costs.sql
+++ app/database/database_functions/heatmap/reached_pois_derive_costs.sql
@@ -54,12 +54,11 @@ BEGIN
 	    SELECT p.gid, p.amenity, p.name, p.gridids, c.arr_true_cost arr_cost, c.accessibility_indices::integer[],userid_input userid, scenario_id_input scenario_id 
 	    FROM p, compute_accessibility_index(fraction,start_cost,end_cost,sensitivities) c;
 	   	
-	   	CREATE INDEX ON reached_pois_partial(gid);
-	   
+	    CREATE INDEX ON reached_pois_partial(gid);
 	   	WITH pois_to_update AS 
 		(
-			SELECT p.gid, r.gridids, r.gridids # p.gridids[1] arr_position, 
-			r.gridids - p.gridids[1] + p.gridids[1] new_gridids, p.gridids[1], r.arr_cost, p.arr_cost[1] new_cost, r.accessibility_indices a1, p.accessibility_indices a2
+			SELECT p.gid, r.gridids, r.gridids # p.gridids[1] arr_position, p.gridids[1] p_gridid, 
+			r.arr_cost, p.arr_cost[1] new_cost, r.accessibility_indices a1, p.accessibility_indices a2
 			FROM reached_pois_heatmap r, reached_pois_partial p 
 			WHERE r.gid = p.gid 
 			AND r.gridids && p.gridids
@@ -69,7 +68,7 @@ BEGIN
 		),
 		updated_arrays AS 
 		(
-			SELECT x.gid, new_gridids AS gridids, arr_cost[1:(-1+arr_position)] + new_cost || arr_cost[arr_position+1:] AS arr_cost, 
+			SELECT x.gid, gridids[1:(-1+arr_position)] + p_gridid || gridids[arr_position+1:]  AS gridids, arr_cost[1:(-1+arr_position)] + new_cost || arr_cost[arr_position+1:] AS arr_cost, 
 			update_accessibility_array(x.a1, x.a2, arr_position) AS accessibility_indices, userid_input userid, scenario_id_input scenario_id
 			FROM pois_to_update x
 		)
@@ -79,7 +78,7 @@ BEGIN
 		WHERE r.gid = u.gid
 		AND r.userid = u.userid
 		AND r.scenario_id = u.scenario_id;
-       	
+	
 		WITH grids_to_add AS 
 		(
 			SELECT p.gid, r.gridids + p.gridids[1] gridids, r.arr_cost + p.arr_cost[1] arr_cost,
