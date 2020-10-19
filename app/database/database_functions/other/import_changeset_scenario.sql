b52f4ae1688fbdef008d066fe0cd96912de55e93
Elias Pajares <elias.pajares@tum.de>
2020-10-19 14:08:21 +0200

    added gid for all layers

--- app/database/database_functions/other/import_changeset_scenario.sql
+++ app/database/database_functions/other/import_changeset_scenario.sql
@@ -49,8 +49,10 @@ BEGIN
 	
 	EXECUTE sql_import_table;
 
+	/*#######################################################################*/
+	/*Inject POIs*/
+	/*#######################################################################*/
 	IF layer = 'pois' THEN 
-		/*Inject POIs*/
 		UPDATE import_pois i 
 		SET new_original_id = u.new_original_id 
 		FROM (
@@ -58,60 +60,121 @@ BEGIN
 			FROM pois_userinput p, import_pois i  
 			WHERE p.amenity = i.amenity 
 			AND p.name = i.name 
-			AND i.edit_type IN ('old_modified', 'deleted', 'new_modified') 
+			AND i.edit_type IN ('old_modified', 'deleted') 
 			AND ST_EQUALS(ST_ASTEXT(ST_QuantizeCoordinates(p.geom,6)),ST_ASTEXT(ST_QuantizeCoordinates(i.geom,6)))
 			AND p.geom && i.geom
 		) u
 		WHERE i.original_id = u.original_id
 		AND i.edit_type IN ('old_modified', 'deleted', 'new_modified'); 
 		
-		INSERT INTO pois_modified(name,amenity,opening_hours,wheelchair,geom,scenario_id, original_id) 
-		SELECT name,amenity,opening_hours,wheelchair,geom,scenario_id_input, new_original_id::integer  
-		FROM import_pois 
-		WHERE edit_type IN ('new','new_modified');
-	
-		SELECT create_geojson(array_agg(to_jsonb(x) - 'geom'), array_agg(geom))
-		INTO pois_json
-		FROM 
+		DROP TABLE IF EXISTS import_pois_gid;
+		CREATE TEMP TABLE import_pois_gid AS 
+		WITH new_with_gid AS 
 		(
-			SELECT name, amenity, opening_hours, wheelchair, original_id, edit_type, geom, scenario_id, 
-			CASE WHEN COALESCE(original_id::integer,0) = COALESCE(new_original_id::integer,0) THEN 'successful' ELSE 'not_successful' END AS upload_status  
-			FROM import_pois 
-		) x;
+			INSERT INTO pois_modified(name,amenity,opening_hours,wheelchair,geom,scenario_id, original_id) 
+			SELECT i.name,i.amenity,i.opening_hours,i.wheelchair,i.geom,i.scenario_id, i.new_original_id::integer  
+			FROM import_pois i
+			WHERE edit_type = 'new'
+			RETURNING gid,name,amenity,opening_hours,wheelchair,geom,scenario_id,original_id
+		), 
+		new_modified_with_gid AS 
+		(
+			INSERT INTO pois_modified(name,amenity,opening_hours,wheelchair,geom,scenario_id, original_id) 
+			SELECT i.name,i.amenity,i.opening_hours,i.wheelchair,i.geom,i.scenario_id, i.new_original_id::integer  
+			FROM import_pois i		
+			WHERE edit_type = 'new_modified' 
+			AND new_original_id IS NOT NULL
+			RETURNING gid,name,amenity,opening_hours,wheelchair,geom,scenario_id,original_id
+		)
+		SELECT gid,name,amenity,opening_hours,wheelchair,'new' AS edit_type,geom,scenario_id,original_id,'successful' AS upload_status 
+		FROM new_with_gid
+		UNION ALL
+		SELECT gid,name,amenity,opening_hours,wheelchair,'new_modified' AS edit_type,geom,scenario_id,original_id,'successful' AS upload_status 
+		FROM new_modified_with_gid
+		UNION ALL 
+		SELECT new_original_id::integer AS gid, name,amenity,opening_hours,wheelchair,edit_type,geom,scenario_id::integer,new_original_id::integer, 
+		CASE WHEN new_original_id IS NOT NULL THEN 'successful' ELSE 'not_successful' END AS upload_status 
+		FROM import_pois
+		WHERE edit_type = 'deleted'
+		UNION ALL 
+		SELECT new_original_id::integer AS gid, name,amenity,opening_hours,wheelchair,edit_type,geom,scenario_id::integer,new_original_id::integer, 
+		CASE WHEN new_original_id IS NOT NULL THEN 'successful' ELSE 'not_successful' END AS upload_status 
+		FROM import_pois
+		WHERE edit_type = 'old_modified';
+
+		SELECT create_geojson(array_agg(to_jsonb(import_pois_gid) - 'geom'), array_agg(geom))
+		INTO pois_json
+		FROM import_pois_gid;
 	
 		all_layers_json = append_geojson(all_layers_json, pois_json, 'pois');
 	
+	/*#######################################################################*/
+	/*Inject Ways*/
+	/*#######################################################################*/
 	ELSEIF layer = 'ways' THEN 
-		/*Inject Ways*/
+	
 		UPDATE import_ways i 
 		SET new_original_id = u.new_original_id 
 		FROM (
 			SELECT p.id AS new_original_id, i.original_id
 			FROM ways_userinput p, import_ways i  
-			WHERE i.edit_type IN ('old_modified', 'deleted', 'new_modified') 
+			WHERE i.edit_type IN ('old_modified', 'deleted') 
 			AND ST_EQUALS(ST_ASTEXT(ST_QuantizeCoordinates(p.geom,6)),ST_ASTEXT(ST_QuantizeCoordinates(i.geom,6)))
 			AND p.geom && i.geom
 		) u
 		WHERE i.original_id = u.original_id
 		AND i.edit_type IN ('old_modified', 'deleted', 'new_modified'); 
 	
-		INSERT INTO ways_modified(lit,foot,bicycle,surface,way_type,wheelchair,street_category,geom,scenario_id, original_id) 
-		SELECT lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom, scenario_id_input, new_original_id::integer  
-		FROM import_ways 
-		WHERE edit_type IN ('new','new_modified');
-	
-		SELECT create_geojson(array_agg(to_jsonb(x) - 'geom'), array_agg(geom))
-		INTO ways_json
-		FROM 
+		DROP TABLE IF EXISTS import_ways_gid;
+		CREATE TEMP TABLE import_ways_gid AS 
+		WITH new_with_gid AS 
 		(
-			SELECT lit, foot, bicycle, surface, way_type, wheelchair, street_category, original_id, edit_type, geom, scenario_id,
-			CASE WHEN COALESCE(original_id::integer,0) = COALESCE(new_original_id::integer,0) THEN 'successful' ELSE 'not_successful' END AS upload_status  
+			INSERT INTO ways_modified(lit,foot,bicycle,surface,way_type,wheelchair,street_category,geom,scenario_id, original_id) 
+			SELECT lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom, scenario_id, new_original_id::integer  
 			FROM import_ways 
-		) x;
+			WHERE edit_type = 'new'
+			RETURNING gid,lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom, scenario_id, original_id
+		), 
+		new_modified_with_gid AS 
+		(
+			INSERT INTO ways_modified(lit,foot,bicycle,surface,way_type,wheelchair,street_category,geom,scenario_id, original_id) 
+			SELECT lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom, scenario_id, original_id::integer  
+			FROM import_ways 	 
+			WHERE edit_type = 'new_modified' 
+			AND new_original_id IS NOT NULL
+			RETURNING gid,lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom, scenario_id, original_id
+		)
+		SELECT gid, lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom,
+		'new' AS edit_type, scenario_id, original_id::integer, 'successful' AS upload_status  
+		FROM new_with_gid
+		UNION ALL
+		SELECT gid, lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom,
+		'new_modified' AS edit_type, scenario_id, original_id::integer,'successful' AS upload_status   
+		FROM new_modified_with_gid
+		UNION ALL 
+		SELECT original_id::integer AS gid, lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom,
+		'deleted' AS edit_type, scenario_id, new_original_id::integer,
+		CASE WHEN new_original_id IS NOT NULL THEN 'successful' ELSE 'not_successful' END AS upload_status 
+		FROM import_ways
+		WHERE edit_type = 'deleted'
+		UNION ALL 
+		SELECT original_id::integer AS gid, lit, foot, bicycle, surface, way_type, wheelchair, street_category, geom,
+		'old_modified' AS edit_type, scenario_id, new_original_id::integer,
+		CASE WHEN new_original_id IS NOT NULL THEN 'successful' ELSE 'not_successful' END AS upload_status 
+		FROM import_ways
+		WHERE edit_type = 'old_modified';
+	
+		SELECT create_geojson(array_agg(to_jsonb(import_ways_gid) - 'geom'), array_agg(geom))
+		INTO ways_json
+		FROM import_ways_gid;
+	
 		all_layers_json = append_geojson(all_layers_json, ways_json, 'ways');	
 	
+	/*#######################################################################*/
+	/*Inject Buildings*/
+	/*#######################################################################*/
 	ELSEIF layer = 'buildings' THEN
-		/*Inject Buildings*/
+	
 		UPDATE import_buildings i 
 		SET new_original_id = u.new_original_id 
 		FROM (
@@ -124,42 +187,48 @@ BEGIN
 		WHERE i.original_id = u.original_id
 		AND i.edit_type IN ('deleted'); 
 	
-		DROP TABLE IF EXISTS buildings_gid;
-		CREATE TEMP TABLE buildings_gid AS 
+		DROP TABLE IF EXISTS import_buildings_gid;
+		CREATE TEMP TABLE import_buildings_gid AS 
 		WITH with_gid AS 
 		(
 			INSERT INTO buildings_modified(building, population,building_levels,gross_floor_area,building_levels_residential,geom,scenario_id, original_id) 
 			SELECT building, population::numeric,building_levels::numeric,gross_floor_area::numeric,building_levels_residential::numeric, geom, i.scenario_id, new_original_id::integer  
 			FROM import_buildings i
-			WHERE edit_type IN ('new','new_modified')
+			WHERE edit_type = 'new'
 			RETURNING gid, building, population, building_levels, gross_floor_area, building_levels_residential, geom, scenario_id, original_id
 		) 
-		SELECT * FROM with_gid; 
-		
-		SELECT create_geojson(array_agg(to_jsonb(x) - 'geom'), array_agg(geom))
-		INTO buildings_json
-		FROM 
-		(
-			SELECT g.gid::integer, 
-			i.population, i.building_levels, i.gross_floor_area, i.building_levels_residential, i.original_id, i.edit_type, i.geom, i.scenario_id,
-			CASE WHEN COALESCE(i.original_id::integer,0) = COALESCE(i.new_original_id::integer,0) THEN 'successful' ELSE 'not_successful' END AS upload_status 
-			FROM import_buildings i, buildings_gid g 
-			WHERE i.geom = g.geom  
-			AND i.edit_type <> 'deleted'	
-			UNION ALL 
-			SELECT new_original_id::integer AS gid, i.population, i.building_levels, i.gross_floor_area, i.building_levels_residential, i.original_id, i.edit_type, i.geom, i.scenario_id,
-			CASE WHEN COALESCE(i.original_id::integer,0) = COALESCE(i.new_original_id::integer,0) THEN 'successful' ELSE 'not_successful' END AS upload_status 
-			FROM import_buildings i
-			WHERE i.edit_type = 'deleted'			
-		) x;
-		all_layers_json = append_geojson(all_layers_json, buildings_json, 'buildings');
-		
-	ELSEIF layer = 'buildings_entrances' THEN  
-		/*Inject Buildings entrances*/
-		INSERT INTO population_modified(population,geom,scenario_id) 
-		SELECT population::numeric, geom, scenario_id_input 
-		FROM import_buildings_entrances; 
+		SELECT gid, building, population, building_levels, gross_floor_area, building_levels_residential, 'new' AS edit_type, 
+		geom, scenario_id, original_id, 'successful' AS upload_status 
+		FROM with_gid 
+		UNION ALL 
+		SELECT new_original_id::integer AS gid, building, population::numeric, building_levels::numeric, gross_floor_area::numeric, building_levels_residential::numeric, 
+		'deleted' AS edit_type, geom, scenario_id::integer, original_id::integer,
+		CASE WHEN new_original_id IS NOT NULL THEN 'successful' ELSE 'not_successful' END AS upload_status  
+		FROM import_buildings i
+		WHERE i.edit_type = 'deleted';
 	
+		SELECT create_geojson(array_agg(to_jsonb(import_buildings_gid) - 'geom'), array_agg(geom))
+		INTO buildings_json
+		FROM import_buildings_gid;
+	
+		all_layers_json = append_geojson(all_layers_json, buildings_json, 'buildings');
+	
+	/*#######################################################################*/
+	/*Inject Buildings entrances*/
+	/*#######################################################################*/
+	ELSEIF layer = 'buildings_entrances' THEN  
+
+		DROP TABLE IF EXISTS import_buildings_entrances_gid;
+		CREATE TEMP TABLE import_buildings_entrances_gid AS 
+		WITH with_gid AS 
+		(
+			INSERT INTO population_modified(population,geom,scenario_id) 
+			SELECT population::numeric, geom, scenario_id_input 
+			FROM import_buildings_entrances
+			RETURNING gid, population, geom, scenario_id
+		)
+		SELECT * FROM with_gid;
+		
 		UPDATE population_modified p
 		SET building_gid = b.gid 
 		FROM buildings b 
@@ -171,8 +240,8 @@ BEGIN
 		INTO buildings_entrances_json 
 		FROM 
 		(
-			SELECT population, geom, scenario_id, 'successful' AS upload_status
-			FROM import_buildings_entrances 
+			SELECT *, 'successful' AS upload_status
+			FROM import_buildings_entrances_gid
 		) x; 
 		all_layers_json = append_geojson(all_layers_json, buildings_entrances_json, 'buildings_entrances');
 
@@ -197,6 +266,7 @@ BEGIN
 
 END;
 $function$
+
 /*
 SELECT import_changeset_scenario(19,42, jsonb_build_object('buildings',export_changeset_scenario(18) -> 'buildings'))
 */
