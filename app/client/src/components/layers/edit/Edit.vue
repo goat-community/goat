fb069b7f4837e23e763ce0455a14bd4f2318587e
Majk Shkurti <majkshkurti94@gmail.com>
2020-11-16 00:59:19 +0100

    fix #864

--- app/client/src/components/layers/edit/Edit.vue
+++ app/client/src/components/layers/edit/Edit.vue
@@ -564,7 +564,7 @@
         <v-btn
           v-show="selectedLayer != null"
           class="white--text"
-          :loading="isUploadBusy"
+          v-if="!isUploadBusy"
           :disabled="
             isDeleteAllBusy ||
               (isUploadBtnEnabled === false &&
@@ -577,12 +577,22 @@
         >
           <v-icon left>cloud_upload</v-icon>{{ $t("appBar.edit.uploadBtn") }}
         </v-btn>
+
+        <v-btn
+          v-else
+          class="white--text"
+          @click.stop="stopUpload"
+          color="error"
+        >
+          <v-icon color="white">close</v-icon>Stop Upload
+        </v-btn>
+
         <v-btn
           v-show="selectedLayer != null"
           class="white--text"
           color="error"
           :loading="isDeleteAllBusy"
-          :disabled="scenarioDataTable.length === 0"
+          :disabled="scenarioDataTable.length === 0 || isUploadBusy"
           @click="deleteAll"
         >
           <v-icon left>delete</v-icon>{{ $t("appBar.edit.clearBtn") }}
@@ -1858,6 +1868,14 @@ export default {
         this.olEditCtrl.source.addFeature(clonedFeature);
         //Commit restore changes. ("commitDelete" just updates array of deleted features ids in the database)
         editLayerHelper.commitDelete("update_deleted_features");
+      }
+    },
+    /**
+     * Stop Upload button
+     */
+    stopUpload() {
+      if (editLayerHelper.cancelReq instanceof Function) {
+        editLayerHelper.cancelReq("cancelled");
       }
     },
     /**
