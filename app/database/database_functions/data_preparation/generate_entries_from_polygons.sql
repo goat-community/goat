2c97d55020a973c67673c7a0743b5abfe11e3c6a
Elias Pajares <elias.pajares@tum.de>
2020-11-25 17:45:22 +0100

    added dump for multipoint

--- app/database/database_functions/data_preparation/generate_entries_from_polygons.sql
+++ app/database/database_functions/data_preparation/generate_entries_from_polygons.sql
@@ -10,7 +10,7 @@ BEGIN
 	INSERT INTO pois (osm_id, origin_geometry, ACCESS, housenumber, amenity, origin, organic, denomination, brand, name, OPERATOR, public_transport, railway, religion, opening_hours, REF, tags, geom, wheelchair)
 	SELECT a.osm_id, a.origin_geometry, a.ACCESS, a.housenumber, a.amenity, a.origin, a.organic, a.denomination, a.brand, 
 	a.amenity || '-' || a.gid AS name, a.OPERATOR, a.public_transport, a.railway,
-	a.religion, a.opening_hours, a.REF, a.tags, ST_intersection(ST_Boundary(a.geom),p.way), a.wheelchair
+	a.religion, a.opening_hours, a.REF, a.tags, (ST_DUMP(ST_intersection(ST_Boundary(a.geom),p.way))).geom, a.wheelchair
 	FROM aois a, planet_osm_line p 
 	WHERE a.amenity IN (SELECT UNNEST(amenity_set))
 	AND p.highway IN (SELECT UNNEST(path_types)) 
