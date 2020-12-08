174e2b762e37b5caa3c0ebaad8d0a8b5044917d3
Elias Pajares <elias.pajares@tum.de>
2020-12-08 17:07:08 +0100

    refactoring interpolate cost for performance

--- app/database/database_functions/heatmap/interpolate_cost.sql
+++ app/database/database_functions/heatmap/interpolate_cost.sql
@@ -1,30 +1,15 @@
-DROP FUNCTION IF EXISTS interpolate_cost;
-CREATE OR REPLACE FUNCTION public.interpolate_cost(fraction float, arr_start_cost smallint[], arr_end_cost smallint[])
-RETURNS TABLE (arr_true_cost smallint[]) 
-AS $function$
-DECLARE 
-	cnt smallint := 0;
-	start_cost smallint;
-	end_cost smallint;
-	true_cost smallint;
-	_true_cost smallint;
-	arr_true_cost smallint[] := ARRAY[]::smallint[];
-BEGIN 
+DROP FUNCTION IF EXISTS interpolate_cost; 
 		
-	FOREACH start_cost IN ARRAY arr_start_cost
-  	LOOP
-  		cnt = cnt + 1;
-  		end_cost = arr_end_cost[cnt];
-  		IF start_cost < end_cost THEN
-  			true_cost = (start_cost + (fraction * (end_cost-start_cost)))::smallint;
-  		ELSE 
-  			true_cost = (end_cost + ((1-fraction) * (start_cost - end_cost)))::smallint;
-  		END IF;
-		arr_true_cost = arr_true_cost || true_cost;
-  	END LOOP;
-  	  
-	RETURN query SELECT arr_true_cost; 
-END;
-$function$ LANGUAGE plpgsql IMMUTABLE;
+CREATE OR REPLACE FUNCTION interpolate_cost(fraction float, start_cost SMALLINT[], end_cost SMALLINT[])
+RETURNS smallint[] AS
+$$
+	SELECT ARRAY_AGG(CASE WHEN start_cost < end_cost THEN (start_cost + fraction * (end_cost-start_cost))::smallint
+	ELSE (end_cost + (1-fraction) * (start_cost - end_cost))::smallint END)
+	FROM (
+		SELECT UNNEST(start_cost) start_cost, UNNEST(end_cost) end_cost
+	) x;
+	
+$$
+LANGUAGE sql immutable;
 
 --SELECT * FROM interpolate_cost(0.5, ARRAY[100,200,300]::SMALLINT[], ARRAY[200,300,400]::SMALLINT[]);
