1463b3742daf5b8b760036053877c3a33440d800
Elias Pajares <elias.pajares@tum.de>
2020-12-20 18:10:52 +0100

    worked on recompute

--- app/api-flask/functions/test_recompute.py
+++ app/api-flask/functions/test_recompute.py
@@ -38,28 +38,27 @@ def recompute_heatmap(scenario_id):
         'walking_standard',%(userid)s, %(scenario_id)s, %(section_id)s)''',
         {"max_cost": [max_cost], "starting_points": i[0], "speed": speed, "gridids": i[1], "userid": userid, "scenario_id": scenario_id, "section_id": i[2]})
 
-    return changed_grids
+    gridids = db.select("""SELECT UNNEST(gridids) FROM changed_grids""")
+    
+    for g in gridids:
+        db.perform("""SELECT compute_area_isochrone(%(grid_id)s,%(scenario_id)s)""", {"grid_id": g[0],"scenario_id": scenario_id})
+    
+    buffer_geom = db.select("""SELECT ST_BUFFER(ST_UNION(geom),0.0014) 
+    FROM area_isochrones_scenario 
+    WHERE scenario_id = %(scenario_id)s""", {"scenario_id": scenario_id})
+    
+    db.perform("""DELETE FROM reached_pois_heatmap r
+    USING pois_userinput p
+    WHERE ST_Intersects(p.geom,%(buffer_geom)s)
+    AND r.gid = p.gid
+    AND r.scenario_id = %(scenario_id)s""", {"buffer_geom": buffer_geom, "scenario_id": scenario_id})
+    
 
+    return buffer_geom
 x = recompute_heatmap(95)
 
 
 '''
-FOR grid_id IN
-    SELECT UNNEST(gridids) FROM changed_grids
-LOOP
-    PERFORM compute_area_isochrone(grid_id,scenario_id_input);
-END LOOP;
-
-SELECT ST_BUFFER(ST_UNION(geom),0.0014) 
-INTO buffer_geom
-FROM area_isochrones_scenario 
-WHERE scenario_id = scenario_id_input;
-
-DELETE FROM reached_pois_heatmap r
-USING pois_userinput p
-WHERE ST_Intersects(p.geom,buffer_geom)
-AND r.gid = p.gid
-AND r.scenario_id = scenario_id_input; 
 
 PERFORM reached_pois_heatmap(buffer_geom, 0.0014, 'scenario', scenario_id_input);
 
