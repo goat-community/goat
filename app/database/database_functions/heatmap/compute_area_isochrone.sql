deecfadfe5bc0224de55682d33a0cdaa7bd3185e
Elias Pajares <elias.pajares@tum.de>
2020-12-29 10:19:18 +0100

    adjusted function for new logic heatmap

--- app/database/database_functions/heatmap/compute_area_isochrone.sql
+++ app/database/database_functions/heatmap/compute_area_isochrone.sql
@@ -3,10 +3,11 @@ CREATE OR REPLACE FUNCTION compute_area_isochrone(grid_id_input integer, scenari
   RETURNS SETOF VOID AS
 $func$
 DECLARE 
-	var_area NUMERIC;  
+	var_area NUMERIC;
+	iso_geom geometry;  
 BEGIN 
-	var_area = (SELECT DISTINCT ST_AREA(ST_CONVEXHULL(ST_COLLECT(geom))) AS area_isochrone
-	FROM 
+	
+	WITH x AS 
 	(
 		SELECT st_startpoint(geom) geom  
 		FROM reached_edges_heatmap 
@@ -17,16 +18,25 @@ BEGIN
 		FROM reached_edges_heatmap 
 		WHERE gridids && ARRAY[grid_id_input]
 		AND scenario_id = scenario_id_input 
-	) x);
-	raise notice '%', var_area;
+	), 
+	convex AS 
+	(
+		SELECT ST_CONVEXHULL(ST_COLLECT(geom)) AS geom 
+		FROM x 
+	)
+	SELECT ST_AREA(geom), geom
+	INTO var_area, iso_geom
+	FROM convex; 
+	
 	IF scenario_id_input = 0 THEN 
 		UPDATE grid_heatmap SET area_isochrone = var_area
 		WHERE grid_id = grid_id_input;
 	ELSE 
-		INSERT INTO area_isochrones_scenario(grid_id, area_isochrone, scenario_id) 
-		VALUES(grid_id_input,var_area,scenario_id_input);
+		INSERT INTO area_isochrones_scenario(grid_id, area_isochrone, geom, scenario_id) 
+		VALUES(grid_id_input,var_area,iso_geom,scenario_id_input);
 	END IF;
 END 	
 $func$  LANGUAGE plpgsql;
 
 /*SELECT compute_area_isochrone(1,0)*/
+
