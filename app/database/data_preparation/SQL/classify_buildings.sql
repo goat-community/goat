033502ab67a32af31b49918a0530935a1455d0b6
Elias Pajares <elias.pajares@tum.de>
2021-05-07 22:44:48 +0200

    WIP

--- app/database/data_preparation/SQL/classify_buildings.sql
+++ app/database/data_preparation/SQL/classify_buildings.sql
@@ -388,7 +388,6 @@ WHERE residential_status = 'with_residents';
 
 
 
-
 /**/
 
 *Performance issues with queries. Ideas for improvement:
@@ -411,9 +410,21 @@ WHERE residential_status IS NULL;
 
 
 DROP TABLE IF EXISTS buildings_classification;
-CREATE TABLE buildings_classification AS 
+CREATE TABLE buildings_classification 
+(
+	gid integer, 
+	landuse_residential_status integer[],
+	landuse_gids integer[],
+	landuse_additional_residential_status integer[],
+	landuse_additional_gids integer[],
+	landuse_osm_residential_status integer[],
+	landuse_osm_gids integer[]
+);
+
+INSERT INTO buildings_classification 
 SELECT gid 
-FROM buildings; 
+FROM buildings
+WHERE residential_status = 'potential_residents'; 
 
 ALTER TABLE buildings_classification ADD PRIMARY KEY(gid);
 
@@ -427,14 +438,14 @@ DO $$
             )
         THEN
         	
-        	DROP TABLE IF EXISTS custom_landuse_subdivide; 
-        	CREATE TABLE custom_landuse_subdivide AS 
+        	DROP TABLE IF EXISTS landuse_subdivide; 
+        	CREATE TABLE landuse_subdivide AS 
         	SELECT landuse, ST_SUBDIVIDE(geom, 200) AS geom
         	FROM landuse; 
         
-        	ALTER TABLE custom_landuse_subdivide ADD COLUMN gid serial;
-        	ALTER TABLE custom_landuse_subdivide ADD PRIMARY KEY(gid);
-    		CREATE INDEX ON custom_landuse_subdivide USING GIST(geom);
+        	ALTER TABLE landuse_subdivide ADD COLUMN gid serial;
+        	ALTER TABLE landuse_subdivide ADD PRIMARY KEY(gid);
+    		CREATE INDEX ON landuse_subdivide USING GIST(geom);
      		
     		DROP TABLE IF EXISTS buildings_landuse;
         	CREATE TABLE buildings_landuse AS 
@@ -442,19 +453,20 @@ DO $$
         	(
 				SELECT b.gid, CASE WHEN l.landuse IS NULL THEN 2 
 				ELSE (select_from_variable_container('custom_landuse_no_residents') && ARRAY[l.landuse])::integer END AS residential_status, l.gid landuse_gid
-				FROM buildings b 
-				LEFT JOIN custom_landuse_subdivide l
+				FROM (
+					SELECT * 
+					FROM buildings 
+					WHERE residential_status = 'potential_residents'
+				) b
+				LEFT JOIN landuse_subdivide l
 				ON ST_INTERSECTS(l.geom, b.geom)
-				AND residential_status = 'potential_residents'
 			) 
 			SELECT gid, ARRAY_AGG(residential_status) AS landuse_residential_status, ARRAY_AGG(landuse_gid) AS landuse_gids			
 			FROM buildings_intersects
 			GROUP BY gid; 
 			
 			ALTER TABLE buildings_landuse ADD PRIMARY KEY(gid);
-			ALTER TABLE buildings_classification ADD COLUMN landuse_residential_status integer[];
-			ALTER TABLE buildings_classification ADD COLUMN landuse_gids integer[];
-		
+
 			UPDATE buildings_classification c
 			SET landuse_residential_status = l.landuse_residential_status,
 			landuse_gids = l.landuse_gids
@@ -465,9 +477,10 @@ DO $$
     END
 $$ ;
 
+
 DO $$                  
     DECLARE 
-    	categories_no_residents TEXT[] := 	
+    	categories_no_residents TEXT[] := select_from_variable_container('custom_landuse_additional_no_residents');
 	BEGIN 
         IF EXISTS
             ( SELECT 1
@@ -490,19 +503,20 @@ DO $$
         	WITH buildings_intersects AS 
         	(
 				SELECT b.gid, CASE WHEN l.landuse IS NULL THEN 2 
-				ELSE (select_from_variable_container('custom_landuse_no_residents') && ARRAY[l.landuse])::integer END AS residential_status, l.gid landuse_gid
-				FROM buildings b 
+				ELSE (categories_no_residents && ARRAY[l.landuse])::integer END AS residential_status, l.gid landuse_gid
+				FROM (
+					SELECT * 
+					FROM buildings 
+					WHERE residential_status = 'potential_residents'
+				) b 
 				LEFT JOIN landuse_additional_subdivide l
 				ON ST_INTERSECTS(l.geom, b.geom)
-				AND residential_status = 'potential_residents'
 			) 
 			SELECT gid, ARRAY_AGG(residential_status) AS landuse_residential_status, ARRAY_AGG(landuse_gid) AS landuse_gids		
 			FROM buildings_intersects
 			GROUP BY gid; 
 			
 			ALTER TABLE buildings_landuse ADD PRIMARY KEY(gid);
-			ALTER TABLE buildings_classification ADD COLUMN landuse_additional_residential_status integer[];
-			ALTER TABLE buildings_classification ADD COLUMN landuse_additional_gids integer[];
 		
 			UPDATE buildings_classification c
 			SET landuse_additional_residential_status = l.landuse_residential_status,
@@ -513,7 +527,13 @@ DO $$
     END
 $$ ;
 
-DO $$                  
+
+
+DO $$
+	DECLARE 
+    	categories_no_residents TEXT[] := select_from_variable_container('osm_landuse_no_residents') 
+    		|| select_from_variable_container('tourism_no_residents')
+    		|| select_from_variable_container('amenity_no_residents');
     BEGIN 
         IF EXISTS
             ( SELECT 1
@@ -529,42 +549,33 @@ DO $$
 			FROM landuse_osm l
 			WHERE landuse IS NOT NULL;
 	
-		
-			AND (tourism IS NULL OR tourism NOT IN(SELECT UNNEST(select_from_variable_container('tourism_no_residents'))))
-			AND (amenity IS NULL OR amenity NOT IN(SELECT UNNEST(select_from_variable_container('amenity_no_residents'))));
-			
-				
-			DROP TABLE IF EXISTS landuse_additional_subdivide ; 
-        	CREATE TABLE landuse_additional_subdivide  AS 
-        	SELECT landuse, ST_SUBDIVIDE(geom, 200) AS geom
-        	FROM landuse_additional; 
-        
-        	ALTER TABLE landuse_additional_subdivide ADD COLUMN gid serial;
-        	ALTER TABLE landuse_additional_subdivide  ADD PRIMARY KEY(gid);
-    		CREATE INDEX ON landuse_additional_subdivide  USING GIST(geom);
+        	ALTER TABLE landuse_osm_subdivide ADD COLUMN gid serial;
+        	ALTER TABLE landuse_osm_subdivide  ADD PRIMARY KEY(gid);
+    		CREATE INDEX ON landuse_osm_subdivide  USING GIST(geom);
      		
     		DROP TABLE IF EXISTS buildings_landuse;
         	CREATE TABLE buildings_landuse AS 
         	WITH buildings_intersects AS 
         	(
 				SELECT b.gid, CASE WHEN l.landuse IS NULL THEN 2 
-				ELSE (select_from_variable_container('custom_landuse_no_residents') && ARRAY[l.landuse])::integer END AS residential_status, l.gid landuse_gid
-				FROM buildings b 
-				LEFT JOIN landuse_additional_subdivide l
+				ELSE (categories_no_residents && ARRAY[l.landuse])::integer END AS residential_status, l.gid landuse_gid
+				FROM (
+					SELECT * 
+					FROM buildings 
+					WHERE residential_status = 'potential_residents'
+				) b
+				LEFT JOIN landuse_osm_subdivide l
 				ON ST_INTERSECTS(l.geom, b.geom)
-				AND residential_status = 'potential_residents'
 			) 
 			SELECT gid, ARRAY_AGG(residential_status) AS landuse_residential_status, ARRAY_AGG(landuse_gid) AS landuse_gids		
 			FROM buildings_intersects
 			GROUP BY gid; 
 			
 			ALTER TABLE buildings_landuse ADD PRIMARY KEY(gid);
-			ALTER TABLE buildings_classification ADD COLUMN landuse_additional_residential_status integer[];
-			ALTER TABLE buildings_classification ADD COLUMN landuse_additional_gids integer[];
-		
+
 			UPDATE buildings_classification c
-			SET landuse_additional_residential_status = l.landuse_residential_status,
-			landuse_additional_gids = l.landuse_gids
+			SET landuse_osm_residential_status = l.landuse_residential_status,
+			landuse_osm_gids = l.landuse_gids
 			FROM buildings_landuse l
 			WHERE c.gid = l.gid; 
 		
@@ -573,6 +584,224 @@ DO $$
     END
 $$ ;
 
+
+SELECT count(*) 
+FROM buildings_classification bc 
+
+/* 0 ==> residential
+ * 1 ==> not_residential
+ * 2 ==> no landuse
+ * */
+
+DROP TABLE test; 
+
+CREATE TABLE test AS 
+
+EXPLAIN ANALYZE 
+SELECT b.*
+FROM buildings_classification c, buildings b
+WHERE c.landuse_residential_status = ARRAY[0]
+AND c.landuse_additional_residential_status = ARRAY[0]
+AND c.landuse_osm_residential_status = ARRAY[0]
+AND c.gid = b.gid 
+
+
+CREATE TABLE test AS 
+SELECT count(*)
+FROM buildings_classification c, buildings b
+WHERE c.landuse_residential_status = ARRAY[1]
+AND c.landuse_additional_residential_status = ARRAY[1]
+AND c.landuse_osm_residential_status = ARRAY[1]
+AND c.gid = b.gid 
+
+DROP TABLE test; 
+
+CREATE TABLE test AS 
+SELECT geom
+FROM buildings_classification c, buildings b
+WHERE c.landuse_residential_status = ARRAY[1]
+AND c.landuse_additional_residential_status = ARRAY[1]
+AND c.landuse_osm_residential_status = ARRAY[2]
+AND c.gid = b.gid 
+
+EXPLAIN ANALYZE 
+
+WITH x AS 
+(	
+	SELECT classify_building(landuse_residential_status,landuse_additional_residential_status,landuse_osm_residential_status) AS residential_status
+	FROM buildings_classification bc 
+)
+SELECT residential_status, count(*) 
+FROM x
+GROUP BY residential_status
+
+SELECT uniq(ARRAY[[1,2],[2,3]])
+
+
+SELECT DISTINCT landuse FROM landuse l 
+
+EXPLAIN ANALYZE 
+SELECT uniq(ARRAY[1,2])
+
+
+DROP FUNCTION IF EXISTS classify_building;
+CREATE OR REPLACE FUNCTION classify_building(bgid integer, categorization jsonb[])
+ RETURNS void --text
+AS $$
+DECLARE
+   categorization_unique integer[][] := ARRAY[[]]::integer[][];
+   cnt integer := 0;
+BEGIN
+
+	FOR cnt IN 1..ARRAY_LENGTH(categorization,1) 
+	LOOP   		
+      	IF ARRAY_LENGTH(uniq(categorization[cnt]),1) > 1 THEN 	
+      		categorization_unique = ARRAY_APPEND(categorization_unique,multiple_landuse_intersection(bgid,categorization[cnt],landuse_gids[cnt],landuse_tables[cnt])); 
+		ELSE 
+			categorization_unique = categorization;
+		END IF;
+   	END LOOP;
+	 
+	RAISE NOTICE '%', categorization_unique; 
+	
+	
+	
+END
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+
+SELECT jsonb_build_array(jsonb_build_object('categorization',landuse_residential_status,'landuse_gid', landuse_gids, 'table','landuse'),
+jsonb_build_object('categorization',landuse_additional_residential_status,'landuse_additional_gid', landuse_additional_gids, 'table','landuse_additional') ,
+jsonb_build_object('categorization',landuse_osm_residential_status,'landuse_osm_gid', landuse_osm_gids, 'table','landuse_osm')) 
+FROM buildings_classification b
+
+SELECT * FROM buildings_classification bc 
+
+SELECT classify_building(24,ARRAY[landuse_residential_status, landuse_additional_residential_status, landuse_osm_residential_status],
+ARRAY[landuse_gids, landuse_additional_gids, landuse_osm_gids],ARRAY['landuse','landuse_additional','landuse_osm'])
+FROM buildings_classification bc 
+LIMIT 100
+
+
+
+IF unique_classes = ARRAY[0] THEN 
+	RETURN 'with_residents';
+ELSEIF unique_classes = ARRAY[1] THEN
+	RETURN 'no_residents';
+END IF;
+
+IF uniq(categorization[1] || categorization[2]) = ARRAY[0] THEN 
+	RETURN 'with_residents';
+ELSEIF uniq(categorization[1] || categorization[2]) = ARRAY[1] 
+	RETURN 'potential_residents';
+END IF;
+
+
+
+
+SELECT array_length(ARRAY[1,2],1) 
+
+CREATE OR REPLACE FUNCTION derive_dominant_class(bgid integer, categorization integer[][], landuse_gids integer[][], landuse_table TEXT)
+ RETURNS integer
+AS $$
+DECLARE
+   bgeom geometry := (SELECT geom FROM buildings WHERE gid = bgid);
+   dominant_class integer;
+BEGIN
+	IF landuse_table = 'landuse' THEN 
+		SELECT cat
+		INTO dominant_class
+		FROM (SELECT UNNEST(categorization) cat, UNNEST(landuse_gids) gid) c, landuse_subdivide l 
+		WHERE l.gid = c.gid
+		GROUP BY cat 
+		ORDER BY ST_AREA(ST_INTERSECTION(bgeom, ST_UNION(l.geom))) 
+		LIMIT 1;
+	ELSEIF landuse_table = 'landuse_additional' THEN 
+		SELECT cat
+		INTO dominant_class
+		FROM (SELECT UNNEST(categorization) cat, UNNEST(landuse_gids) gid) c, landuse_additional_subdivide l 
+		WHERE l.gid = c.gid
+		GROUP BY cat 
+		ORDER BY ST_AREA(ST_INTERSECTION(bgeom, ST_UNION(l.geom)))
+		LIMIT 1;
+	ELSEIF landuse_table = 'landuse_osm' THEN 
+		SELECT cat
+		INTO dominant_class
+		FROM (SELECT UNNEST(categorization) cat, UNNEST(landuse_gids) gid) c, landuse_osm_subdivide l 
+		WHERE l.gid = c.gid
+		GROUP BY cat 
+		ORDER BY ST_AREA(ST_INTERSECTION(bgeom, ST_UNION(l.geom))) 
+		LIMIT 1;
+	ELSE 
+		RAISE NOTICE 'No valid landuse table was selected.';
+	END IF; 
+
+	RETURN dominant_class;
+END
+$$ LANGUAGE plpgsql;
+
+
+SELECT cat
+--INTO dominant_class
+FROM (SELECT UNNEST(categorization) cat, UNNEST(landuse_gids) gid) c, landuse l 
+WHERE l.gid = c.gid
+GROUP BY cat 
+ORDER BY ST_AREA(ST_INTERSECTION(bgeom, ST_UNION(l.geom))); 
+
+SELECT 
+
+SELECT * FROM test_classes
+
+
+DROP FUNCTION multiple_landuse_intersection
+
+EXPLAIN ANALYZE 
+SELECT multiple_landuse_intersection(24,ARRAY[0,0,0,0,0,1,0],ARRAY[4293,4412,9376,9458,9459,9472,9554],'landuse')
+
+
+
+
+
+SELECT * 
+FROM landuse
+WHERE gid IN (SELECT UNNEST(ARRAY[4293,4412,9376,9458,9459,9472,9554]))
+
+
+DROP TABLE test 
+
+CREATE TABLE test AS 
+SELECT * 
+FROM buildings
+WHERE gid = 24
+
+SELECT * FROM buildings_classification bc 
+
+
+SELECT categorization, ST_UNION(l.geom) AS geom 
+FROM (SELECT UNNEST(ARRAY[1,2]) categorization, UNNEST(ARRAY[1,2]) gid) c, landuse l 
+WHERE l.gid = c.gid
+GROUP BY categorization 
+
+DROP TABLE test 
+
+CREATE TABLE test AS 
+SELECT c.gid, b.geom 
+FROM buildings_classification c, buildings b
+WHERE uniq(c.landuse_residential_status) = ARRAY[0,1]
+AND c.gid = b.gid 
+
+SELECT ARRAY[1] && ARRAY[1] && ARRAY[1]
+
+SELECT * 
+FROM buildings_classification bc 
+WHERE gid = 36886
+
+
+
+UNION ALL 
+SELECT b.geom 
+FROM buildings b 
+WHERE b.residential_status = 'with_residents'
 
 CREATE INDEX ON landuse_residents USING GIST(geom);
 CREATE INDEX ON landuse_no_residents USING GIST(geom);
