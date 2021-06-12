ad753313603db9b0be33b5aad3b6bf68dad317c9
Elias Pajares <elias.pajares@tum.de>
2021-06-12 21:00:56 +0200

    WIP slope using vector points

--- app/database/scripts/slope_profile.py
+++ app/database/scripts/slope_profile.py
@@ -1,193 +1,180 @@
-# Rounding solution from: https://gis.stackexchange.com/questions/321518/rounding-coordinates-to-5-decimals-in-geopandas
-import copy
-import re
-import geopandas as gpd
-import zipfile
 from db.db import Database
-from geocube.api.core import make_geocube
 from shapely.wkt import loads
 from os import remove
 
-db = Database()
-
-
 class Profiles:
-    def __init__(self):
-        self.batch_size = 1000
+    def __init__(self, db_suffix, ways_table, filter_ways):
+        self.db_suffix = db_suffix
+        self.ways_table = ways_table
+        self.filter_ways = filter_ways
+
+        db = Database(self.db_suffix)
+        conn = db.connect()
+        cur = conn.cursor()
+        
+        cur.execute('''SELECT count(*) FROM {0} {1};'''.format(self.ways_table, self.filter_ways))
+        self.total_cnt = cur.fetchall()[0][0]
+        
+        cur.execute('''SELECT select_from_variable_container_s('one_meter_degree');''')
+        self.meter_degree = float(cur.fetchall()[0][0])
+
+        cur.execute('''SELECT id FROM {0} {1};'''.format(self.ways_table, self.filter_ways))
+        ids = cur.fetchall()
+        self.ids = [x[0] for x in ids]
+
+        conn.close()
+
+        self.batch_size = 200
         self.output_format = 'GeoJSON'
         self.out_dir = '/opt/data/'
         self.filename = self.out_dir + 'ways_profile_points.geojson'
         self.profile_table = 'ways_profile'
         self.impedance_table = 'ways_impedance'
-        self.impedance_step = 10
-        self.compress=True
-        self.trim_decimals = True
-        self.decimals = 5
-        self.simpledec = re.compile(r"\d*\.\d+")
-        self.check_drivers
-        self.enable_driver=False
-        self.raster=False
-        self.bigtiff='YES'
-        self.filter_ways='''WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container(\'excluded_class_id_cycling\')))'''                        
+        self.elevs_interval = 25
+        
+        
+    def elevation_profile(self): 
+        db = Database(self.db_suffix)
+        conn = db.connect() 
+        cur = conn.cursor()
 
-    def check_drivers(self):
-        from fiona import supported_drivers
-        from fiona._env import GDALEnv        
+        sql_create_table = '''
+            DROP TABLE IF EXISTS elevs_{0};
+            CREATE TABLE elevs_{0} 
+            (
+                id bigint,
+                elevs float[],
+                elevs_interval float,
+                length_m float
+            );'''.format(self.ways_table)
+        cur.execute(sql_create_table)
+        conn.commit()
+ 
+        cnt = 0
+        for i in self.ids:         
+            cnt = cnt + 1 
+            if (cnt/self.batch_size).is_integer():
+                print('Slope profile for %s out of %s lines' % (cnt,self.total_cnt)) 
+                conn.commit()
 
-        # Check default Geopandas against FIONA drivers
-        if self.output_format not in supported_drivers:
-            self.enable_driver=True        
-            drv = GDALEnv().drivers()
-            if self.output_format not in drv:
-                print('GDAL Driver {0} not present or misspelled'.format(self.output_format))
-                exit(1)
+            sql_elevs = '''
+            INSERT INTO elevs_{0}(id, elevs, elevs_interval, length_m)
+            WITH way AS 
+            (
+                SELECT id, geom, length_m
+                FROM ways 
+                WHERE id = {1}
+            )
+            SELECT w.id, s.elevs, {2} elevs_interval, w.length_m
+            FROM way w, 
+            LATERAL (
+                SELECT ARRAY_AGG(elev) elevs
+                FROM get_elevation_profile_vector(geom, length_m, {3}, {2})
+            ) s;'''.format(self.ways_table, i, self.elevs_interval, self.meter_degree)
 
-    @staticmethod
-    def ways2sql(ways):
-        lsql = '('
-        for w in ways:
-            lsql = lsql + '{0},'.format(w)
-        lsql = lsql[:-1] + ')'
-        return lsql
+            cur.execute(sql_elevs)
 
-    def mround(self, match):        
-        return "{:.{}f}".format(float(match.group()), self.decimals)
+        conn.commit()
+        cur.execute('ALTER TABLE elevs_{0} ADD PRIMARY KEY (id);'.format(self.ways_table))
+        conn.commit() 
+        conn.close()
 
-    def trim(self, gdf):
-        return gdf.geometry.apply(lambda x: loads(re.sub(self.simpledec, self.mround, x.wkt)))
+    def compute_cycling_impedance(self):
+        db = Database(self.db_suffix)
+        conn = db.connect() 
+        cur = conn.cursor()
+        sql_create_table = '''
+            DROP TABLE IF EXISTS impedances_{0};
+            CREATE TABLE impedances_{0} 
+            (
+                id bigint,
+                s_imp float,
+                rs_imp float
+            );'''.format(self.ways_table)
+        cur.execute(sql_create_table)
+        conn.commit()
 
-    def get_chunks(self, table='ways'):
-        r = db.select(query='SELECT count(*) FROM ways {0};'.format(self.filter_ways))
-        chunks = range(int(r[0][0] / self.batch_size)+1)
-        return chunks
+        cnt = 0
+        for i in self.ids:         
+            cnt = cnt + 1 
+            if (cnt/self.batch_size).is_integer():
+                print('Compute impedances for %s out of %s lines' % (cnt,self.total_cnt)) 
+                conn.commit()
+            sql_update_impedance = '''
+                INSERT INTO impedances_{0}(id, s_imp, rs_imp)
+                SELECT x.id, i.imp, i.rs_imp
+                FROM (
+                    SELECT * 
+                    FROM elevs_{0} 
+                    WHERE id = {1}
+                    AND elevs IS NOT NULL 
+                ) x, LATERAL compute_impedances(elevs, length_m, elevs_interval)  i'''.format(self.ways_table, i)
+            cur.execute(sql_update_impedance)
 
-    def get_elevation(self, way_list=None):
-        if way_list is None:
-            gdf_master = None
-            batches = self.get_chunks()
-            for b in batches:
-                sql = """
-                    WITH segments AS (
-                        SELECT id FROM ways {2} LIMIT {0} OFFSET {1}
-                    )                                
-                    
-                    SELECT seg.return_id as way_id, seg.elevs as elevation, seg.return_geom as geom
-                    FROM segments s,
-                    LATERAL get_slope_profile_precompute(s.id) seg;        
-                """.format(self.batch_size, b * self.batch_size, self.filter_ways)
-                gdf = db.select(query=sql, return_type='geodataframe')                
-                if self.trim_decimals:
-                    gdf.geometry = self.trim(gdf)
-                if gdf_master is None:
-                    gdf_master = copy.deepcopy(gdf)
-                else:
-                    t = gdf_master.append(gdf)
-                    gdf_master = copy.deepcopy(t)            
-            return gdf_master
-        else:
-            ways = self.ways2sql(way_list)
-            sql = """
-                    WITH segments AS (
-                        SELECT id FROM ways WHERE id IN {0}
-                    )                                
-                    
-                    SELECT seg.return_id as way_id, seg.elevs as elevation, seg.return_geom as geom
-                    FROM segments s,
-                    LATERAL get_slope_profile_precompute(s.id) seg;        
-                """.format(ways)
-            gdf = db.select(query=sql, return_type='geodataframe')
-            if self.trim_decimals:
-                gdf.geometry = self.trim(gdf)
-            return gdf
+        conn.commit()    
+        sql_primary_key = 'ALTER TABLE impedances_{0} ADD PRIMARY KEY (id);'.format(self.ways_table)
+        cur.execute(sql_primary_key)
+        conn.commit()
+        conn.close()
+    
+    def compute_average_slope(self):
+        db = Database(self.db_suffix)
+        conn = db.connect() 
+        cur = conn.cursor()
 
-    def get_impedance(self):
-        sql = '''
-                WITH profiles AS (
-                    SELECT a.way_id, array_agg(a.elevation) AS elevation, max(length_m) AS sub_link_length,
-                        max(b.geom) AS geom        
-                    FROM {0} a
-                    JOIN ways b ON a.way_id::bigint = b.id
-                    GROUP by a.way_id
-                )
-                
-                SELECT a.elevation::text, sub_link_length, c.imp AS s_imp, c.rs_imp, a.geom    
-                FROM profiles a,
-                LATERAL compute_impedances(a.elevation, sub_link_length, {1}) c;            
-            '''.format(self.profile_table, self.impedance_step)
-        gdf = db.select(query=sql, return_type='geodataframe')
-        if self.trim_decimals:
-            gdf.geometry = self.trim(gdf)
-        return gdf
+        sql_create_table = '''
+            DROP TABLE IF EXISTS average_slopes_{0};
+            CREATE TABLE average_slopes_{0} 
+            (
+                id bigint,
+                slope float
+            );'''.format(self.ways_table)
 
-    def write_zip(self):
-        zout = zipfile.ZipFile(self.filename + '.zip', "w", zipfile.ZIP_DEFLATED)
-        zout.write(self.filename)
-        zout.close()
-        remove(self.filename)
+        cur.execute(sql_create_table)
+        conn.commit()
+        cnt = 0
+        for i in self.ids:         
+            cnt = cnt + 1 
+            if (cnt/self.batch_size).is_integer():
+                print('Compute slopes for %s out of %s lines' % (cnt,self.total_cnt)) 
+                conn.commit()
 
-    def write_file(self, df):
-        if not self.raster:
-            # Enable not default FIONA drivers
-            if self.enable_driver:
-                gpd.io.file.fiona.drvsupport.supported_drivers[self.output_format] = 'rw'
-            df.to_file(self.filename, driver=self.output_format)
-        else:
-            self.write_raster(df)
+            sql_update_impedance = '''
+                INSERT INTO average_slopes_{0}(id, slope)
+                SELECT e.id, compute_average_slope(elevs, length_m, elevs_interval) 
+                FROM (SELECT * FROM elevs_{0} WHERE id  = {1}) e'''.format(self.ways_table, i)
 
-        if self.compress:
-            self.write_zip()
+            cur.execute(sql_update_impedance)
 
-    def write_raster(self, df):
-        # TODO: https://github.com/corteva/geocube/blob/master/geocube/geo_utils/geobox.py#L79
-        df.rename(columns={'geom': 'geometry'}, inplace=True)
-        cube = make_geocube(
-            df,
-            measurements=["elevation"],
-            resolution=(-0.0001, 0.0001)
-        )
-        cube.elevation.rio.to_raster(self.filename, bigtiff=self.bigtiff)
+        conn.commit()
+        cur.execute('ALTER TABLE average_slopes_{0} ADD PRIMARY KEY (id);'.format(self.ways_table))
+        conn.commit()
+        conn.close()
 
-# TODO: Move this to dedicated UnitTest class
-# Test...
+    def create_export_table(self):
+        db = Database(self.db_suffix)
+        conn = db.connect() 
+        cur = conn.cursor()
+        
+        sql_merge_tables = '''DROP TABLE IF EXISTS slope_profile_{0};   
+        CREATE TABLE slope_profile_{0} AS 
+        SELECT e.*, i.s_imp, i.rs_imp, s.slope, w.geom   
+        FROM elevs_{0} e
+        LEFT JOIN average_slopes_{0} s
+        ON e.id = s.id
+        LEFT JOIN impedances_{0} i 
+        ON e.id = i.id 
+        LEFT JOIN {0} w 
+        ON e.id = w.id;'''.format(self.ways_table) 
 
-# Enable timer
-# import time
-# start = time.time()
-
-# Initialize
-# p = Profiles()
-
-# Test elevation profiles
-# df = p.get_elevation(way_list=[31,47,48,49,9,15,20,11,16,17])
-# df = p.get_elevation()
-
-# Vector Output
-# p.output_format = 'PGDUMP'
-# p.check_drivers()
-# p.filename = '/opt/data/ways_profile_test.sql'
-# p.write_file(df)
-
-# p.output_format = 'GeoJSON'
-# p.check_drivers()
-# p.filename = '/opt/data/ways_profile_test.geojson'
-# p.write_file(df)
-
-# Raster Output
-# p.raster = True
-# p.filename = '/opt/data/ways_profile_test.tif'
-# p.write_file(df)
-
-# Test Impedance Calculation
-# p.profile_table = 'ways_profile_test'
-# df = p.get_impedance()
-
-# Vector Output
-# p.output_format = 'PGDUMP'
-# p.check_drivers()
-# p.filename = '/opt/data/ways_impedance_test.sql'
-# p.write_file(df)
-
-# Finish timmer
-# end = time.time()
-# print('Finished in ' + str(end - start) + ' seconds')
-
+        cur.execute(sql_merge_tables)
+        cur.execute('ALTER TABLE slope_profile_{0} ADD PRIMARY KEY (id);'.format(self.ways_table))
+        conn.commit()
+        conn.close()
+        
+slope_profiles = Profiles(db_suffix='',ways_table='ways', filter_ways='''WHERE class_id::text NOT IN(SELECT UNNEST(select_from_variable_container(\'excluded_class_id_cycling\')))''' )
+#slope_profiles = Profiles(db_suffix='',ways_table='ways', filter_ways='''LIMIT 100''')
+slope_profiles.elevation_profile()
+slope_profiles.compute_cycling_impedance()
+slope_profiles.compute_average_slope()
+slope_profiles.create_export_table()
