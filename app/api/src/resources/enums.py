eafdd97787c9c3fe7bfbd5eebd9ff4e07ef70fea
EPajares <elias.pajares@plan4better.de>
2022-03-02 11:05:39 +0100

    added further enums

--- app/api/src/resources/enums.py
+++ app/api/src/resources/enums.py
@@ -7,6 +7,43 @@ class VectorType(str, Enum):
     pbf = "pbf"
     mvt = "mvt"
 
+class AllowedVectorTables(str, Enum):
+    """Allowed Vector Tables Enums."""
+    sub_study_area = "basic.sub_study_area"
+
+class SQLReturnTypes(str, Enum):
+    """Allowed Vector Tables Enums."""
+    db_geobuf = f"""
+    WITH make_geobuf AS
+    (
+        %s
+    )
+    SELECT ST_AsGeobuf(g.*, 'geom')
+    FROM make_geobuf g;
+    """
+    geojson = f"""
+    WITH make_geojson AS 
+    (
+        %s
+    )
+    SELECT json_build_object
+    (
+        'type', 'FeatureCollection',
+        'features', json_agg(ST_AsGeoJSON(g.*)::json)
+    ) 
+    FROM make_geojson g; 
+    """
+    geobuf = f"""WITH make_geojson AS 
+    (
+        %s
+    )
+    SELECT json_build_object
+    (
+        'type', 'FeatureCollection',
+        'features', json_agg(ST_AsGeoJSON(g.*)::json)
+    ) 
+    FROM make_geojson g; 
+    """
 
 class MimeTypes(str, Enum):
     """Responses MineTypes."""
@@ -19,3 +56,9 @@ class MimeTypes(str, Enum):
     pbf = "application/x-protobuf"
     mvt = "application/x-protobuf"
     geobuf = "application/geobuf.pbf"
+
+class UploadFileTypes(str, Enum):
+    """Upload File Types."""
+
+    geojson = "application/geo+json"
+    zip = "application/zip" 
