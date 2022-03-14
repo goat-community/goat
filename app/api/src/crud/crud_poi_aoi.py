e3b8254f985224a257136a03451ad3f140b194f3
EPajares <elias.pajares@plan4better.de>
2022-03-14 15:34:12 +0100

    refactoring and bug fix

--- app/api/src/crud/crud_poi_aoi.py
+++ app/api/src/crud/crud_poi_aoi.py
@@ -1,25 +1,26 @@
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.sql import text
-from fastapi.encoders import jsonable_encoder
 from src.db import models
-from src.schemas.poi_aoi import POIAOIVisualization
-from src.resources.enums import SQLReturnTypes
+from src.resources.enums import CalculationTypes, SQLReturnTypes, ReturnType
 
 class CRUDPoiAoi:
     async def poi_aoi_visualization(
-        self, db: AsyncSession, *, obj_in: POIAOIVisualization, current_user: models.User, return_type: str = 'geojson'
+        self, db: AsyncSession, *, scenario_id: int, modus: CalculationTypes, current_user: models.User, return_type: ReturnType 
     ):
-        obj_in_data = jsonable_encoder(obj_in)
-        obj_in_data["user_id"] = current_user.id
-
-        if return_type in SQLReturnTypes._member_names_:
-            template_sql = SQLReturnTypes[return_type].value
+        template_sql = SQLReturnTypes[return_type.value].value
 
         sql = text(template_sql % 
-            """SELECT * FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :amenities, :modus, :active_upload_ids, :active_study_area_id)"""
+            """SELECT * FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :modus, :active_upload_ids, :active_study_area_id)"""
         )
-        result = await db.execute(sql, obj_in_data)
+        result = await db.execute(sql, 
+            {
+                "user_id": current_user.id, 
+                "scenario_id": scenario_id, 
+                "modus": modus.value, 
+                "active_upload_ids": current_user.active_data_upload_ids, 
+                "active_study_area_id": current_user.active_study_area_id
+            }
+        )
         return result.fetchall()[0][0] 
-
 
 poi_aoi = CRUDPoiAoi()
