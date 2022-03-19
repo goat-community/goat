2715813a15c78db64362b05864253628835d96b9
EPajares <elias.pajares@plan4better.de>
2022-03-19 17:51:49 +0100

    adjusted layer library table

--- app/api/src/db/models/layer_library.py
+++ app/api/src/db/models/layer_library.py
@@ -7,13 +7,24 @@ from sqlmodel import (
     ForeignKey,
     Integer,
     SQLModel,
-    Text
+    Text,
+    UniqueConstraint,
+    DateTime
 )
 from sqlalchemy.dialects.postgresql import JSONB
 if TYPE_CHECKING:
     from .data_upload import DataUpload
     from .scenario import Scenario
 
+class StyleLibrary(SQLModel, table=True):
+    __tablename__ = "style_library"
+    __table_args__ = {"schema": "customer"}
+
+    id: Optional[int] = Field(sa_column=Column(Integer, primary_key=True, autoincrement=True))
+    name: str = Field(sa_column=Column(Text(), nullable=False, index=True))
+    style: dict = Field(sa_column=Column(JSONB))
+    translations: dict = Field(sa_column=Column(JSONB))
+UniqueConstraint(StyleLibrary.__table__.c.name)
 
 class LayerLibrary(SQLModel, table=True):
     __tablename__ = "layer_library"
@@ -25,20 +36,40 @@ class LayerLibrary(SQLModel, table=True):
     access_token: Optional[str] = Field(sa_column=Column(Text))
     type: str = Field(sa_column=Column(Text(), nullable=False, index=True))
     map_attribution: Optional[str] = Field(sa_column=Column(Text))
-    attributes: Optional[dict] = Field(sa_column=Column(JSONB))
-    style_library_id: int = Field(
+    date: Optional[datetime] = Field(
+        sa_column=Column(Text)
+    )
+    source: Optional[str] = Field(
         sa_column=Column(
-            Integer,
-            ForeignKey("customer.style_library.id"),
+            Text,
+            ForeignKey("customer.layer_source.name", onupdate="CASCADE"),
             nullable=True,
         )
     )
-    
-class StyleLibrary(SQLModel, table=True):
-    __tablename__ = "style_library"
+    date_1: Optional[datetime] = Field(
+        sa_column=Column(Text)
+    )
+    source_1: Optional[str] = Field(
+        sa_column=Column(
+            Text,
+            ForeignKey("customer.layer_source.name", onupdate="CASCADE"),
+            nullable=True,
+        )
+    )
+    style_library_name: str = Field(
+        sa_column=Column(
+            Text,
+            ForeignKey("customer.style_library.name", onupdate="CASCADE"),
+            nullable=True,
+        )
+    )
+UniqueConstraint(LayerLibrary.__table__.c.name)
+
+
+class LayerSource(SQLModel, table=True):
+    __tablename__ = "layer_source"
     __table_args__ = {"schema": "customer"}
 
     id: Optional[int] = Field(sa_column=Column(Integer, primary_key=True, autoincrement=True))
     name: str = Field(sa_column=Column(Text(), nullable=False, index=True))
-    style: dict = Field(sa_column=Column(JSONB))
-    translations: dict = Field(sa_column=Column(JSONB))
+UniqueConstraint(LayerSource.__table__.c.name)
