5a24ad6a370b6cde5bed91f7568c9d6bc4efc123
EPajares <elias.pajares@plan4better.de>
2022-03-24 14:21:42 +0100

    cleaned

--- app/api/src/endpoints/v1/isochrones.py
+++ app/api/src/endpoints/v1/isochrones.py
@@ -92,13 +92,6 @@ async def calculate_reached_network(
    
     network = await crud.isochrone.calculate_reached_network(db=db, obj_in=obj_calculation)
 
-    if modus.value == CalculationTypes.comparison.value:
-        obj_calculation.modus = CalculationTypes.default.value
-        network_default = await crud.isochrone.calculate_reached_network(db=db, obj_in=obj_calculation)
-        obj_calculation.modus = CalculationTypes.scenario.value
-        network_scenario = crud.isochrone.calculate_reached_network(db=db, obj_in=obj_calculation)
-        network = network_default | network_scenario
-        
     return return_geojson_or_geobuf(json.JSONDecoder().decode(json.dumps(network)), return_type.value)
 
 
