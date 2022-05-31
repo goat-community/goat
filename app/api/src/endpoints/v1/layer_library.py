fa7047dfd21c647d6df8ce815e7aecbb39669449
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-05-31 14:02:08 +0430

    [FIX] Temporarily remove Response Model for read_layers @ layer_library endpoint

--- app/api/src/endpoints/v1/layer_library.py
+++ app/api/src/endpoints/v1/layer_library.py
@@ -1,13 +1,16 @@
-from fastapi import APIRouter, Depends, Body
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.endpoints import deps
-from src.db import models
 from typing import List
+
+from fastapi import APIRouter, Body, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src import crud
+from src.db import models
+from src.endpoints import deps
 
 router = APIRouter()
 
-@router.get("",response_model=List[models.LayerLibrary])
+
+@router.get("")
 async def read_layers(
     db: AsyncSession = Depends(deps.get_db),
     skip: int = 0,
@@ -17,20 +20,22 @@ async def read_layers(
     layers = await crud.layer_library.get_multi(db, skip=skip, limit=limit)
     return layers
 
+
 @router.get("/{layer_name}", response_model=models.LayerLibrary)
 async def read_layer_by_name(
     layer_name: str,
     db: AsyncSession = Depends(deps.get_db),
     current_user: models.User = Depends(deps.get_current_active_user),
 ):
-    layer = await crud.layer_library.get_by_key(db,key="name", value=layer_name)
+    layer = await crud.layer_library.get_by_key(db, key="name", value=layer_name)
     return layer
 
-router.post("", response_model=models.LayerLibrary)
+
+@router.post("", response_model=models.LayerLibrary)
 async def create_layer(
+    layer_in: models.LayerLibrary,
     db: AsyncSession = Depends(deps.get_db),
     current_user: models.User = Depends(deps.get_current_active_superuser),
-    layer_in: models.LayerLibrary = Body()
 ):
-    layer = crud.layer_library.create(db,obj_in=layer_in)
+    layer = crud.layer_library.create(db, obj_in=layer_in)
     return layer
