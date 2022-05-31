7752d9b0092bee254d4fa8fbe8cf9700133ebe54
Mohammed  Etemaddar <mohammad.etemaddar@gmail.com>
2022-05-31 09:08:28 +0430

    Add Read endpoint for layer library

---
+++ app/api/src/endpoints/v1/layer_library.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.endpoints import deps
+from src.db import models
+from typing import List
+from src import crud
+
+router = APIRouter()
+
+@router.get("",response_model=List[models.LayerLibrary])
+async def get_layers(
+    db: AsyncSession = Depends(deps.get_db),
+    skip: int = 0,
+    limit: int = 100,
+    current_user: models.User = Depends(deps.get_current_active_user),
+):
+    layers = await crud.layer_library.get_multi(db, skip=skip, limit=limit)
+    return layers
