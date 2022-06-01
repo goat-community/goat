dd1a7ba8d6cc59d693def780e448744cfc41196b
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-06-02 00:44:46 +0430

    [ADD] Validations for CreateLayerLibrary schema
    validation for url, legend_urls, style_library_name vs the field:type
    Issue: #1328

--- app/api/src/schemas/layer_library.py
+++ app/api/src/schemas/layer_library.py
@@ -1,23 +1,53 @@
 from src.db import models
+from pydantic import root_validator
 
 
 class CreateLayerLibrary(models.LayerLibrary):
+    @root_validator
+    def urls_emptiness_vs_type(cls, values):
+        # according to the issue: https://github.com/goat-community/goat/issues/1328
+        url, the_type = values.get("url"), values.get("type")
+        url_mandatory_types = ("WMS", "WMS", "OSM", "BING")
+        if the_type in url_mandatory_types and not url:
+            raise ValueError("url should not be empty for this layer type.")
+
+        return values
+
+    @root_validator
+    def legend_urls_emptiness_vs_type(cls, values):
+        # according to the issue: https://github.com/goat-community/goat/issues/1328
+        legend_urls, the_type = values.get("legend_urls"), values.get("type")
+        legend_url_mandatory_types = ("WMS",)
+        if the_type in legend_url_mandatory_types and not legend_urls:
+            raise ValueError("legend_urls should not be empty for this layer type.")
+
+        return values
+
+    @root_validator
+    def style_library_name_emptiness_vs_type(cls, values):
+        # according to the issue: https://github.com/goat-community/goat/issues/1328
+        style_library_name, the_type = values.get("style_library_name"), values.get("type")
+        legend_url_mandatory_types = ("MVT", "GEOBUF", "WFS")
+        if the_type in legend_url_mandatory_types and not style_library_name:
+            raise ValueError("style_library_name should not be empty for this layer type.")
+
+        return values
+
     class Config:
         schema_extra = {
             "example": {
                 "url": "https://{a-c}.basemaps.somecdn.com/dark_all/{z}/{x}/{y}.png",
-                "legend_urls": "",
+                "legend_urls": None,
                 "special_attribute": {"imagery_set": "Aerial"},
                 "access_token": "some_token",
                 "map_attribution": "Attribution to the source",
-                "date": "",
-                "source": "",
-                "date_1": "",
-                "source_1": "",
-                "style_library_name": "",
-                "max_resolution": "",
-                "min_resolution": "",
-                "id": 6,
+                "date": None,
+                "source": None,
+                "date_1": None,
+                "source_1": None,
+                "style_library_name": None,
+                "max_resolution": None,
+                "min_resolution": None,
                 "name": "test",
                 "type": "BING",
             }
