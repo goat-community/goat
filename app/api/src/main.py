dac152276b274cc29947c3a8e7c92d37932dfecf
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-06-02 12:54:33 +0430

    [ADD] details on database already_exists exception Folloing issue #1336 the details has extracted from IntegrityError exception. Of course there is a #TODO to replace it with a more clear message.

--- app/api/src/main.py
+++ app/api/src/main.py
@@ -1,7 +1,7 @@
 import os
 
 import sentry_sdk
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 from sqlalchemy.exc import IntegrityError
@@ -76,5 +76,9 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 @app.exception_handler(IntegrityError)
 async def item_already_exists_handler(request: Request, exc: IntegrityError):
     return JSONResponse(
-        status_code=409, content={"detail": "object with a unique field already exists."}
+        status_code=409,
+        content={
+            "message": "object with a unique field already exists.",
+            "detail": str(exc.__dict__.get("orig")),
+        },
     )
