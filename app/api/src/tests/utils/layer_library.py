7ede2550f132a73ab24ab30665ddaf008ef000f1
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-06-06 10:45:00 +0200

    [FIX] Move layer_library generator into the body When random_layer generator is in the arguments, python caches the arguments for the second time call.

--- app/api/src/tests/utils/layer_library.py
+++ app/api/src/tests/utils/layer_library.py
@@ -8,7 +8,8 @@ from src.schemas.layer_library import request_examples
 
 async def create_random_layer_library(
     db: AsyncSession,
-    layer_library_in: CreateLayerLibrary = request_examples["single_layer_library"](),
 ) -> models.LayerLibrary:
+    layer_library_in = request_examples.single_layer_library
+    layer_library_in = CreateLayerLibrary(**layer_library_in)
     layer = await crud.layer_library.create(db=db, obj_in=layer_library_in)
     return layer
