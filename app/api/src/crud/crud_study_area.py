f7939224bed38b3efc54e02c5cb11facec7ea070
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-06-09 06:55:15 +0200

    [ADD] Tests for study area endpoints. :testtube:

--- app/api/src/crud/crud_study_area.py
+++ app/api/src/crud/crud_study_area.py
@@ -1,9 +1,20 @@
+from typing import Any, List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.crud.base import CRUDBase
 from src.db import models
 
+from .base import ModelType
+
 
 class CRUDStudyArea(CRUDBase[models.StudyArea, models.StudyArea, models.StudyArea]):
-    pass
+    async def get_first(self, db: AsyncSession) -> ModelType:
+        all_study_areas = await self.get_all(db=db)
+        if all_study_areas:
+            return all_study_areas[0]
+        else:
+            return None
 
 
 study_area = CRUDStudyArea(models.StudyArea)
