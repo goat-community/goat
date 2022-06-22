42e8585ad64aa4365bc7c0ed8759ddd5d757b310
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-06-22 21:35:53 +0200

    [TEST] add test for interpolation :alembic:

--- app/api/src/exts/cpp/tests/isochrone_surface.py
+++ app/api/src/exts/cpp/tests/isochrone_surface.py
@@ -1,12 +1,14 @@
 import asyncio
+import time
 
 import cppimport
+import matplotlib.pyplot as plt
+import numpy as np
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy.orm import sessionmaker
+from scipy.interpolate import NearestNDInterpolator
 
 from src.crud import isochrone
 from src.db.session import async_session
-from src.endpoints.deps import get_db
 from src.exts.cpp.bind import split_edges_isochrone
 from src.schemas.isochrone import IsochroneSingle, IsochroneTypeEnum
 
@@ -16,7 +18,7 @@ isochrone_cpp = cppimport.imp("src.exts.cpp.src.isochrone")
 async def get_split_network():
     single_isochrone = {
         "minutes": 20,
-        "speed": 3,
+        "speed": 5,
         "n": 2,
         "modus": "default",
         "x": 11.5696284,
@@ -27,18 +29,50 @@ async def get_split_network():
     }
     obj_in = IsochroneSingle(**single_isochrone)
     obj_in_data = jsonable_encoder(obj_in)
+
     db = async_session()
+    read_network_start = time.time()
     (
         edges_network,
         starting_id,
         distance_limits,
         obj_starting_point,
     ) = await isochrone.read_network(db, IsochroneTypeEnum.single.value, obj_in, obj_in_data)
+    read_network_stop = time.time()
+    print("Read network took ", (read_network_stop - read_network_start), " seconds.")
     await db.close()
+
     split_length = 20
 
     result = split_edges_isochrone(edges_network, starting_id, distance_limits, split_length)
+
     return result
 
 
-a = asyncio.run(get_split_network())
+def interpolate():
+    start = time.time()
+    a = asyncio.run(get_split_network())
+    X = np.arange(start=int(a.boundry.min_x), stop=int(a.boundry.max_x), step=20)
+    Y = np.arange(start=int(a.boundry.min_y), stop=int(a.boundry.max_y), step=20)
+    X, Y = np.meshgrid(X, Y)  # 2D grid for interpolation
+    points = np.array(a.points)
+    transposed = points.transpose()
+    x = transposed[0]
+    y = transposed[1]
+    z = np.array(a.costs)
+    interpolate_start = time.time()
+    interpolate_function = NearestNDInterpolator(list(zip(x, y)), z)
+    Z = interpolate_function(X, Y)
+    interpolate_stop = time.time()
+    print("Inter polate time is ", (interpolate_stop - interpolate_start), " seconds.")
+    end = time.time()
+    print("Took ", (end - start), " seconds.")
+    plt.pcolormesh(X, Y, Z, shading="nearest")
+    # plt.plot(x, y, "ok", label="input point")
+    plt.legend()
+    plt.colorbar()
+    plt.axis("equal")
+    plt.savefig("NearestNDInterpolate.png")
+
+
+interpolate()
