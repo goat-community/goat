1abae3c52e37eea7ef6d98090e24291be39cb12d
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-06-29 16:16:51 +0200

    [TEST] add CRUD tests for style library endpoints.

--- app/api/src/tests/api/api_v1/test_layer_library.py
+++ app/api/src/tests/api/api_v1/test_layer_library.py
@@ -7,7 +7,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import settings
 from src.schemas.layer_library import request_examples
-from src.tests.utils.layer_library import create_random_layer_library
+from src.tests.utils.layer_library import (
+    create_random_layer_library,
+    create_random_style_library,
+)
 
 pytestmark = pytest.mark.asyncio
 
@@ -93,6 +96,93 @@ async def test_delete_layer_library(
 
     r = await client.delete(
         f"{settings.API_V1_STR}/config/layers/library/{random_layer.name}",
+        headers=superuser_token_headers,
+    )
+
+    assert r.status_code == 404
+
+
+async def test_read_styles_list(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    await create_random_style_library(db=db)
+    await create_random_style_library(db=db)
+    r = await client.get(
+        f"{settings.API_V1_STR}/config/layers/library/styles", headers=superuser_token_headers
+    )
+    assert 200 <= r.status_code < 300
+    styles = r.json()
+    assert len(styles) > 1
+
+
+async def test_get_style_library_by_name(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    random_style = await create_random_style_library(db=db)
+    r = await client.get(
+        f"{settings.API_V1_STR}/config/layers/library/styles/{random_style.name}",
+        headers=superuser_token_headers,
+    )
+    assert 200 <= r.status_code < 300
+    retrieved_style = r.json()
+    assert retrieved_style.get("name") == random_style.name
+
+
+async def test_create_style_library(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    random_style = request_examples.single_style_library
+    r = await client.post(
+        f"{settings.API_V1_STR}/config/layers/library/styles",
+        headers=superuser_token_headers,
+        json=random_style,
+    )
+    assert 200 <= r.status_code < 300
+    retrieved_style = r.json()
+    assert retrieved_style.get("name") == random_style.get("name")
+    assert retrieved_style.get("id")
+
+
+async def test_update_style_library(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    random_style = await create_random_style_library(db=db)
+    style_name = random_style.name
+    random_style.name += "_updated"
+    r = await client.put(
+        f"{settings.API_V1_STR}/config/layers/library/styles/{style_name}",
+        headers=superuser_token_headers,
+        json=jsonable_encoder(random_style),
+    )
+    assert 200 <= r.status_code < 300
+    retrieved_style = r.json()
+    assert retrieved_style.get("name") == random_style.name
+    assert retrieved_style.get("id")
+
+
+async def test_delete_style_library(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    random_style = await create_random_style_library(db=db)
+    r = await client.delete(
+        f"{settings.API_V1_STR}/config/layers/library/styles/{random_style.name}",
+        headers=superuser_token_headers,
+    )
+
+    assert 200 <= r.status_code < 300
+
+    # Try to get
+    r = await client.get(
+        f"{settings.API_V1_STR}/config/layers/library/styles/{random_style.name}",
+        headers=superuser_token_headers,
+    )
+
+    assert r.status_code == 404
+
+    # Try to delete again
+
+    r = await client.delete(
+        f"{settings.API_V1_STR}/config/layers/library/styles/{random_style.name}",
         headers=superuser_token_headers,
     )
 
--- app/api/src/tests/utils/layer_library.py
+++ app/api/src/tests/utils/layer_library.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import crud
 from src.db import models
-from src.schemas import CreateLayerLibrary
+from src.schemas import CreateLayerLibrary, CreateStyleLibrary
 from src.schemas.layer_library import request_examples
 
 
@@ -13,3 +13,12 @@ async def create_random_layer_library(
     layer_library_in = CreateLayerLibrary(**layer_library_in)
     layer = await crud.layer_library.create(db=db, obj_in=layer_library_in)
     return layer
+
+
+async def create_random_style_library(
+    db: AsyncSession,
+) -> models.StyleLibrary:
+    style_library_in = request_examples.single_style_library
+    style_library_in = CreateStyleLibrary(**style_library_in)
+    style = await crud.style_library.create(db=db, obj_in=style_library_in)
+    return style
