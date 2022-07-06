e389e03a5451773e417a6967d1053a4fef2892e4
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-07-06 18:40:03 +0200

    [TEST] StaticLayer READ, CREATE, UPDATE tests

---
+++ app/api/src/tests/api/api_v1/test_static_layers.py
@@ -0,0 +1,65 @@
+from typing import Dict
+
+import pytest
+from fastapi.encoders import jsonable_encoder
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.config import settings
+from src.tests.utils.static_layer import create_static_layer
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_read_static_layer(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    static_layer = await create_static_layer(db=db)
+    r = await client.get(
+        f"{settings.API_V1_STR}/config/layers/vector/static/{static_layer.id}",
+        headers=superuser_token_headers,
+    )
+    assert 200 <= r.status_code < 300
+    data_frame = r.json().get("data_frame")
+    assert len(data_frame) > 1
+
+
+async def test_create_static_layer(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    files = {
+        "upload_file": (
+            "zip_shapefile.zip",
+            open("/app/src/data/zip_shapefile.zip", "rb"),
+            "application/zip",
+        )
+    }
+    r = await client.post(
+        f"{settings.API_V1_STR}/config/layers/vector/static",
+        headers=superuser_token_headers,
+        files=files,
+    )
+    assert 200 <= r.status_code < 300
+    retrieved_layer = r.json()
+    assert retrieved_layer.get("id")
+
+
+async def test_update_static_layer(
+    client: AsyncClient, superuser_token_headers: Dict[str, str], db: AsyncSession
+) -> None:
+    static_layer = await create_static_layer(db=db)
+    files = {
+        "upload_file": (
+            "zip_shapefile.zip",
+            open("/app/src/data/zip_shapefile.zip", "rb"),
+            "application/zip",
+        )
+    }
+    r = await client.put(
+        f"{settings.API_V1_STR}/config/layers/vector/static/{static_layer.id}",
+        headers=superuser_token_headers,
+        files=files,
+    )
+    assert 200 <= r.status_code < 300
+    retrieved_layer = r.json()
+    assert retrieved_layer.get("id") == static_layer.id
---
+++ app/api/src/tests/utils/static_layer.py
@@ -0,0 +1,25 @@
+import geopandas
+
+from src import crud
+from src.db import models
+from src.db.session import legacy_engine
+from src.utils import generate_static_layer_table_name
+
+
+async def create_static_layer(db):
+    data_frame = geopandas.read_file("/app/src/data/zip_shapefile.zip")
+    users = await crud.user.get_all(db)
+    static_layer = models.StaticLayer(
+        user_id=users[0].id, table_name=generate_static_layer_table_name()
+    )
+    # Save Data Frame to Database
+    data_frame.to_postgis(
+        name=static_layer.table_name,
+        con=legacy_engine.connect(),
+        schema="extra",
+        if_exists="fail",
+        index=True,
+    )
+    # Create Static Layer DB Object
+    static_layer = await crud.static_layer.create(db, obj_in=static_layer)
+    return static_layer
