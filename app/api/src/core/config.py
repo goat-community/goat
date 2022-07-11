a45c5728db3f24f352a27c0a039607b9b8264ab6
Majk Shkurti <majkshkurti@outlook.com>
2022-07-11 13:47:03 +0200

    [FIX] CI/CD

--- app/api/src/core/config.py
+++ app/api/src/core/config.py
@@ -23,7 +23,7 @@ class Settings(BaseSettings):
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["dashboard.plan4better.de"]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["https://dashboard.plan4better.de"]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
