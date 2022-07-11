5eadbe4422d03b0a384a66f0725008082b0c15e7
Majk Shkurti <majkshkurti@outlook.com>
2022-07-11 13:26:45 +0200

    [FIX] CI/CD BACKEND_CORS_ORIGINS env

--- app/api/src/core/config.py
+++ app/api/src/core/config.py
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
-
+    
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
