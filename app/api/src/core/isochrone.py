0b93da94a35d1c383a9457e631012986e9f6f449
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-07-15 13:23:07 +0200

    [ADD] Licence & Citation to isochrone.cpp

--- app/api/src/core/isochrone.py
+++ app/api/src/core/isochrone.py
@@ -1,3 +1,24 @@
+"""
+Translated from: https://github.com/goat-community/goat/blob/6b1dcbf592b5f59958998ae787059c26d29e19db/app/api/src/exts/cpp/src/isochrone.cpp
+PGR-GNU*****************************************************************
+File: isochrone.cpp
+Copyright (c) 2021 Majk Shkurti, <majk.shkurti@plan4better.de>
+Copyright (c) 2020 Vjeran Crnjak, <vjeran@crnjak.xyz>
+------
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ ********************************************************************PGR-GNU*
+"""
+
 from collections import defaultdict, deque
 from math import sqrt
 
