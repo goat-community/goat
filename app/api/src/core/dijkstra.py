93bd936341c08a73e233ae0618f2ddbec5a960e1
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-07-21 16:40:07 +0200

    [ADD] new python dijkstra class :warning: not tested

---
+++ app/api/src/core/dijkstra.py
@@ -0,0 +1,100 @@
+from collections import deque
+
+import numpy as np
+from scipy.sparse import coo_matrix, csr_matrix
+
+
+class Dijkstra:
+    def generate_sparse_graph(self):
+        """
+        Generate Sparse Graph of edges and their costs (sources, targets, costs)
+        """
+        data_edges = self.data_edges
+        row = np.concatenate([data_edges["source"], data_edges["target"]])
+        col = np.concatenate([data_edges["target"], data_edges["source"]])
+        cost_data = np.concatenate([data_edges["cost"], data_edges["reverse_cost"]])
+        sparse_graph = csr_matrix((cost_data, (row, col)), dtype=np.double)
+        unvisited_vertexes = set(row)
+        return sparse_graph, unvisited_vertexes
+
+    def calculate_target_costs(self, node_id, calculator_id):
+        """
+        Calculate costs for adjacent vertexes
+        """
+        sparse_graph = self.sparse_graph
+        targets = coo_matrix(sparse_graph.getrow(node_id))
+        self.unvisited_vertexes.remove(node_id)
+        for cost, target_id in zip(targets.data, targets.col):
+            distance = self.distances[node_id] + cost
+            if distance < self.distances[target_id]:
+                self.distances[target_id] = distance
+                self.calculator[target_id] = calculator_id
+                self.to_visit.append(target_id)
+            else:
+                if self.calculator[target_id] < calculator_id:
+                    self.unvisited_vertexes.remove(target_id)
+
+    def walk(self, node_id, unvisited_vertexes, calculator_id):
+        """
+        Walk through adjacent vertexes to the end
+        Recursive function
+        """
+        sparse_graph = self.sparse_graph
+        targets = coo_matrix(sparse_graph.getrow(node_id))
+        for node_id_ in targets.col:
+            self.calculate_target_costs(node_id, calculator_id)
+            if node_id_ not in self.unvisited_vertexes:
+                if self.distances[node_id_] < self.distance_limit:
+                    self.walk(node_id_, unvisited_vertexes, calculator_id)
+                else:
+                    unvisited_vertexes.remove(node_id)
+
+    def dijkstra(self):
+        """
+        Calculate distances with walking through Graph WITH recursive function
+        """
+        calculator_id = 0
+        for start_vertex in self.start_vertexes:
+            calculator_id += 1
+            unvisited_vertexes = self.unique_vertexes
+            node_id = start_vertex
+            self.distances[start_vertex] = 0
+            self.walk(node_id, unvisited_vertexes, calculator_id)
+
+        return self.distances
+
+    def dijkstra2(self):
+        """
+        Calculate distances with walking through Graph WITHOUT recursive function
+        """
+        calculator_id = 0
+        for start_vertex in self.start_vertexes:
+
+            calculator_id += 1
+            unvisited_vertexes = self.unique_vertexes
+            self.to_visit = deque(start_vertex)
+            self.distances[start_vertex] = 0
+            while self.to_visit:
+                node_id = self.to_visit.popleft()
+                if node_id not in unvisited_vertexes:
+                    if self.distances[node_id] < self.distance_limit:
+                        self.calculate_target_costs(node_id, calculator_id)
+                    else:
+                        unvisited_vertexes.remove(node_id)
+        return self.distances
+
+    def __init__(self, data_edges, start_vertexes, distance_limit):
+        self.data_edges = data_edges
+        self.start_vertexes = start_vertexes
+        self.distance_limit = distance_limit
+        self.distances = {}
+        self.calculator = {}
+        self.sparse_graph, self.unique_vertexes = self.generate_sparse_graph()
+        for vertex in self.unique_vertexes:
+            self.distances[vertex] = np.inf
+            self.calculator[vertex] = 0
+
+
+def dijkstra(data_edges, start_vertexes, distance_limit):
+    d = Dijkstra(data_edges, start_vertexes, distance_limit)
+    return d.dijkstra2()
