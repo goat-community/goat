b9b2c09ba4eebbdb96c70cd58bfb18a879edd415
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-08-15 16:25:53 +0200

    [FIX] opportunity_config request exmaples - Convert to class to have random category name

--- app/api/src/schemas/opportunity_config.py
+++ app/api/src/schemas/opportunity_config.py
@@ -1,20 +1,26 @@
+from random import random
 from src.db import models
+from src.tests.utils.utils import random_lower_string
 
 
-request_examples = {
-    "opportunity_study_area_config": {
-        "sensitivity": None,
-        "multiple_entrance": True,
-        "opportunity_group_id": 14,
-        "category": "subway_entrance",
-        "icon": "fas fa-train-subway-tunnel",
-        "color": ["#E182A5"],
-        "study_area_id": 83110000,
-        "is_active": False,
-    }
-}
+class RequestExample:
+    @property
+    def oportunity_study_area_config(self):
+        return {
+            "sensitivity": None,
+            "multiple_entrance": True,
+            "opportunity_group_id": 14,
+            "category": "test_" + random_lower_string(),
+            "icon": "fas fa-train-subway-tunnel",
+            "color": ["#E182A5"],
+            "study_area_id": 83110000,
+            "is_active": False,
+        }
+
+
+request_examples = RequestExample()
 
 
 class CreateOpportunityStudyAreaConfig(models.OpportunityStudyAreaConfig):
     class Config:
-        schema_extra = {"example": request_examples["opportunity_study_area_config"]}
+        schema_extra = {"example": request_examples.oportunity_study_area_config}
