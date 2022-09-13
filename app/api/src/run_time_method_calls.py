598a391f69dcb7b1ca667f4c1d324519654eecae
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-09-14 00:04:36 +0430

    [ADD] first run call for transit

--- app/api/src/run_time_method_calls.py
+++ app/api/src/run_time_method_calls.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 import httpx
+import requests
 
 from src.core.config import settings
 from src.tests.api.api_v1 import test_isochrones as isochrones
@@ -21,6 +22,32 @@ isochrone_request_examples = {
         "scenario": {"id": 0, "modus": "default"},
         "output": {"type": "grid", "steps": "12"},
     },
+    "transit": {
+        "mode": "transit",
+        "settings": {
+            "travel_time": "60",
+            "transit_modes": ["bus", "tram", "subway", "rail"],
+            "weekday": "0",
+            "access_mode": "walk",
+            "egress_mode": "walk",
+            "bike_traffic_stress": 4,
+            "from_time": 25200,
+            "to_time": 39600,
+            "max_rides": 4,
+            "max_bike_time": 20,
+            "max_walk_time": 20,
+            "percentiles": [5, 25, 50, 75, 95],
+            "monte_carlo_draws": 200,
+            "decay_function": {
+                "type": "logistic",
+                "standard_deviation_minutes": 12,
+                "width_minutes": 10,
+            },
+        },
+        "starting_point": {"input": [{"lat": 48.1502132, "lon": 11.5696284}]},
+        "scenario": {"id": 0, "modus": "default"},
+        "output": {"type": "grid", "resolution": "9"},
+    },
 }
 
 
@@ -28,7 +55,19 @@ async def run_calculate_isochrone_single_default(
     client: httpx.AsyncClient, superuser_token_headers: Dict[str, str]
 ) -> None:
     # data["routing_profile"] = "single_walking_default"
-    for mode in ["walking", "cycling"]:
+    for mode in ["transit", "walking"]:
+        if mode == "transit":
+            if settings.R5_API_URL:
+                try:
+                    print("check R5 for 3 seconds...")
+                    requests.get(settings.R5_API_URL, verify=False, timeout=3)
+                except:
+                    print("Coudn't reach R5! So skip call transit.")
+                    print()
+                    continue
+            else:
+                continue
+
         data = isochrone_request_examples[mode]
         print(mode)
         r = await client.post(
