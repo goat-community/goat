2692260989b39ddc3c4cbeb6a2eefc0cf4d6d784
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2022-09-29 08:54:54 +0330

    [REFACTOR] compute_oev_gueteklassen endpoint
    - add station_config to required arguments and remove the default config

--- app/api/src/crud/crud_indicator.py
+++ app/api/src/crud/crud_indicator.py
@@ -13,6 +13,7 @@ from rich import print
 from shapely import wkb
 from shapely.geometry import Polygon
 from shapely.ops import transform, unary_union
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
@@ -658,47 +659,12 @@ class CRUDIndicator:
         end_time,
         weekday,
         study_area_id,
+        station_config,
     ) -> FeatureCollection:
         """
         Calculate the OEV-Gueteklassen for a given time period and weekday.
         """
         # TODO: Use isochrone calculation instead of buffer
-        station_config = {
-            "groups": {
-                "0": "B",  # route_type: category of public transport route
-                "1": "A",
-                "2": "A",
-                "3": "C",
-                "4": "B",
-                "5": "B",
-                "6": "B",
-                "7": "B",
-                "11": "B",
-                "12": "B",
-            },
-            "time_frequency": [0, 4, 10, 19, 39, 60, 119],
-            "categories": [
-                {
-                    "A": 1,  # i.e. types of transports in category A are in transport stop category I
-                    "B": 1,
-                    "C": 2,
-                },
-                {"A": 1, "B": 2, "C": 3},
-                {"A": 2, "B": 3, "C": 4},
-                {"A": 3, "B": 4, "C": 5},
-                {"A": 4, "B": 5, "C": 6},
-                {"A": 5, "B": 6, "C": 6},
-            ],
-            "classification": {
-                "1": {300: "A", 500: "A", 750: "B", 1000: "C"},
-                "2": {300: "A", 500: "B", 750: "C", 1000: "D"},
-                "3": {300: "B", 500: "C", 750: "D", 1000: "E"},
-                "4": {300: "C", 500: "D", 750: "E", 1000: "F"},
-                "5": {300: "D", 500: "E", 750: "F"},
-                "6": {300: "E", 500: "F"},
-                "7": {300: "F"},
-            },
-        }
 
         time_window = (end_time - start_time) / 60
         stations = await db.execute(
@@ -744,7 +710,7 @@ class CRUDIndicator:
             if time_interval == len(station_config["time_frequency"]):
                 continue    # no category found
             station_category = station_config["categories"][time_interval - 1].get(station_group)
-     
+
             if not station_category:
                 continue
             # - find station classification based on category
