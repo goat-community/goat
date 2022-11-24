0937205aa9f40c00980457d0c1d03573cedfb841
Majk Shkurti <majkshkurti@outlook.com>
2022-11-24 08:40:04 +0100

    [FIX] lint

--- app/client/src/style/OlStyleDefs.js
+++ app/client/src/style/OlStyleDefs.js
@@ -1010,7 +1010,7 @@ export function poisAoisStyle(feature, resolution) {
       if (!poiIconConf || !poiIconConf.icon) {
         return [];
       }
-      
+
       let radiusBasedOnZoom = 20;
       let offsetInYDir = -20;
       poisShadowStyle.getImage().setScale(1);
