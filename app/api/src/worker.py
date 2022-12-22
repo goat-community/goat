7e2db83f9549b248f981af434abec2fe66c48751
Majk Shkurti <majkshkurti@outlook.com>
2022-12-22 15:23:48 +0100

    build: api

--- app/api/src/worker.py
+++ app/api/src/worker.py
@@ -5,7 +5,6 @@ from src.core.config import settings
 
 client_sentry = Client(settings.SENTRY_DSN)
 
-
 @celery_app.task(acks_late=True)
 def test_celery(word: str) -> str:
     return f"test task return {word}"
