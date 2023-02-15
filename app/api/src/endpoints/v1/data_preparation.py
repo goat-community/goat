09394741afaa2763d7c0cbb321ba074a93d09135
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2023-02-15 19:52:59 +0100

    [ADD] create_travel_time_matrices endpoint

--- app/api/src/endpoints/v1/data_preparation.py
+++ app/api/src/endpoints/v1/data_preparation.py
@@ -34,3 +34,17 @@ async def create_opportunity_matrices(
     calculation_object = await crud_compute_heatmap.create_calculation_object(parameters.calculation_resolution, buffer_size, parameters.bulk_id)
     await crud_compute_heatmap.compute_opportunity_matrix(isochrone_dto, calculation_object)
     return JSONResponse("Ok")
+
+@router.post("/traveltime-matrices")
+async def create_traveltime_matrices(
+    *,
+    db: AsyncSession = Depends(deps.get_db),
+    current_super_user: models.User = Depends(deps.get_current_active_superuser),
+    parameters: schemas.OpportunityMatrixParameters = Body(..., example=schemas.OpportunityMatrixParametersExample)):
+    
+    isochrone_dto = parameters.isochrone_dto
+    buffer_size = isochrone_dto.settings.speed * (isochrone_dto.settings.travel_time * 60)
+    crud_compute_heatmap = CRUDComputeHeatmap(db, current_super_user)
+    calculation_object = await crud_compute_heatmap.create_calculation_object(parameters.calculation_resolution, buffer_size, parameters.bulk_id)
+    await crud_compute_heatmap.compute_traveltime_active_mobility(isochrone_dto, calculation_object)
+    return JSONResponse("Ok")
