47066375b24dd9e17257b27322dd6cedeef97c9a
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2023-02-20 19:30:00 +0100

    [ADD] create opportunity matrices endpoint

--- app/api/src/endpoints/v1/data_preparation_tasks.py
+++ app/api/src/endpoints/v1/data_preparation_tasks.py
@@ -2,12 +2,22 @@ import asyncio
 
 from src.core.celery_app import celery_app
 
-from .data_preparation_connector import create_traveltime_matrices_async
+from .data_preparation_connector import (
+    create_opportunity_matrices_async,
+    create_traveltime_matrices_async,
+)
 
 
 @celery_app.task
 def create_traveltime_matrices_sync(current_super_user, parameters):
     loop = asyncio.get_event_loop()
     coroutine = create_traveltime_matrices_async(current_super_user=current_super_user, parameters=parameters)
+    loop.run_until_complete(coroutine)
+    return "Ok"
+
+@celery_app.task
+def create_opportunity_matrices_sync(current_super_user, parameters):
+    loop = asyncio.get_event_loop()
+    coroutine = create_opportunity_matrices_async(current_super_user=current_super_user, parameters=parameters)
     loop.run_until_complete(coroutine)
     return "Ok"
