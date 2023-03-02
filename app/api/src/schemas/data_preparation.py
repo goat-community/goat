a6bed5c9ae640aa1040f5d44895abd659c5f097e
Majk Shkurti <majkshkurti@outlook.com>
2023-03-02 20:18:38 +0100

    [WIP] crud_heatmap

--- app/api/src/schemas/data_preparation.py
+++ app/api/src/schemas/data_preparation.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 from .heatmap import HeatmapMode, HeatmapProfile
@@ -5,19 +7,19 @@ from .isochrone import IsochroneDTO
 
 
 class BulkIdParameters(BaseModel):
-    buffer_distance: int #in meters 
+    buffer_distance: int  # in meters
     study_area_ids: list[int]
 
 
 class OpportunityMatrixParameters(BaseModel):
-    bulk_id: str
-    calculation_resolution: int
-    isochrone_dto: IsochroneDTO
-
-class OpportunityMatrixParameters2(BaseModel):
     bulk_id: list[str]
     calculation_resolution: int
     isochrone_dto: IsochroneDTO
+    s3_folder: Optional[str] = ""
+
+class OpportunityMatrixParametersSingleBulk(OpportunityMatrixParameters):
+    bulk_id: str
+
 
 class ConnectivityMatrixParameters(BaseModel):
     mode: str
@@ -26,13 +28,14 @@ class ConnectivityMatrixParameters(BaseModel):
     max_time: int
 
 
-
 ConnectivityMatrixExample = {
     "mode": "walking",
     "profile": "standard",
     "study_area_id": 91620000,
-    "max_time": 20
+    "max_time": 20,
 }
+
+
 class OpportunityMatrixOldParameters(BaseModel):
     calculation_resolution: int
     isochrone_dto: IsochroneDTO
@@ -43,55 +46,68 @@ class OpportunityMatrixOldParameters(BaseModel):
     travel_time: float
     study_area_ids: list[int]
 
+
 class ConnectivityHeatmapParameters(BaseModel):
     mode: HeatmapMode
     profile: HeatmapProfile
     study_area_id: int
     max_travel_time: int
-    
-    
-    
+
 
 ConnectivityHeatmapParametersExample = {
     "mode": "walking",
     "profile": "standard",
     "study_area_id": 91620000,
-    "max_travel_time": 20
-}
-    
-    
-    
-BulkIdParametersExample = {
-    "buffer_distance": 1000,
-    "study_area_ids": [91620000]
+    "max_travel_time": 20,
 }
 
+
+BulkIdParametersExample = {"buffer_distance": 1000, "study_area_ids": [91620000]}
+
+
 OpportunityMatrixParametersExample = {
-    "bulk_id": ["861f8d55fffffff"],
-    "calculation_resolution": 10,
-    "isochrone_dto": {
-        "mode": "walking",
-        "settings": {
-            "travel_time": 20,
-            "speed": 5,
-            "walking_profile": "standard"
+    "active_mobility": {
+        "summary": "Opportunity Matrix for Active Mobility",
+        "value": {
+            "bulk_id": ["861f8d55fffffff"],
+            "calculation_resolution": 10,
+            "isochrone_dto": {
+                "mode": "walking",
+                "settings": {"travel_time": 20, "speed": 5, "walking_profile": "standard"},
+                "starting_point": {"input": [{"lat": 48.1502132, "lon": 11.5696284}]},
+                "scenario": {"id": 0, "modus": "default"},
+                "output": {"type": "grid", "resolution": 12},
+            },
+            "s3_folder": "test",
         },
-        "starting_point": {
-            "input": [
-            {
-                "lat": 48.1502132,
-                "lon": 11.5696284
-            }
-            ]
+    },
+    "motorized_transport": {
+        "summary": "Opportunity Matrix for Motorized Transport (Public Transport or Car)",
+        "value": {
+            "bulk_id": ["861f8d55fffffff"],
+            "calculation_resolution": 9,
+            "isochrone_dto": {
+                "mode": "transit",
+                "settings": {
+                    "travel_time": "60",
+                    "transit_modes": ["bus", "tram", "subway", "rail"],
+                    "weekday": "0",
+                    "access_mode": "walk",
+                    "egress_mode": "walk",
+                    "bike_traffic_stress": 4,
+                    "from_time": 25200,
+                    "to_time": 39600,
+                    "max_rides": 4,
+                    "max_bike_time": 20,
+                    "max_walk_time": 20,
+                    "percentiles": [5, 25, 50, 75, 95],
+                    "monte_carlo_draws": 200,
+                },
+                "starting_point": {"input": [{"lat": 48.1502132, "lon": 11.5696284}]},
+                "scenario": {"id": 0, "modus": "default"},
+                "output": {"type": "grid", "resolution": "9"},
+            },
+            "s3_folder": "test",
         },
-        "scenario": {
-            "id": 0,
-            "modus": "default"
-        },
-        "output": {
-            "type": "grid",
-            "resolution": 12
-        }
-        }
+    },
 }
-        
--- app/api/src/workers/heatmap_active_mobility.py
+++ app/api/src/workers/heatmap_active_mobility.py
@@ -5,14 +5,14 @@ from src.workers.celery_app import celery_app
 from .method_connector import (
     create_connectivity_matrices_async,
     create_opportunity_matrices_async,
-    create_traveltime_matrices_active_mobility_async,
+    create_traveltime_matrices_async,
 )
 
 
 @celery_app.task
-def create_traveltime_matrices_active_mobility_sync(current_super_user, parameters):
+def create_traveltime_matrices_sync(current_super_user, parameters):
     loop = asyncio.get_event_loop()
-    coroutine = create_traveltime_matrices_active_mobility_async(current_super_user=current_super_user, parameters=parameters)
+    coroutine = create_traveltime_matrices_async(current_super_user=current_super_user, parameters=parameters)
     loop.run_until_complete(coroutine)
     return "Ok"
 
