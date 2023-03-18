52da669d437adead1acec15bb896f66936596f2d
EPajares <elias.pajares@plan4better.de>
2023-03-18 21:10:34 +0100

    Added grouping for multi entrance POIs

--- app/api/src/crud/crud_poi_aoi.py
+++ app/api/src/crud/crud_poi_aoi.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.sql import text
 
 from src.db import models
-from src.resources.enums import CalculationTypes, ReturnType, SQLReturnTypes
+from src.resources.enums import ReturnType, SQLReturnTypes
 
 
 class CRUDPoiAoi:
@@ -12,15 +12,32 @@ class CRUDPoiAoi:
         *,
         scenario_id: int,
         current_user: models.User,
-        return_type: ReturnType
+        return_type: ReturnType,
+        full_attributes: bool = True,
+        grouped_multi_entrance: bool = False,
     ):
         _return_type = return_type.value
         if return_type == ReturnType.geobuf.value:
             _return_type = "db_geobuf"
         template_sql = SQLReturnTypes[_return_type].value
+
+        if full_attributes == True:
+            attributes = '*'
+        else:
+            attributes = 'id, category, name, geom'
+
+
+        if grouped_multi_entrance == True:
+            query = """SELECT row_number() over() AS id, category, name, ST_CENTROID(ST_COLLECT(geom)) AS geom 
+            FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id, :grouped_multi_entrance)
+            GROUP BY category, name"""
+        else: 
+            query = f"""SELECT {attributes} 
+            FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id, :grouped_multi_entrance)"""
+            
         sql = text(
             template_sql
-            % """SELECT * FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id)"""
+            % query
         )
         result = await db.execute(
             sql,
@@ -29,6 +46,7 @@ class CRUDPoiAoi:
                 "scenario_id": scenario_id,
                 "active_upload_ids": current_user.active_data_upload_ids,
                 "active_study_area_id": current_user.active_study_area_id,
+                "grouped_multi_entrance": grouped_multi_entrance,
             },
         )
         return result.fetchall()[0][0]
