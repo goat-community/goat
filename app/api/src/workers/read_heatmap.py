2b1cf72e2e5fce7e1878ec64f1c19d5a28ccf7f6
Majk Shkurti <majkshkurti@outlook.com>
2023-03-25 19:55:26 +0100

    [FIX] pt-oev-gueteklassen, pt-station-count

--- app/api/src/workers/read_heatmap.py
+++ app/api/src/workers/read_heatmap.py
@@ -1,7 +1,11 @@
 import asyncio
 
 from src.workers.celery_app import celery_app
-from src.workers.method_connector import read_heatmap_async
+from src.workers.method_connector import (
+    read_heatmap_async,
+    read_pt_station_count_async,
+    read_pt_oev_gueteklassen_async,
+)
 from src.core.config import settings
 
 
@@ -12,3 +16,20 @@ def read_heatmap_task(current_user, heatmap_settings):
     heatmap = loop.run_until_complete(read_heatmap_async(current_user, heatmap_settings))
     return heatmap
 
+
+@celery_app.task(time_limit=settings.CELERY_TASK_TIME_LIMIT)
+def read_pt_station_count_task(current_user, payload):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(read_pt_station_count_async(current_user, payload))
+    return result
+
+
+@celery_app.task(time_limit=settings.CELERY_TASK_TIME_LIMIT)
+def read_pt_oev_gueteklassen_task(current_user, payload):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    result = loop.run_until_complete(
+        read_pt_oev_gueteklassen_async(current_user, payload)
+    )
+    return result
