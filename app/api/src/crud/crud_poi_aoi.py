4ad3e078fb650e3e0898ae92739ce84344127f8f
EPajares <elias.pajares@plan4better.de>
2023-03-27 15:49:56 +0200

    Added logic for grouped POIs

--- app/api/src/crud/crud_poi_aoi.py
+++ app/api/src/crud/crud_poi_aoi.py
@@ -13,28 +13,23 @@ class CRUDPoiAoi:
         scenario_id: int,
         current_user: models.User,
         return_type: ReturnType,
-        full_attributes: bool = True,
-        grouped_multi_entrance: bool = False,
     ):
         _return_type = return_type.value
         if return_type == ReturnType.geobuf.value:
             _return_type = "db_geobuf"
         template_sql = SQLReturnTypes[_return_type].value
 
-        if full_attributes == True:
-            attributes = '*'
-        else:
-            attributes = 'id, category, name, geom'
+        attributes = 'uid, id, category, name, geom, opening_hours, street, housenumber, zipcode, min_zoom, max_zoom, edit_type'
 
-
-        if grouped_multi_entrance == True:
-            query = """SELECT row_number() over() AS id, category, name, ST_CENTROID(ST_COLLECT(geom)) AS geom 
-            FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id, :grouped_multi_entrance)
-            GROUP BY category, name"""
-        else: 
-            query = f"""SELECT {attributes} 
-            FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id, :grouped_multi_entrance)"""
-            
+        query = f"""
+        SELECT {attributes} 
+        FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id, FALSE)
+        UNION ALL 
+        SELECT NULL AS uid, row_number() over() AS id, category, name, ST_CENTROID(ST_COLLECT(ST_ClusterWithin(geom, 0.001))) AS geom,
+        NULL, NULL, NULL, NULL, 15, 20, NULL  
+        FROM basic.poi_aoi_visualization(:user_id, :scenario_id, :active_upload_ids, :active_study_area_id, TRUE)
+        GROUP BY category, name
+        """
         sql = text(
             template_sql
             % query
@@ -45,11 +40,10 @@ class CRUDPoiAoi:
                 "user_id": current_user.id,
                 "scenario_id": scenario_id,
                 "active_upload_ids": current_user.active_data_upload_ids,
-                "active_study_area_id": current_user.active_study_area_id,
-                "grouped_multi_entrance": grouped_multi_entrance,
+                "active_study_area_id": current_user.active_study_area_id
             },
         )
-        return result.fetchall()[0][0]
+        return result.fetchall()[0][0] 
 
 
 poi_aoi = CRUDPoiAoi()
