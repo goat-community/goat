61353e845a2fddc05d9d918b158d50a66809a2b6
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2023-04-05 08:10:26 +0000

    [ADD] normal_user_token_headers fixtures

--- app/api/src/tests/conftest.py
+++ app/api/src/tests/conftest.py
@@ -3,10 +3,12 @@ from typing import Dict, Generator
 
 import pytest_asyncio
 from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.session import async_session
 from src.main import app
 from src.tests.utils.utils import get_superuser_token_headers
+from src.tests.utils.user import get_user_token_headers
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -29,3 +31,7 @@ async def client() -> Generator:
 @pytest_asyncio.fixture(scope="module")
 async def superuser_token_headers(client: AsyncClient) -> Dict[str, str]:
     return await get_superuser_token_headers(client)
+
+@pytest_asyncio.fixture(scope="module")
+async def normal_user_token_headers(client: AsyncClient, db: AsyncSession) -> Dict[str, str]:
+    return await get_user_token_headers(client=client, db=db)
