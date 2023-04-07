8b4e57b1465877d84d886a6724ee367dd1e2f1fc
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2023-04-07 10:22:08 +0000

    [TEST] for isochrone calculations

--- app/api/src/tests/api/api_v1/test_isochrones.py
+++ app/api/src/tests/api/api_v1/test_isochrones.py
@@ -1,3 +1,4 @@
+import asyncio
 import mimetypes
 from collections import defaultdict
 from typing import Dict
@@ -198,16 +199,95 @@ async def test_convert_geojson_to_shapefile_and_xlsx(
         assert 200 <= r.status_code < 300
         assert r.headers["content-type"] == zip_mime_type
 
+test_payloads = {
+    "reached_network_default_geojson": {"mode":"walking","settings":{"travel_time":20,"speed":5,"walking_profile":"standard"},"starting_point":{"input":[{"lat":48.17414317972603,"lon":11.5058719230466}]},"scenario":{"id":0,"modus":"default"},"output":{"type":"grid","resolution":13}},
+    "reached_network_scenario_geojson": {"mode":"walking","settings":{"travel_time":20,"speed":5,"walking_profile":"standard"},"starting_point":{"input":[{"lat":48.150184567895735,"lon":11.55799774461109}]},"scenario":{"id":0,"modus":"scenario"},"output":{"type":"grid","resolution":13}},
+    "multi_count_pois_study_area": {"mode":"walking","settings":{"travel_time":20,"speed":5,"walking_profile":"standard"},"starting_point":{"input":["nursery"],"region_type":"study_area","region":["95","105"]},"scenario":{"id":0,"modus":"default"},"output":{"type":"grid","resolution":13}},
+    "multi_count_pois_draw":{"mode":"walking","settings":{"travel_time":20,"speed":5,"walking_profile":"standard"},"starting_point":{"input":["nursery"],"region_type":"draw","region":["POLYGON((11.497258198370304 48.16147474742104,11.547536667106236 48.163870221958376,11.540354028715388 48.13607583948297,11.497258198370304 48.12936459519315,11.477865074715016 48.14709955093468,11.497258198370304 48.16147474742104))"]},"scenario":{"id":0,"modus":"default"},"output":{"type":"grid","resolution":13}},
+    "outside_of_study_area_and_gets_error": {"mode":"walking","settings":{"travel_time":20,"speed":5,"walking_profile":"standard"},"starting_point":{"input":[{"lat":48.13128218306605,"lon":11.336367098415318}]},"scenario":{"id":0,"modus":"default"},"output":{"type":"grid","resolution":13}},
 
-# TODO: Calculate isochrone reached network default geojson
-# TODO: Calculate isochrone reached network scenario geojson
+}
+
+async def isochrone_set_request(
+    client: AsyncClient, superuser_token_headers: dict[str, str], data: dict
+) -> None:
+    r = await client.post(
+        f"{settings.API_V1_STR}/indicators/isochrone",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    return r
+
+async def isochrone_get_results(
+    client: AsyncClient, superuser_token_headers: dict[str, str], task_id: str
+) -> None:
+    r = await client.get(
+        f"{settings.API_V1_STR}/indicators/result/{task_id}",
+        headers=superuser_token_headers,
+        params={"return_type": "geojson"},
+    )
+    return r
+
+async def isochrone_test_base(
+    client: AsyncClient, superuser_token_headers: dict[str, str], data: dict
+) -> None:
+    task_request = await isochrone_set_request(client, superuser_token_headers, data)
+    assert task_request.status_code == 200
+    task_id = task_request.json()["task_id"]
+
+    for i in range(40):
+        task_results = await isochrone_get_results(client, superuser_token_headers, task_id)
+        assert task_results.status_code >= 200 and task_results.status_code < 300
+        if task_results.status_code == 200:
+            break
+        else:
+            await asyncio.sleep(1)
+
+    assert task_results.status_code == 200
+
+# Calculate isochrone reached network default geojson
+async def test_calculate_isochrone_reached_network_default_geojson(
+    client: AsyncClient, superuser_token_headers: dict[str, str]
+) -> None:
+    data = test_payloads["reached_network_default_geojson"]
+    await isochrone_test_base(client, superuser_token_headers, data)
+    
+# Calculate isochrone reached network scenario geojson
+async def test_calculate_isochrone_reached_network_scenario_geojson(
+    client: AsyncClient, superuser_token_headers: dict[str, str]
+) -> None:
+    data = test_payloads["reached_network_scenario_geojson"]
+    await isochrone_test_base(client, superuser_token_headers, data)
+
+# Calculate isochrone multi count pois draw
+async def test_calculate_isochrone_multi_count_pois_draw(
+    client: AsyncClient, superuser_token_headers: dict[str, str]
+) -> None:
+    data = test_payloads["multi_count_pois_draw"]
+    await isochrone_test_base(client, superuser_token_headers, data)
+    
+# Calculate isochrone multi count pois study area
+async def test_calculate_isochrone_multi_count_pois_study_area(
+    client: AsyncClient, superuser_token_headers: dict[str, str]
+) -> None:
+    data = test_payloads["multi_count_pois_study_area"]
+    await isochrone_test_base(client, superuser_token_headers, data)
+
 # TODO: Calculate isochrone reached network comparision geojson
 # TODO: Calculate isochrone reached network comparision geobuf
-# TODO: Calculate isochrone multi count pois draw
-# TODO: Calculate isochrone multi count pois study area
 # TODO: Calculate isochrone multi isochrone calculation with coordinates as start points
 # TODO: Calculate isochrone multi isochrone calculation with pois as start points
+#‌ TODO: Calculate isochrone multi isochrone calculation with pois and scenario
+
 # TODO: Isochrone export geojson
 # TODO: Isochrone export shapefile
 # TODO: Isochrone export xlsx
-# TODO: User calculates isochrone outside of study area and gets error
+
+
+# User calculates isochrone outside of study area and gets error
+async def test_calculate_isochrone_outside_of_study_area_and_gets_error(
+    client: AsyncClient, normaluser_token_headers: dict[str, str]
+) -> None:
+    data = test_payloads["outside_of_study_area_and_gets_error"]
+    task_request = await isochrone_set_request(client, normaluser_token_headers, data)
+    assert task_request.status_code == 403
