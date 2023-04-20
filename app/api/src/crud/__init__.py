78f84f1cfa7b313eaa370175290ec68d457b010c
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2023-04-20 08:58:58 +0000

    [ADD] crud sub_study_area to init

--- app/api/src/crud/__init__.py
+++ app/api/src/crud/__init__.py
@@ -16,7 +16,7 @@ from .crud_poi_aoi import poi_aoi
 from .crud_r5 import r5
 from .crud_role import role, user_role
 from .crud_static_layer import static_layer
-from .crud_study_area import study_area
+from .crud_study_area import study_area, sub_study_area
 from .crud_study_area_geostore import study_area_geostore
 from .crud_system import system
 from .crud_upload import data_upload, upload
