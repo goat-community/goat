4fd7e13ef5b51d8479d4de8eb2c1493ccf54968a
Majk Shkurti <majkshkurti@outlook.com>
2023-05-03 11:47:54 +0200

    add: config package

--- app/client-v2/.eslintrc.js
+++ app/client-v2/.eslintrc.js
@@ -1,10 +1,1 @@
-module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
-  },
-};
+module.exports = require("./packages/config/eslint-preset");
