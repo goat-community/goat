0fd09439792473787f742592f2d210b938e962d0
Mohammad Etemaddar <mohammad.etemaddar@gmail.com>
2023-05-22 08:19:57 +0000

    [REFACTOR] indicator results return type
    - Take IsochroneOutputType as return_type

--- app/api/src/endpoints/v1/indicators.py
+++ app/api/src/endpoints/v1/indicators.py
@@ -63,11 +63,15 @@ async def calculate_isochrone(
         await deps.check_user_owns_scenario(db, isochrone_in.scenario.id, current_user)
 
     if isochrone_in.is_single:
-        user_access_starting_point = await crud.user.user_study_area_starting_point_access(
-            db, current_user.id, isochrone_in.starting_point.input
+        user_access_starting_point = (
+            await crud.user.user_study_area_starting_point_access(
+                db, current_user.id, isochrone_in.starting_point.input
+            )
         )
         if not user_access_starting_point:
-            raise HTTPException(detail="User has no access to starting point", status_code=403)
+            raise HTTPException(
+                detail="User has no access to starting point", status_code=403
+            )
 
     study_area = await crud.user.get_active_study_area(db, current_user)
     study_area_bounds = study_area["bounds"]
@@ -75,11 +79,15 @@ async def calculate_isochrone(
     current_user = json.loads(current_user.json())
 
     if not settings.CELERY_BROKER_URL:
-        results = task_calculate_isochrone(isochrone_in, current_user, study_area_bounds)
+        results = task_calculate_isochrone(
+            isochrone_in, current_user, study_area_bounds
+        )
         return read_results(results)
 
     else:
-        task = task_calculate_isochrone.delay(isochrone_in, current_user, study_area_bounds)
+        task = task_calculate_isochrone.delay(
+            isochrone_in, current_user, study_area_bounds
+        )
         task_id = f"isochrone-{task.id}"
         return {"task_id": task_id}
 
@@ -147,7 +155,9 @@ async def calculate_heatmap(
         return {"task_id": task.id}
 
     else:
-        results = await read_heatmap_async(current_user=current_user, settings=heatmap_settings)
+        results = await read_heatmap_async(
+            current_user=current_user, settings=heatmap_settings
+        )
         return read_results(results)
 
 
@@ -169,7 +179,10 @@ async def count_pt_service_stations(
         le=86400,
     ),
     weekday: Optional[int] = Query(
-        description="Weekday (1 = Monday, 7 = Sunday) (Default: Monday)", default=1, ge=1, le=7
+        description="Weekday (1 = Monday, 7 = Sunday) (Default: Monday)",
+        default=1,
+        ge=1,
+        le=7,
     ),
     study_area_id: Optional[int] = Query(
         default=None, description="Study area id (Default: User active study area)"
@@ -179,7 +192,9 @@ async def count_pt_service_stations(
     Return the number of trips for every route type on every station given a time period and weekday.
     """
     if start_time >= end_time:
-        raise HTTPException(status_code=422, detail="Start time must be before end time")
+        raise HTTPException(
+            status_code=422, detail="Start time must be before end time"
+        )
 
     is_superuser = crud.user.is_superuser(current_user)
     if study_area_id is not None and not is_superuser:
@@ -208,7 +223,9 @@ async def count_pt_service_stations(
             payload=payload,
         )
     else:
-        results = await read_pt_station_count_async(current_user=current_user, payload=payload)
+        results = await read_pt_station_count_async(
+            current_user=current_user, payload=payload
+        )
         return return_geojson_or_geobuf(results, return_type="geobuf")
     return {"task_id": task.id}
 
@@ -217,7 +234,9 @@ async def count_pt_service_stations(
 async def calculate_oev_gueteklassen(
     *,
     current_user: models.User = Depends(deps.get_current_active_user),
-    params: CalculateOevGueteklassenParameters = Body(..., example=oev_gueteklasse_config_example),
+    params: CalculateOevGueteklassenParameters = Body(
+        ..., example=oev_gueteklasse_config_example
+    ),
 ):
     """
     ÖV-Güteklassen (The public transport quality classes) is an indicator for access to public transport.
@@ -225,7 +244,9 @@ async def calculate_oev_gueteklassen(
     The calculation in an automated process from the data in the electronic timetable (GTFS).
     """
     if params.start_time >= params.end_time:
-        raise HTTPException(status_code=422, detail="Start time must be before end time")
+        raise HTTPException(
+            status_code=422, detail="Start time must be before end time"
+        )
 
     is_superuser = crud.user.is_superuser(current_user)
 
@@ -248,7 +269,9 @@ async def calculate_oev_gueteklassen(
             payload=payload,
         )
     else:
-        results = await read_pt_oev_gueteklassen_async(current_user=current_user, payload=payload)
+        results = await read_pt_oev_gueteklassen_async(
+            current_user=current_user, payload=payload
+        )
         return return_geojson_or_geobuf(results, return_type="geobuf")
     return {"task_id": task.id}
 
@@ -257,7 +280,9 @@ async def calculate_oev_gueteklassen(
 async def get_indicators_result(
     task_id: str,
     current_user: models.User = Depends(deps.get_current_active_user),
-    return_type: ReturnType = Query(..., description="Return type of the response"),
+    return_type: IsochroneOutputType = Query(
+        ..., description="Return type of the response"
+    ),
 ):
     """Fetch result for given task_id"""
 
