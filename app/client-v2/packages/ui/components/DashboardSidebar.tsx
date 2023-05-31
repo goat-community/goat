f1f01847334da36593ccabaca6e5a5a479ba7d62
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-05-31 14:49:52 +0330

    fix: dashboard sidebar

--- app/client-v2/packages/ui/components/DashboardSidebar.tsx
+++ app/client-v2/packages/ui/components/DashboardSidebar.tsx
@@ -1,54 +1,41 @@
-import FolderIcon from "@mui/icons-material/Folder";
-import HelpIcon from "@mui/icons-material/Help";
-import HomeIcon from "@mui/icons-material/Home";
-import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
 import { Fade, Link, Stack, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import { useState } from "react";
 
-import { SIDEBAR_WIDTH, SIDEBAR_WIDTH_EXTENDED } from "../constants/sidebar";
 import { makeStyles } from "../lib/ThemeProvider";
-import { createIcon } from "./Icon";
 
-const { Icon } = createIcon({
-  help: HelpIcon,
-  home: HomeIcon,
-  settings: SettingsSuggestIcon,
-  folder: FolderIcon,
-});
-
-export type SidebarProps = {
-  items: { link: string; iconId: "home" | "help" | "folder" | "settings"; placeholder: string }[];
+export type DashboardSidebarProps = {
+  items: { link: string; icon: () => JSX.Element; placeholder: string }[];
+  width: number;
+  extended_width: number;
 };
 
-export function DashboardSidebar(props: SidebarProps) {
+export function DashboardSidebar(props: DashboardSidebarProps) {
   const { items } = props;
-  const { classes, cx } = useStyles();
+  const { classes, cx } = useStyles(props)();
 
   const [hover, setHover] = useState(false);
 
   const handleHover = () => {
-    setTimeout(() => {
-      setHover((currHover) => !currHover);
-    }, 100);
+    setHover((currHover) => !currHover);
   };
 
   return (
     <Box className={cx(classes.root)} onMouseEnter={handleHover} onMouseLeave={handleHover}>
-      {items?.map(({ link, iconId, placeholder }) => (
+      {items?.map(({ link, icon, placeholder }) => (
         <Box key={link} className={cx(classes.item, { [classes.itemHover]: hover })}>
           <Link href={link}>
             {hover ? (
               <Fade in={true}>
                 <Stack direction="row" spacing="14px" alignItems="center">
-                  <Icon iconId={iconId} />
+                  {icon()}
                   <Typography fontSize="14px" color="#103361">
                     {placeholder}
                   </Typography>
                 </Stack>
               </Fade>
             ) : (
-              <Icon iconId={iconId} size="large" />
+              <>{icon()}</>
             )}
           </Link>
         </Box>
@@ -57,32 +44,33 @@ export function DashboardSidebar(props: SidebarProps) {
   );
 }
 
-const useStyles = makeStyles({ name: { DashboardSidebar } })((theme) => ({
-  root: {
-    backgroundColor: theme.colors.palette.light.light,
-    cursor: "pointer",
-    width: SIDEBAR_WIDTH,
-    left: 0,
-    top: 0,
-    bottom: 0,
-    position: "fixed",
-    transition: "width 0.4s ease",
-    display: "flex",
-    flexDirection: "column",
-    "&:hover": {
-      width: SIDEBAR_WIDTH_EXTENDED,
+const useStyles = (props: DashboardSidebarProps) =>
+  makeStyles({ name: { DashboardSidebar } })((theme) => ({
+    root: {
+      backgroundColor: theme.colors.palette.light.light,
+      cursor: "pointer",
+      width: props.width,
+      left: 0,
+      top: 0,
+      bottom: 0,
+      position: "fixed",
+      transition: "width 0.4s ease",
+      display: "flex",
+      flexDirection: "column",
+      "&:hover": {
+        width: props.extended_width,
+      },
     },
-  },
-  item: {
-    height: 48,
-    marginBottom: 4,
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  itemHover: {
-    justifyContent: "flex-start",
-    padding: "0 15px",
-  },
-}));
+    item: {
+      height: 48,
+      marginBottom: 4,
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    itemHover: {
+      justifyContent: "flex-start",
+      padding: "0 15px",
+    },
+  }));
--- app/client-v2/packages/ui/constants/sidebar.ts
+++
@@ -1,2 +0,0 @@
-export const SIDEBAR_WIDTH = 52;
-export const SIDEBAR_WIDTH_EXTENDED = 182;
