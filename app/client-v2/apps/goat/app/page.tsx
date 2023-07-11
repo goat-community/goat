80fbe8c926310cb39c34385045bdb132de88cc22
Majk Shkurti <majkshkurti@outlook.com>
2023-07-11 18:38:47 +0200

    wip: auth client-v2

---
+++ app/client-v2/apps/goat/app/page.tsx
@@ -0,0 +1,18 @@
+import { getServerSession } from "next-auth/next";
+
+import { options } from "./api/auth/[...nextauth]/options";
+import UserCard from "./components/UserCard";
+
+export default async function Home() {
+  const session = await getServerSession(options);
+
+  return (
+    <>
+      {session ? (
+        <UserCard user={session?.user} pagetype={"Home"} />
+      ) : (
+        <h1 className="text-5xl">You Shall Not Pass!</h1>
+      )}
+    </>
+  );
+}
--- app/client-v2/apps/goat/pages/settings/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Settings = () => {
-  return <h1>Settings</h1>;
-};
-
-export default Settings;
