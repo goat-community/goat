41eb14496a44ee693e1cd19c47883e86b4ced3b6
Norayr Baghdasarov <norayr.baghdasarov@plan4better.de>
2023-07-12 18:37:45 +0400

    Create the map based on the JSON from project

--- app/client-v2/apps/goat/pages/index.tsx
+++ app/client-v2/apps/goat/pages/index.tsx
@@ -1,20 +1,22 @@
 import Head from "next/head";
 import React from "react";
 
-import CustomMap from "../components/Map";
+import MapByStyle from "../components/MapByStyle";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Dashboard</title>
+        <title>GOAT</title>
         <meta name="description" content="Data Dashboard" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Main Dashboard</h1>
-        <CustomMap />
+        <h1>Map Demo</h1>
+
+        {/*<CustomMap />*/}
+        <MapByStyle style="edge" />
       </main>
     </>
   );
