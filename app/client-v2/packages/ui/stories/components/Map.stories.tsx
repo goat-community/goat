65209dec7924f34b2fcf9aeea26fdcc700edcd0c
Norayr Baghdasarov <norayr.baghdasarov@plan4better.de>
2023-07-17 17:49:59 +0400

    Add layers in storyBook

---
+++ app/client-v2/packages/ui/stories/components/Map.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import MapByLayer from "../../components/MapByLayer";
+import { ThemeProvider } from "../theme";
+
+const meta: Meta<typeof MapByLayer> = {
+  component: MapByLayer,
+  tags: ["autodocs"],
+  argTypes: {
+    layer: {
+      options: ["aoi", "poi", "edge"],
+      control: false,
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: "",
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof MapByLayer>;
+
+export const AOI: Story = {
+  args: { layer: "aoi" },
+  parameters: {},
+};
+
+export const POI: Story = {
+  args: { layer: "poi" },
+  parameters: {},
+};
+
+export const EDGE: Story = {
+  args: { layer: "edge" },
+  parameters: {},
+};
