93c96226ccb2a54784b109d0fe2254ab7d3fa7c7
Norayr Baghdasarov <norayr.baghdasarov@plan4better.de>
2023-07-25 15:19:04 +0400

    Added demo jso data for preview map

--- app/client-v2/apps/goat/app/(dashboard)/content/ContentManagement.tsx
+++ app/client-v2/apps/goat/app/(dashboard)/content/ContentManagement.tsx
@@ -26,6 +26,7 @@ const ContentManagement = () => {
     size: string;
   } | null>(null);
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const [path, setPath] = useState<string[]>(["home"]);
 
   // Dumb Data
   // These are the rows of the table, it is only temporary for now
@@ -290,8 +291,6 @@ const ContentManagement = () => {
       link: "/home",
     },
   ];
-
-  const [path, setPath] = useState<string[]>(["home"]);
 
   // this is the Info Modal
   const modal = modalContent
