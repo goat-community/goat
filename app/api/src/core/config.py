2bb48d67a058e1300fe28491040c9f1e790d0435
EPajares <elias.pajares@plan4better.de>
2023-08-03 13:59:50 +0200

    refactor base class and cleaning

--- app/api/src/core/config.py
+++ app/api/src/core/config.py
@@ -89,9 +89,7 @@ class Settings(BaseSettings):
     ASYNC_SQLALCHEMY_DATABASE_URI: Optional[AsyncPostgresDsn] = None
 
     @validator("ASYNC_SQLALCHEMY_DATABASE_URI", pre=True)
-    def assemble_async_db_connection(
-        cls, v: Optional[str], values: Dict[str, Any]
-    ) -> Any:
+    def assemble_async_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
         return AsyncPostgresDsn.build(
@@ -140,9 +138,7 @@ class Settings(BaseSettings):
     @validator("EMAILS_ENABLED", pre=True)
     def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
         return bool(
-            values.get("SMTP_HOST")
-            and values.get("SMTP_PORT")
-            and values.get("EMAILS_FROM_EMAIL")
+            values.get("SMTP_HOST") and values.get("SMTP_PORT") and values.get("EMAILS_FROM_EMAIL")
         )
 
     FIRST_ORGANIZATION: str
@@ -231,6 +227,7 @@ class Settings(BaseSettings):
     GITHUB_ACCESS_TOKEN: Optional[str] = None
 
     COMPOSE_PROJECT_NAME: Optional[str] = None
+    SAMPLE_AUTHORIZATION = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI0OG80Z1JXelh3YXBTY3NTdHdTMXZvREFJRlNOa0NtSVFpaDhzcEJTc2kwIn0.eyJleHAiOjE2OTEwMDQ1NTYsImlhdCI6MTY5MTAwNDQ5NiwiYXV0aF90aW1lIjoxNjkxMDAyNjIzLCJqdGkiOiI1MjBiN2RhNC0xYmM0LTRiM2QtODY2ZC00NDU0ODY2YThiYjIiLCJpc3MiOiJodHRwczovL2Rldi5hdXRoLnBsYW40YmV0dGVyLmRlL3JlYWxtcy9tYXN0ZXIiLCJzdWIiOiI3NDRlNGZkMS02ODVjLTQ5NWMtOGIwMi1lZmViY2U4NzUzNTkiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJzZWN1cml0eS1hZG1pbi1jb25zb2xlIiwibm9uY2UiOiJjNGIzMDQ3Yi0xODVmLTQyOWEtOGZlNS1lNDliNTVhMzE3MzIiLCJzZXNzaW9uX3N0YXRlIjoiMzk5ZTc2NWMtYjM1MC00NDEwLTg4YTMtYjU5NDIyMmJkZDlhIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2Rldi5hdXRoLnBsYW40YmV0dGVyLmRlIl0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiIzOTllNzY1Yy1iMzUwLTQ0MTAtODhhMy1iNTk0MjIyYmRkOWEiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InA0YiJ9.mjywr9Dv19egsXwM1fK6g3sZ0trk87X0tEfK7oOizuBuCdkr6PZN1Eg58FCdjIgEBXqjltOWV43UIkXde4iPVa-KU5Q34Qjv6w0STa3Aq9vFbaUfSm_690qCdr8XSKMJUWQXWYwD2cjck5UCqf7-QqsF2Ab56i40_CJLZkJOi25WKIC855qPDi8BkJgh5eWoxobdyCbwJMEeoM-3QnxY5ikib5a2_AASEN3_5MYmT6-fvpW2t-MS6u4vtcG-WfqriK8YNoGPS2a1pFjLqQLHkM__j0O_t4wXP56x9yjkUdHCXqVcSlDvZYNWrv5CLqecqjOoliNMs6RTu9gV0Gr-cA"
 
     class Config:
         case_sensitive = True
--- app/api/src/crud/base.py
+++ app/api/src/crud/base.py
@@ -1,16 +1,22 @@
 from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
 
+from fastapi_pagination import Page, Params
+from fastapi_pagination.ext.sqlalchemy import paginate
 from pydantic import BaseModel
-from sqlalchemy import delete, func, or_
+from sqlalchemy import delete, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import RelationshipProperty, selectinload
+from sqlmodel import SQLModel
+from sqlmodel.sql.expression import Select
 
 from src.db.models._base_class import Base
+from src.schemas import OrderEnum
 
 ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
+T = TypeVar("T", bound=SQLModel)
 
 
 class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
@@ -22,33 +28,6 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         * `schema`: A Pydantic model (schema) class
         """
         self.model = model
-
-    def order_by(self, statement: select, ordering: str):
-        if not ordering:
-            return statement
-        orders = ordering.split(",")
-        for order in orders:
-            order_ = order
-            if order.startswith("-"):
-                order_ = order[1:]
-            if hasattr(self.model, order_):
-                attribute_order = getattr(self.model, order_)
-                if order.startswith("-"):
-                    attribute_order = attribute_order.desc()
-                statement = statement.order_by(attribute_order)
-
-        return statement
-
-    def search(self, statement: select, query: str):
-        if not hasattr(self.model.Config, "search_fields") or not query:
-            return statement
-        search_objects = []
-        for field in self.model.Config.search_fields:
-            column = getattr(self.model, field)
-            containes = column.ilike(query.lower())
-            search_objects.append(containes)
-        statement = statement.filter(or_(*search_objects))
-        return statement
 
     def extend_statement(self, statement: select, *, extra_fields: List[Any] = []) -> select:
         for field in extra_fields:
@@ -101,35 +80,37 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         self,
         db: AsyncSession,
         *,
-        skip: int = 0,
-        limit: int = 100,
+        query: T | Select[T] | None = None,
+        page_params: Params | None = None,
+        order_by: str | None = None,
+        order: OrderEnum | None = OrderEnum.ascendent,
         extra_fields: List[Any] = [],
-        ordering: str = None,
-        query: str = None,
-    ) -> List[ModelType]:
-        statement = select(self.model).offset(skip).limit(limit)
-        statement = self.extend_statement(statement, extra_fields=extra_fields)
-        statement = self.order_by(statement, ordering)
-        statement = self.search(statement, query)
-        result = await db.execute(statement)
-        return result.scalars().all()
+        search_text: dict | None = None,
+    ) -> Page[ModelType] | List[ModelType]:
+        if query is None:
+            query = select(self.model)
+        query = self.extend_statement(query, extra_fields=extra_fields)
 
-    async def get_multi_by_key(
-        self,
-        db: AsyncSession,
-        *,
-        key: str,
-        value: Any,
-        skip: int = 0,
-        limit: int = 100,
-        extra_fields: List[Any] = [],
-    ) -> List[ModelType]:
-        statement = (
-            select(self.model).offset(skip).limit(limit).where(getattr(self.model, key) == value)
-        )
-        statement = self.extend_statement(statement, extra_fields=extra_fields)
-        result = await db.execute(statement)
-        return result.scalars().all()
+        # Search for text in specified column
+        if search_text is not None:
+            for key, value in search_text.items():
+                # Search for text in specified column make both input and column lowercase
+                query = query.where(func.lower(getattr(self.model, key)).contains(value.lower()))
+
+        columns = self.model.__table__.columns
+        if order_by and order_by in columns:
+            if order == OrderEnum.ascendent:
+                query = query.order_by(columns[order_by].asc())
+            else:
+                query = query.order_by(columns[order_by].desc())
+
+        if page_params is None:
+            result = await db.execute(query)
+            items = result.scalars().all()
+            return items
+        else:
+            result = await paginate(db, query, page_params)
+            return result
 
     async def create(self, db: AsyncSession, *, obj_in: CreateSchemaType) -> ModelType:
         db_obj = self.model.from_orm(obj_in)
@@ -145,7 +126,6 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db_obj: ModelType,
         obj_in: Union[UpdateSchemaType, Dict[str, Any]],
     ) -> ModelType:
-
         if isinstance(obj_in, dict):
             update_data = obj_in
             fields = obj_in.keys()
@@ -165,6 +145,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         await db.delete(obj)
         await db.commit()
         return obj
+
+    async def delete(self, db: AsyncSession, *, id: int) -> ModelType:
+        return await self.remove(db, id=id)
 
     async def remove_multi(self, db: AsyncSession, *, ids: Union[int, List[int]]) -> ModelType:
         if type(ids) == int:
