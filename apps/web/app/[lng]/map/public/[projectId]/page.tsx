d1ed1a0f7b54d3207624ce4dd29f875e3f0d665a
majkshkurti <majkshkurti@outlook.com>
2025-04-30 15:33:00 +0200

    fix: mapMode bug on public maps

--- apps/web/app/[lng]/map/public/[projectId]/page.tsx
+++ apps/web/app/[lng]/map/public/[projectId]/page.tsx
@@ -12,7 +12,7 @@ import { usePublicProject } from "@/lib/api/projects";
 import type { RootState } from "@/lib/store";
 import { selectFilteredProjectLayers } from "@/lib/store/layer/selectors";
 import { setProjectLayers } from "@/lib/store/layer/slice";
-import { setProject } from "@/lib/store/map/slice";
+import { setMapMode, setProject } from "@/lib/store/map/slice";
 import type { Project, ProjectLayer } from "@/lib/validations/project";
 
 import { useBasemap } from "@/hooks/map/MapHooks";
@@ -45,6 +45,7 @@ export default function MapPage({ params: { projectId } }) {
     if (projectLayers && project) {
       dispatch(setProjectLayers(projectLayers as ProjectLayer[]));
       dispatch(setProject(project));
+      dispatch(setMapMode("public"));
     }
   }, [dispatch, project, projectLayers]);
   const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down("md"));
