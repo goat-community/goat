0782290faaeb21d01ec92e364941edd826488b15
majkshkurti <majkshkurti@outlook.com>
2025-06-17 15:59:49 +0200

    Add legend options

--- apps/web/components/map/panels/style/legend/LegendOptions.tsx
+++ apps/web/components/map/panels/style/legend/LegendOptions.tsx
@@ -6,6 +6,9 @@ import { useTranslation } from "@/i18n/client";
 import type { FeatureLayerProperties } from "@/lib/validations/layer";
 import type { ProjectLayer } from "@/lib/validations/project";
 
+import SectionHeader from "@/components/map/panels/common/SectionHeader";
+import SectionOptions from "@/components/map/panels/common/SectionOptions";
+
 export const LegendOptions = ({
   layer,
   onStyleChange,
@@ -19,48 +22,58 @@ export const LegendOptions = ({
 
   return (
     <>
-      <Stack spacing={4} sx={{ mb: 4 }}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              size="small"
-              color="primary"
-              checked={!!layer.properties?.legend?.show}
-              onChange={(e) => {
-                const showInLegend = e.target.checked;
-                const newStyle = JSON.parse(JSON.stringify(layer.properties)) || {};
-                newStyle.legend = {
-                  ...newStyle.legend,
-                  show: showInLegend,
-                };
-                onStyleChange?.(newStyle as FeatureLayerProperties);
-              }}
+      <SectionHeader active alwaysActive label={t("options")} disableAdvanceOptions />
+      <SectionOptions
+        active={true}
+        baseOptions={
+          <Stack spacing={4} sx={{ mb: 4 }}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  size="small"
+                  color="primary"
+                  checked={!!layer.properties?.legend?.show}
+                  onChange={(e) => {
+                    const showInLegend = e.target.checked;
+                    const newStyle = JSON.parse(JSON.stringify(layer.properties)) || {};
+                    newStyle.legend = {
+                      ...newStyle.legend,
+                      show: showInLegend,
+                    };
+                    onStyleChange?.(newStyle as FeatureLayerProperties);
+                  }}
+                />
+              }
+              label={<Typography variant="body2">{t("show")}</Typography>}
             />
-          }
-          label={<Typography variant="body2">{t("show")}</Typography>}
-        />
-        {layer.properties?.legend?.show && (
-          <TextField
-            value={legendCaption}
-            inputProps={{ style: { fontSize: 13 } }}
-            placeholder={t("caption")}
-            onChange={(e) => {
-              setLegendCaption(e.target.value);
-            }}
-            onBlur={() => {
-              const newStyle = JSON.parse(JSON.stringify(layer.properties)) || {};
-              newStyle.legend = {
-                ...newStyle.legend,
-                caption: legendCaption,
-              };
-              onStyleChange?.(newStyle as FeatureLayerProperties);
-            }}
-            fullWidth
-            label={t("caption")}
-            variant="outlined"
-          />
-        )}
-      </Stack>
+            {layer.properties?.legend?.show && (
+              <TextField
+                value={legendCaption}
+                inputProps={{ style: { fontSize: 13 } }}
+                InputLabelProps={{ style: { fontSize: 13 } }}
+                placeholder={t("caption")}
+                onChange={(e) => {
+                  setLegendCaption(e.target.value);
+                }}
+                onBlur={() => {
+                  const newStyle = JSON.parse(JSON.stringify(layer.properties)) || {};
+                  newStyle.legend = {
+                    ...newStyle.legend,
+                    caption: legendCaption,
+                  };
+                  onStyleChange?.(newStyle as FeatureLayerProperties);
+                }}
+                fullWidth
+                size="small"
+                label={t("caption")}
+                variant="outlined"
+              />
+            )}
+          </Stack>
+        }
+        advancedOptions={<></>}
+        collapsed={false}
+      />
     </>
   );
 };
