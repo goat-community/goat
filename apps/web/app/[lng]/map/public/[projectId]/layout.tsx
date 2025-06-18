7b57ff818187fd80c20c1840ca4ab1262b841ee8
majkshkurti <majkshkurti@outlook.com>
2025-06-18 19:19:54 +0200

    fix(web): remove export from PROJECT_API_BASE_URL in layout.tsx

--- apps/web/app/[lng]/map/public/[projectId]/layout.tsx
+++ apps/web/app/[lng]/map/public/[projectId]/layout.tsx
@@ -1,7 +1,7 @@
 import { APP_URL } from "@/lib/constants";
 import { getLocalizedMetadata } from "@/lib/metadata";
 
-export const PROJECTS_API_BASE_URL = new URL("api/v2/project", process.env.NEXT_PUBLIC_API_URL).href;
+const PROJECTS_API_BASE_URL = new URL("api/v2/project", process.env.NEXT_PUBLIC_API_URL).href;
 
 export async function generateMetadata({ params: { projectId, lng } }) {
   const publicProject = await fetch(`${PROJECTS_API_BASE_URL}/${projectId}/public`).then((res) => res.json());
